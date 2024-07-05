51fbb075fb85df9a6a02a46b653cb0a091029a89
Ilya Andrianov <ilandroxxy@gmail.com>
2024-07-05 15:45:34 +0700

    Занятия Вторник `02Июль2024`:
    - #Варя Разбиради 25 номера различной сложности и типов (длеители).

--- VaryaBarbarina.py
+++ VaryaBarbarina.py
@@ -6,96 +6,69 @@
 # #
 # region Урок: ********************************************************************
 
-"""
-import time
-start = time.time()
-
+# № КЕГЭ № 4335 (Уровень: Базовый)
 '''
-def divisors(x):
-    div = []
-    for j in range(1, x+1):
-        if x % j == 0:
-            div.append(j)
-    return div
-'''
+from fnmatch import *
 
 def divisors(x):
-    div = []
-    for j in range(1, int(x**0.5)+1):
-        if x % j == 0:
-            div += [j, x//j]
-    return sorted(set(div))
-
-print(divisors(100_000_000))
-
-print(time.time() - start)
-"""
-
-'''
-def divisors(x):
-    div = []
+    res = []
     for j in range(2, int(x**0.5)+1):
         if x % j == 0:
-            div += [j, x//j]
-    return sorted(set(div))
+            res += [j, x // j]
+    return sorted(set(res))
+
+
+def prime(x):
+    for j in range(2, int(x**0.5)+1):
+        if x % j == 0:
+            return False
+    return True
 
 
 k = 0
-for x in range(600_001, 10**10):
-    d = [i for i in divisors(x) if i % 10 == 7 and i != 7]
-    if len(d) > 0:
-        print(x, min(d))
+for x in range(960_000+1, 10**10):
+    div = [j for j in divisors(x) if fnmatch(str(j), '*3?') and prime(j)]
+    if len(div) >= 3:
+        print(x, sum(div))
         k += 1
         if k == 5:
             break
 '''
-# 600001 437
-# 600002 47
-# 600003 1227
-# 600005 217
-# 600012 16667
 
+
+# КЕГЭ № 9710 (Уровень: Средний)
+'''
+from fnmatch import *
+R = []
+for x in range(10**9 + 1268, 10**10, 2023):
+    if fnmatch(str(x), '1*1'):
+        # R.append(sum([int(i) for i in str(x)]))
+        R.append([sum(map(int, str(x))), x // 2023])
+
+R = sorted(R)
+M = sorted([x[1] for x in R[-5:]])
+for x in M:
+    print(x)
+'''
+
+# КЕГЭ № 9170 (Уровень: Средний)
 '''
 def divisors(x):
-    div = []
-    for j in range(2, int(x**0.5)+1):
-        if x % j == 0:
-            div += [j, x//j]
-    return sorted(set(div))
-
-
-k = 0
-for x in range(700_001, 10**10):
-    d = divisors(x)
-    if len(d) > 0:
-        M = min(d) + max(d)
-        if M % 10 == 8:
-            print(x, M)
-            k += 1
-            if k == 5:
-                break
-'''
-# 700005 233338
-# 700007 100008
-# 700012 350008
-# 700015 140008
-# 700031 24168
-
-
-from functools import *
-lru_cache(None)
-def divisors(x):
-    div = []
+    res = []
     for j in range(1, int(x**0.5)+1):
         if x % j == 0:
-            div += [j, x//j]
-    return sorted(set(div))
+            if str(j)[0] == '4':
+                res.append(j)
+            if str(x // j)[0] == '4':
+                res.append(x // j)
+    return sorted(set(res))
 
 
-for x in range(35_000_000, 40_000_000+1):
-    n = [i for i in divisors(x) if i % 2 != 0]
-    if len(n) == 5:
-        print(x)
+for x in range(1, 10**6+1):
+    div = divisors(x)
+    if len(div) == 24:
+        print(x, max(div))
+'''
 
 # endregion Урок: ******************************************************************
 # #
