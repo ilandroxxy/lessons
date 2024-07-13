976ae2ddc6847ef5d958a3dcef946fc20f9d1694
Ilya Andrianov <ilandroxxy@gmail.com>
2024-07-13 19:21:32 +0700

    Занятия Суббота `13Июль2024`:
    - #Дарья Разобрали универсальную функцию перевода в различные системы счисления и разобрали 14 номер.
    
    - #Alexander Разобрали 2 номера разные прототипы.
    
    - #Славик Решали сложные номера: 5, 14, 12, 13, 14 новое
    
    - #Алексей Разобрали вопросы по сложным 2 номерам и разобрали 6 номер.

--- AlexanderLosh4ra.py
+++ AlexanderLosh4ra.py
@@ -1,175 +1,24 @@
 # region Домашка: ******************************************************************
 
+# n = int(input())
+# a = str(n)
+# min = int(a[1])
+# max = int(a[1])
+# for i in (a):
+#     if int(i) > max:
+#         max = int(i)
+#     if int(i) < min:
+#         min= int(i)
+# print(max)
+# print(min)
 '''
-a, b, c = int(input()), int(input()), int(input())
-if (a == b) or (b == c) or (a == c):
-    print('Это равнобедренный треугольник')
-elif a == b == c:
-    print('Это равносторонний треугольник')
-'''
-
-# endregion Домашка: ******************************************************************
-# #
-# #
-# region Урок: ********************************************************************
-
-# Цикл - это некоторое повторяющееся действие.
-
-# Цикл for отвечает на запросы: "Повтори n раз", "
-
-# Работа цикла for с функцией range()
-'''
-for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
-print()
-
-
-for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 2 3 4 5 6 7 8 9
-print()
-
-for i in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 - числа кратные 2
-print()
-
-
-for i in range(2, 10+1, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 10 - числа кратные 2
-print()
-
-
-for i in range(1, 10, 2):  # range(START=1, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 1 3 5 7 9 - числа некратные 2
-print()
-
-
-for i in range(10, 0):  # range(START=10, STOP=0-1, STEP=1)
-    print(i, end=' ')  # 2 4 6 8 - числа кратные 2
-print()
-
-for i in range(10, 0, -1):  # range(START=10, STOP=0-1, STEP=-1)
-    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1 - диапазон в обратном порядке
-print()
+n = int(input())
+a = str(n)
+print(max(a))
+print(min(a))
 '''
 
 '''
-for i in range(6518, 10**10, 6518):  
-    print(i, end=' ')  # 2 4 6 8 - числа кратные 6518
-print()
-'''
-
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-# -i -5   -4   -3   -2   -1
-'''
-print(len(M))  # 5 - len() возвращает длину списка M (кол-во элементов в списке).
-
-for i in range(len(M)):
-    # print(i, end=' ')  # 0 1 2 3 4
-    print(M[i], end=' ')  # a b c d e
-print()
-
-for i in range(len(M)):
-    M[i] = M[i]  # ['a', 'b', 'c', 'd', 'e']
-print(M)
-
-for i in range(len(M)):
-    M[i] = M[i] * i  # ['', 'b', 'cc', 'ddd', 'eeee']
-print(M)
-
-
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-# -i -5   -4   -3   -2   -1
-
-s = ''
-for i in range(len(M)):
-    s += M[i]
-    M[i] = M[i] + s
-print(M)  # ['aa', 'bab', 'cabc', 'dabcd', 'eabcde']
-'''
-
-# Работа цикла for напрямую с последовательностями
-'''
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-# -i -5   -4   -3   -2   -1
-
-for x in M:
-    print(x, end=' ')  # a b c d e
-print()
-
-import random
-M = [random.randint(0, 100) for _ in range(10)]
-for x in M:
-    if x % 2 == 0:  # Таким образом отсеиваем только четные элементы
-        print(x, end=' ')  # 70 4 58 40 84
-print()
-'''
-
-# Цикл while отвечает на запросы: "Повторяй действие пока условие истинно", "Бесконечные циклы"
-'''
-for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 10
-print()
-
-i = 2
-while i <= 10:
-    print(i, end=' ')  # 2 4 6 8 10
-    i += 2
-'''
-
-
-# Переводы в base системы счисления
-'''
-n = 8  # n - number
-b = 2  # b - base
-r = ''  # r - result
-while n > 0:
-    r += str(n % b)
-    n //= b
-r = r[::-1]
-print(r)  # 1000
-
-
-n = 8  # n - number
-b = 2  # b - base
-r = ''  # r - result
-while n > 0:
-    r = str(n % b) + r
-    n //= b
-r = r
-print(r)  # 1000
-
-alphabet = sorted('1234567890QWERTYUIOPASDFGHJKLZXCVBNM')
-print(alphabet)
-n = int(input('Введите число в 10-й системе: '))  # n - number
-b = int(input('Введите base систему счисления: '))  # b - base
-r = ''  # r - result
-while n > 0:
-    r = alphabet[n % b] + r
-    n //= b
-r = r
-print(r)  # 1000
-'''
-# 1112
-
-
-# Бесконечные циклы и операторы break, continue, exit()
-'''
-k = 0
-while True:
-    k += 1
-    if k % 2 != 0:
-        continue  # - прерывает итерацию (шаг) цикла в котором лежит
-    if k == 50000:
-        exit()  # выходит полностью из выполнения программы
-    if k == 100000:
-        break  # - прерывает цикл в котором сейчас находится
-    print(k)
-print('КОНЕЦ')
-'''
-
 alphabet = sorted('1234567890QWERTYUIOPASDFGHJKLZXCVBNM')
 while True:
 
@@ -196,14 +45,72 @@ while True:
         # ValueError: int() base must be >= 2 and <= 36, or 0
 
     elif case == 3:
-        pass  # todo #ДЗ попробовать прописать case 3
+        b = int(input('Введите base систему счисления: '))
+        x = input(f'Введите число в {b}-й системе: ')
+        d = int(x, b)  # 10-ая система
+        n = int(input('Введите n-ю систему счисления: '))
+        r = ''
+        while d > 0:
+            r = alphabet[d % n] + r
+            d //= n
+        print(f'Результат перевода в {n}-ю систему счисления: {r}')
 
     elif case == 0:
         break
 
     else:
         print('Что-то я не очень понял вас. ')
+'''
+# endregion Домашка: ******************************************************************
+# #
+# #
+# region Урок: ********************************************************************
 
+# Тип 2 №27371
+# Логическая функция F задаётся выражением ((x ∧ ¬y) → (¬z ∨ ¬w))∧((w→x) ∨ y).
+'''
+print('x y z w')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = ((x and (not y)) <= ((not z) or (not w))) and ((w <= x) or y)
+                if F == 0:
+                    print(x, y, z, w)
+'''
+
+# Тип 2 №18704
+# Логическая функция F задаётся выражением (x ∨ ¬y) ∧ ¬(w ≡ z) ∧ w.
+'''
+print('x y z w')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = (x or (not y)) and (not(w == z)) and w
+                if F == 1:
+                    print(x, y, z, w)
+'''
+
+# Тип 2 №18483
+# Логическая функция F задаётся выражением ((y → w) ≡ (x → ¬z)) ∧ (x ∨ w)
+
+print('x y z w F')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = ((y <= w) == (x <= (not z))) and (x or w)
+                if F == 0:
+                    print(x, y, z, w, int(F))
+
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = ((y <= w) == (x <= (not z))) and (x or w)
+                if F == 1:
+                    print(x, y, z, w, int(F))
 
 # endregion Урок: *************************************************************
 # #
@@ -213,6 +120,6 @@ while True:
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = []
+# ФИПИ = [2]
 # КЕГЭ  = []
 # на следующем уроке:
--- AlexeyWerwerasd.py
+++ AlexeyWerwerasd.py
@@ -1,330 +1,8 @@
 # region Домашка: ************************************************************
 
-'''
-x = int(input())
-if x % 2 == 0:
-    print('Чётное')
-else:
-    print('Нечётное')
-'''
-
-'''
-number = input()  # str
-
-if len(number) != 5:    # str != int
-    print("Вы ввели не пятизначное число")
-else:
-    first = int(number[0])
-    second = int(number[1])
-    third = int(number[2])
-    fourth = int(number[3])
-    fifth = int(number[4])
-
-    if first * third == second + fourth + fifth:
-        print("Да")
-    else:
-        print("Нет")
-'''
-
-'''
-number = int(input())
-
-if 10000 <= number <= 99999: 
-    number = str(number)
-    first = int(number[0])
-    second = int(number[1])
-    third = int(number[2])
-    fourth = int(number[3])
-    fifth = int(number[4])
-
-    if first * third == second + fourth + fifth:
-        print("Да")
-    else:
-        print("Нет")
-else:
-    print("Вы ввели не пятизначное число")
-'''
-
-'''
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
-
-summa = 0
-
-if num1 % 7 == 0 and num1 % 49 != 0 or num1 % 40 == 0:
-    summa += num1
-
-if num2 % 7 == 0 and num2 % 49 != 0 or num2 % 40 == 0:
-    summa += num2
-
-if num3 % 7 == 0 and num3 % 49 != 0 or num3 % 40 == 0:
-    summa += num3
-
-print(summa)
-'''
-
-# endregion Домашка: *********************************************************
-#
-# #
-# region Урок: ************************************************************
-
-# Цикл - это какое-либо повторяющееся действие
-
-# Цикл for: отвечает на запросы: "повтори n раз", "пробеги от a до b"
-
-# Работа с циклом for через функцию range:
-'''
-for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
-print()
-
-
-for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 2 3 4 5 6 7 8 9
-print()
-
-
-for i in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 - все четные числа (кратные 2)
-print()
-
-
-for i in range(1, 10, 2):  # range(START=1, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 1 3 5 7 9 - все нечетные числа (не кратные 2)
-print()
-
-
-for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 10 - все четные числа (кратные 2)
-print()
-
-
-for i in range(10, 0):  # range(START=10, STOP=0-1, STEP=1)
-    print(i, end=' ')  #
-print()
-
-
-for i in range(10, 0, -1):  # range(START=10, STOP=0-1, STEP=-1)
-    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1
-print()
-
-
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-
-print(M[2])  # c
-print(len(M))   # 5 - len() возвращает длину списка, то есть кол-во элементов в нем
-
-for i in range(len(M)):  # range(START=0, STOP=5-1, STEP=1)
-    # print(i, end=' ')  # 0 1 2 3 4
-    print(M[i], end=' ')  # a b c d e
-print()
-
-
-for i in range(len(M)):
-    M[i] = M[i]
-print(M)  # ['a', 'b', 'c', 'd', 'e']
-
-
-for i in range(len(M)):
-    M[i] = M[i] * i
-print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
-'''
-
-# Работа с циклом for напрямую через элементы последовательности:
-'''
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-
-for x in M:
-    print(x, end=' ')  # a b c d e
-print()
-
-for x in M:
-    if x in 'ae':
-        print(x, end=' ')  # a e
-print()
-'''
-
-# Цикл while: отвечает на запросы: "пока условие верно, повторяй действие", "бесконечные циклы"
-'''
-for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 10 - все четные числа (кратные 2)
-print()
-
-
-i = 2
-while i <= 10:
-    print(i, end=' ')  # 2 4 6 8 10
-    i += 2
-'''
-
-#  Перевод в base систему счисления
-
-'''
-num = 8
-base = 2
-r = ''
-while num > 0:
-    r += str(num % base)
-    num //= base
-r = r[::-1]
-print(r)
-'''
-
-'''
-num = int(input('Введите число в 10-й система для перевода: '))
-base = int(input('Введите base систему счисления: '))
-r = ''
-while num > 0:
-    r = str(num % base) + r
-    num //= base
-print(r)
-'''
-
-
-'''
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLXZCVBNM')
-num = int(input('Введите число в 10-й система для перевода: '))
-base = int(input('Введите base систему счисления: '))
-r = ''
-while num > 0:
-    r = alphabet[num % base] + r
-    num //= base
-print(r)
-'''
-
-
-# Бесконечные циклы и операторы: break, continue, exit()
-
-'''
-cnt = 0
-while True:
-    cnt += 1
-    if cnt % 2 != 0:
-        continue  # - прерывает итерацию (шаг) цикла
-    if cnt == 10000:
-        exit()  # - просто прерывает всю программу
-        break  # - прерывает цикл в котором он находится
-    print(cnt)
-print('Конец программы.')
-'''
-
-'''
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLXZCVBNM')
-while True:
-    case = int(input('\ncase 1: Перевод из 10-й в base систему счисления. \n'
-                     'case 2: Перевод из base системы счисления в 10-ю. \n'
-                     'case 3: Перевод из base системы счисления в n-ю. \n'
-                     'case 0: exit \n'))
-
-    if case == 1:
-        num = int(input('Введите число в 10-й система для перевода: '))
-        base = int(input('Введите base систему счисления: '))
-        r = ''
-        while num > 0:
-            r = alphabet[num % base] + r
-            num //= base
-        print(f'Результат перевода: {r}')
-
-    elif case == 2:
-        base = int(input('Введите base систему счисления: '))
-        r = input(f'Введите число в {base} системе счисления: ')
-        print(f'Результат перевода: {int(r, base)}')
-
-    elif case == 3:
-        pass
-
-    elif case == 0:
-        print('Конец программы. ')
-        exit()
-
-    else:
-        print('Неверная команда.')
-'''
-
-# ********************************************
-'''
-x = 19
-print(x % 10)  # 9
-x = -19
-print(x % 10)  # 1
-
-
-a = int(input())
-b = int(input())
-
-for i in range(a,b):
-    if i % 20 == 0 or (i % 7 == 0 and i % 14 == 0) or abs(i) % 10 == 9:
-        print(i)
-'''
-
-'''
-s = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM'
-print(sorted(s))
-# ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-'''
-
-'''
-n = int(input())  # 12345
-summa = 0
-count = 0
-prodd = 1
-while n > 0:
-    x = n % 10
-    summa += x
-    count += 1
-    prodd *= x
-    n //= 10
-print(summa)
-print(count)
-print(prodd)
-'''
-
-# № 16368 ЕГКР 27.04.24 (Уровень: Базовый)
-# Миша заполнял таблицу истинности логической функции F=¬(x∨y)∧¬w∨¬(z∨w)∧y
-'''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = (not(x or y)) and (not w) or (not(z or w)) and y
-                if F == 1:
-                    print(x, y, z, w)
-'''
-
-
-# № 14651 Открытый курс "Слово пацана" (Уровень: Базовый)
-# Логическая функция F задаётся выражением ((z→x)≡(w→y)∨(x∧w)).
-'''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = ((z <= x) == (w <= y) or (x and w))
-                if F == 0:
-                    print(x, y, z, w)
-'''
-
-# КЕГЭ № 6598 Пробник ИМЦ СПб (Уровень: Базовый)
-# Логическая функция F задаётся выражением ((x → w) ∧ (¬y → z)) → ((z ≡ x) ∨ (w ∧ ¬y)).
-'''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = ((x <= w) and ((not y) <= z)) <= ((z == x) or (w and (not y)))
-                if F == 0:
-                    print(x, y, z, w)
-'''
-
 # Решу ЕГЭ № 18483 (Уровень: Базовый)
 # Логическая функция F задаётся выражением ((y → w) ≡ (x → ¬z)) ∧ (x ∨ w).
-
+'''
 print('x y z w F')
 for x in 0, 1:
     for y in 0, 1:
@@ -342,6 +20,117 @@ for x in 0, 1:
                 F = ((y <= w) == (x <= (not z))) and (x or w)
                 if F == 1:
                     print(x, y, z, w, int(F))
+'''
+
+
+# Решу ЕГЭ № 52173 (Уровень: Базовый)
+# Логическая функция F задаётся выражением: (z ≡ ¬x) → ((w → ¬y) ∧ (y → x)).
+'''
+print('x y z w F')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = ((z == (not x)) <= ((w <= (not y)) and (y <= x)))
+                if F == 0:
+                    print(x, y, z, w, int(F))
+
+
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = ((z == (not x)) <= ((w <= (not y)) and (y <= x)))
+                if F == 1:
+                    print(x, y, z, w, int(F))
+
+'''
+# endregion Домашка: *********************************************************
+#
+# #
+# region Урок: ************************************************************
+
+# Тип 6 №47404
+# В начальный момент Черепаха находится в начале координат, её голова
+# направлена вдоль положительного направления оси ординат, хвост опущен.
+# Черепахе был дан для исполнения следующий алгоритм:
+# Повтори 4 [Вперёд 10 Направо 90]
+# Направо 30
+# Повтори 5 [Вперёд 6 Направо 60 Вперёд 6 Направо 120].
+'''
+import turtle as t  # Переименовали библиотеку в t
+t.screensize(-10000, 10000)  # Добавляем масштаб окна, чтобы регулировать его
+t.tracer(0)  # Отключает анимацию отрисовки (мгоновенная отрисовка)
+t.lt(90)  # t.left(90) - изначально по условию необходимо повернуть голову вверх
+l = 20  # Переменная для регулировки масштаба окна
+
+# Тут пишем псевдокод:
+for i in range(4):
+    t.fd(10 * l)
+    t.rt(90)
+t.rt(30)
+t.color('green')
+for i in range(5):
+    t.fd(6 * l)
+    t.rt(60)
+    t.fd(6 * l)
+    t.rt(120)
+
+t.up()  # Поднять перо отрисовки (поднять хвост)
+for x in range(-50, 50):  # Тут перебираем координаты точек
+    for y in range(-50, 50):
+        t.goto(x * l, y * l)  # Перемещаемся в конкретную координату
+        t.dot(3, 'red')  # Рисуем точку толщины 2 и красного цвета
+
+t.update()  # Необходима для .tracer()
+t.done()  # Фиксация окна отрисовки, чтобы оно не закрывалось.
+'''
+
+
+# Тип 6 №59739
+# Черепахе был дан для исполнения следующий алгоритм:
+#
+# Повтори 2 [Вперёд 3 Налево 90 Назад 10 Налево 90]
+# Поднять хвост
+# Назад 10 Направо 90 Вперед 8 Налево 90
+# Опустить хвост
+# Повтори 2 [Вперёд 16 Направо 90 Вперёд 8 Направо 90].
+
+import turtle as t
+t.screensize(-10000, 10000)
+t.tracer(0)
+t.lt(90)
+l = 40
+
+# Тут пишем псевдокод:
+for i in range(2):
+    t.fd(3 * l)
+    t.lt(90)
+    t.bk(10 * l)
+    t.lt(90)
+t.up()  # t.penup()
+t.bk(10 * l)
+t.rt(90)
+t.fd(8 * l)
+t.lt(90)
+t.down()  # t.pendown()
+for i in range(2):
+    t.fd(16 * l)
+    t.rt(90)
+    t.fd(8 * l)
+    t.rt(90)
+
+t.up()
+for x in range(-50, 50):  # Тут перебираем координаты точек
+    for y in range(-50, 50):
+        t.goto(x * l, y * l)  # t.setpos(x * l, y * l)
+        t.dot(2, 'red')
+
+t.update()
+t.done()
+
+
+
 # endregion Урок: ************************************************************
 # #
 # #
