3d97c02f56f860faecb7f79f06499c522280b528
Ilya Andrianov <ilandroxxy@gmail.com>
2024-08-01 23:24:14 +0700

    Занятия Среда `31Июль2024`:
    - #Егор Установили рабочий репозиторий для студентов. Совместно решили несколько 14 номеров из ДЗ.
    
    - #Стас  Установили рабочий репозиторий для студентов. Совместно решили несколько 8 номеров среднего уровня с Практики.
    
    - #Alexander #Александр Провели совместный урок по теории строк str(). И разобрали вопросы по домашки из списков list().

--- StasPrrosto.py
+++ StasPrrosto.py
@@ -1,123 +1,67 @@
 # region Домашка: ******************************************************************
 
+# КЕГЭ № 6985 (Уровень: Средний)
+# (Д. Статный) Все шестибуквенные слова, в составе которых могут быть только русские буквы М, А, Р, К, С, Л
+# записаны в алфавитном порядке и пронумерованы начиная с 1.
+#
+# 1. АААААА
+#  2. АААААК
+#  3. АААААЛ
+#  4. АААААМ
+#  5. АААААР
+#  6. АААААС
+#
+# Под каким последним номером идёт слово, в котором буквы К не стоят рядом
+# с буквой С и одна буква повторяется трижды, а остальные не повторяются?
 '''
-from itertools import permutations
-R = []
-for per in permutations(['1' * 20, '2' * 15, '3' * 40]):
-    s = '>' + ''.join(per) + '<'
-    while '><' not in s:
-        s = s.replace('>1', '3>', 1)
-        s = s.replace('>2', '2>', 1)
-        s = s.replace('>3', '1>', 1)
-        s = s.replace('3<', '<1', 1)
-        s = s.replace('2<', '<3', 1)
-        s = s.replace('1<', '<2', 1)
-    summa = sum(int(i) for i in s if i.isdigit())
-    R.append(summa)
-print(R)
-print(max(R))
+from itertools import *
+n = 0
+sp = []
+for i in product(sorted('МАРКСЛ'), repeat=6):
+    s = ''.join(i)
+    n += 1
+    if 'КС' not in s or 'СК' not in s:
+        # if len(set(s)) == 4 and any(s.count(x) == 3 for x in s):
+        copied = [x for x in s if s.count(x) == 3]
+        not_copied = [x for x in s if s.count(x) == 1]
+        if len(copied) == 3 and len(not_copied) == 3:
+            sp.append(n)
+
+print(max(sp))
 '''
 
 '''
-s = '>' + "1"*11 + '2'*12 + '3'*30
-while '>1' in s or '>2' in s or '>3' in s:
-    if '>1' in s:
-        s = s.replace('>1', '22>', 1)
-    if '>2' in s:
-        s = s.replace('>2', '222>', 1)
-    if '>3' in s:
-        s = s.replace('>3', '1>', 1)
-print(sum(int(i) for i in s if i.isdigit()))
+from itertools import *
+
+sp = []  # БАААА
+n = 0
+for i in product(sorted('БАРШ'), repeat=5):
+    s = ''.join(i)
+    n += 1
+    if s.count('А') >= 2:
+        alp = [s.count(a) for a in s]
+        if alp.count(2) == 2 and alp.count(1) == 3:
+            sp.append(n)
+
+print(max(sp))
+'''
+
+'''
+from itertools import product
+a0 = '2468'
+a1 = '1357'
+k = 0
+for x in product(a1,a0,a1 ,a0,a1,a0,a1,a0,a1):
+    s = ''.join(x)
+    if all(s.count(x) <= 3 for x in s):
+        k += 1
+print(k*2)
 '''
 
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
-
-# Тип 8 №18789
-# Все четырёхбуквенные слова, составленные из букв С, Е, Н, О, записаны
-# в алфавитном порядке и пронумерованы, начиная с 1. Начало списка выглядит так:
-# 1. ЕЕЕЕ
-# 2. ЕЕЕН
-# 3. ЕЕЕО
-# 4. ЕЕЕС
-# 5. ЕЕНЕ
-# Под каким номером в списке идёт первое слово, которое начинается с буквы С?
-'''
-s = sorted('СЕНО')
-num = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                slovo = a + b + c + d
-                num += 1
-                if a == 'С':
-                    print(num, slovo)
-                    exit()
-'''
-'''
-from itertools import product
-num = 0
-for per in product(sorted('СЕНО'), repeat=4):
-    slovo = ''.join(per)
-    num += 1
-    if slovo[0] == 'С':
-        print(num, slovo)
-        exit()
-'''
-
-
-# Тип 8 №16037
-# Вася составляет 5- буквенные слова, в которых есть только буквы З, И, М, А,
-# причём в каждом слове есть ровно одна гласная буква и она встречается ровно 1 раз.
-# Сколько существует таких слов, которые может написать Вася?
-'''
-from itertools import product
-cnt = 0
-for per in product('ЗИМА', repeat=5):
-    s = ''.join(per)
-    if s.count('И') + s.count('А') == 1:
-        cnt += 1
-print(cnt)
-'''
-
-# Тип 8 №58235
-# Сколько существует различных трёхзначных чисел, записанных в четверичной
-# системе счисления, в записи которых сумма первой и последней цифры
-# строго больше цифры стоящей по середине?
-'''
-from itertools import product
-cnt = 0
-for per in product('0123', repeat=3):
-    num = ''.join(per)
-    if num[0] != '0':
-        if int(num[0]) + int(num[2]) > int(num[1]):
-            cnt += 1
-print(cnt)
-'''
-
-# Тип 8 №59741
-# Сколько существует чисел, восьмеричная запись которых содержит 5 цифр,
-# причем в записи нет цифры 1.
-# Также все цифры записи различны и никакие две чётные и две нечётные
-# цифры не стоят рядом.
-
-from itertools import permutations
-cnt = 0
-for per in permutations('0234567', 5):
-    num = ''.join(per)
-    if num[0] != '0':
-        num = num.replace('0', '2').replace('4', '2').replace('6', '2')
-        num = num.replace('5', '3').replace('7', '3')
-        if '22' not in num and '33' not in num:
-            cnt += 1
-print(cnt)
-
-
-
-
 
 
 # endregion Урок: *************************************************************
@@ -131,4 +75,4 @@ print(cnt)
 # #
 # ФИПИ = [2, 5, 6, 8, 12, 14]
 # КЕГЭ  = []
-# на следующем уроке:
+# на следующем уроке: 13, 25
