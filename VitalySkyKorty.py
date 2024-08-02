1c796fbe4721b78bc352da38bec872d44d181686
Ilya Andrianov <ilandroxxy@gmail.com>
2024-08-02 21:16:17 +0700

    Занятия Пятница `02Август2024`:
    - #Виталий Говорили про циклы for с функцией range()
    
    - #Варя Повторили 22 номера новых прототипов, и задачи с экзамена.
    
    - #Егор Прорешали 12 всех прототипов.
    
    - #Николай Проговорили теорию строк и разобрали один самый базовый 12 номер.
    
    - #Alexey Разобрали 12 номера полностью, немного затронули 13 номер (один прототип).

--- VitalySkyKorty.py
+++ VitalySkyKorty.py
@@ -8,114 +8,111 @@
 
 # Цикл - это некоторое повторяющееся действие
 
-# Цикла for отвечает на запрос: "повтори n раз", "пробеги от a до b"
+# Цикл for: "повтори n раз", "пробеги от а до b"
+'''
+# range(STOP-1), range(START=0, STOP=2-1, STEP=1)
+for i in range(2):  # повтори 2 раз
+    print(i)
 
-for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
-    print(i, end=' ')
-print()  # 0 1 2 3 4 5 6 7 8 9
-
-for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 2 3 4 5 6 7 8 9
-print()
-
-for i in range(2, 10, 3):  # range(START=2, STOP=10-1, STEP=3)
-    print(i, end=' ')  # 2 5 8
-print()
-
-
-for i in range(3, 20, 5):
-    print(i, end=' ')  # 3 8 13 18
-print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# range(STOP), range(START, STOP), range(START, STOP, STEP)
-
-for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
+# range(START=0, STOP=10-1, STEP=1)
+for i in range(10):  # повтори 10 раз
     print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
 print()
 
-for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 2 3 4 5 6 7 8 9
+# range(START=5, STOP=10-1, STEP=1)
+for i in range(5, 10):
+    print(i, end=' ')  # 5 6 7 8 9
 print()
 
-for i in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 - Все четные числа или числа кратные 2
+# range(START=8, STOP=12-1, STEP=1)
+for i in range(8, 12):
+    print(i, end=' ')  # 8 9 10 11
 print()
 
-for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 - Все четные числа или числа кратные 2
+# range(START=4, STOP=17-1, STEP=2)
+for i in range(4, 16+1, 2):
+    print(i, end=' ')  # 4 6 8 10 12 14 16
 print()
 
-for i in range(1, 10, 2):  # range(START=1, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 1 3 5 7 9 - Все нечетные числа
+# range(START=16, STOP=4-1, STEP=-2)
+for i in range(16, 4, -2):
+    print(i, end=' ')  # 16 14 12 10 8 6
 print()
 
-for i in range(10, 0):  # range(START=10, STOP=0-1, STEP=1)
-    print(i, end=' ')  #
+# range(START=4, STOP=-16-1, STEP=-2)
+for i in range(4, -16, -2):
+    print(i, end=' ')  # 4 2 0 -2 -4 -6 -8 -10 -12 -14
 print()
 
-for i in range(10, 0, -1):  # range(START=10, STOP=0-1, STEP=-1)
-    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1
+# range(START=3, STOP=13-1, STEP=2)
+for i in range(3, 13, 2):
+    print(i, end=' ')
+print()  # 3 5 7 9 11
+'''
+
+# Списки list()
+'''
+M = [9, 5, 2, 7, '5']
+print(len(M))  # 5 - Функция len() - возвращает длину списка, то есть кол-во элементов в нем
+
+# range(START=0, STOP=5-1, STEP=1)
+for i in range(len(M)):
+    print(i, end=' ')  # 0 1 2 3 4
 print()
 
-# Работа цикла for с последовательностями
-L = [1, 2, 3]
-T = (1, 2, 3)
-S = {1, 2, 3}
-
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-
-print(M[0])  # a
-print(len(M))  # Функция len() - возвращает длину списка/последовательности (кол-во элементов).
-
-for i in range(len(M)):  # range(start=0, stop=5-1, step=1)
-    # print(i, end=' ')  # 0 1 2 3 4
-    print(M[i], end=' ')  # a b c d e
-
-# i  0   1   2    3    4      5
-B = [2, '3', 5, True, 17, [1, 2, 3]]
-print(B[1])  # '3'
-print(B[4])  # 17
-print(B[5])  # [1, 2, 3]
-
-
-# i  0     1     2     3      4       5
-A = [41, '45', 'ABC', 'V', (3, 4), False]
-print(A[4])  # (3, 4)
-print(A[5])  # False
-print(B[3])  # True
+A = ['a', 'b', 'c', 'd', 'e', 'f']
 print(len(A))  # 6
 
+# range(START=0, STOP=6-1, STEP=1)
+for i in range(len(A)):
+    print(i, end=' ')  # 0 1 2 3 4 5
+print()
+
+
+L = [45, 65, 23, 12, 12, 0]
+print(len(L))  # 6
+
+for i in range(len(L)):
+    print(i, end=' ')  # 0 1 2 3 4 5
+'''
+
+'''
+a = 5
+b = 7
+print(a + b)  # 12
+
+
+print(4 * 6)  # 24
+
+print(12 // 4)  # 3 - целая часть от деления
+print(13 // 4)  # 3
+
+print(12 / 4)  # 3.0
+print(13 / 4)  # 3.25
+
+print(12 % 4)  # 0 - остаток от деления
+print(13 % 4)  # 1 - остаток от деления
+
+print(4 ** 2)  # 16
+print(4 * 4)  # 16
+
+print(4 ** 3)  # 64
+print(4 * 4 * 4)  # 64
+
+print(2**4)
+print(2 * 2 * 2 * 2)  # 16
+
+print(3 * 3 * 3)  # 27
+print(3**3)
+
+print(16 ** (1/2))  # 4
+'''
 
 # i   0    1    2    3    4
 M = ['a', 'b', 'c', 'd', 'e']
 print(len(M))  # 5
-for i in range(5):       # range(START, STOP, STEP)
-    print(i)
-
-
-
-
-
-
-
-
-# Цикл while отвечает на запрос: "делай действие пока условие верно", "бесконечные циклы"
+print(M[0])  # a
+print(M[3])  # d
 
 # endregion Урок: ************************************************************
 # #
