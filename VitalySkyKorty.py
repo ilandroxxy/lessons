80dea072039bd9ef63591500ebf8d6825f6b448a
Ilya Andrianov <ilandroxxy@gmail.com>
2024-08-07 23:51:39 +0700

    Занятия Суббота `03Август2024`:
    - #Дарья Обсудили все прототипы 8 номера.
    
    - #Виталий Написали программу проверку паролей через бесконечный цикл while
    
    - #Славик Прорешали сложные задачки на двумерные списки с курса на Степике

--- VitalySkyKorty.py
+++ VitalySkyKorty.py
@@ -8,111 +8,130 @@
 
 # Цикл - это некоторое повторяющееся действие
 
-# Цикл for: "повтори n раз", "пробеги от а до b"
+# Цикл while это цикл с условием: "выполняем действие пока условие верно", "бесконечный цикл"
 '''
-# range(STOP-1), range(START=0, STOP=2-1, STEP=1)
-for i in range(2):  # повтори 2 раз
-    print(i)
-
-# range(START=0, STOP=10-1, STEP=1)
-for i in range(10):  # повтори 10 раз
-    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
+for i in range(2, 10+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
 print()
 
-# range(START=5, STOP=10-1, STEP=1)
-for i in range(5, 10):
-    print(i, end=' ')  # 5 6 7 8 9
+i = 2
+while i <= 10:
+    print(i, end=' ')  # 2 4 6 8 10
+    i += 2
 print()
-
-# range(START=8, STOP=12-1, STEP=1)
-for i in range(8, 12):
-    print(i, end=' ')  # 8 9 10 11
-print()
-
-# range(START=4, STOP=17-1, STEP=2)
-for i in range(4, 16+1, 2):
-    print(i, end=' ')  # 4 6 8 10 12 14 16
-print()
-
-# range(START=16, STOP=4-1, STEP=-2)
-for i in range(16, 4, -2):
-    print(i, end=' ')  # 16 14 12 10 8 6
-print()
-
-# range(START=4, STOP=-16-1, STEP=-2)
-for i in range(4, -16, -2):
-    print(i, end=' ')  # 4 2 0 -2 -4 -6 -8 -10 -12 -14
-print()
-
-# range(START=3, STOP=13-1, STEP=2)
-for i in range(3, 13, 2):
-    print(i, end=' ')
-print()  # 3 5 7 9 11
-'''
-
-# Списки list()
-'''
-M = [9, 5, 2, 7, '5']
-print(len(M))  # 5 - Функция len() - возвращает длину списка, то есть кол-во элементов в нем
-
-# range(START=0, STOP=5-1, STEP=1)
-for i in range(len(M)):
-    print(i, end=' ')  # 0 1 2 3 4
-print()
-
-A = ['a', 'b', 'c', 'd', 'e', 'f']
-print(len(A))  # 6
-
-# range(START=0, STOP=6-1, STEP=1)
-for i in range(len(A)):
-    print(i, end=' ')  # 0 1 2 3 4 5
-print()
-
-
-L = [45, 65, 23, 12, 12, 0]
-print(len(L))  # 6
-
-for i in range(len(L)):
-    print(i, end=' ')  # 0 1 2 3 4 5
 '''
 
 '''
-a = 5
-b = 7
-print(a + b)  # 12
+num = 123
+print(num // 10)  # 12
+print(num % 10)  # 3
 
+num2 = 457
+print(num2 // 10)  # 45
+print(num2 % 10)  # 7
 
-print(4 * 6)  # 24
+num3 = 1245
+print(num3 // 10)  # 124
+print(num3 % 10)  # 5
 
-print(12 // 4)  # 3 - целая часть от деления
-print(13 // 4)  # 3
-
-print(12 / 4)  # 3.0
-print(13 / 4)  # 3.25
-
-print(12 % 4)  # 0 - остаток от деления
-print(13 % 4)  # 1 - остаток от деления
-
-print(4 ** 2)  # 16
-print(4 * 4)  # 16
-
-print(4 ** 3)  # 64
-print(4 * 4 * 4)  # 64
-
-print(2**4)
-print(2 * 2 * 2 * 2)  # 16
-
-print(3 * 3 * 3)  # 27
-print(3**3)
-
-print(16 ** (1/2))  # 4
+print(num3 // 100)  # 12
+print(num3 % 100)  # 45
 '''
 
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-print(len(M))  # 5
-print(M[0])  # a
-print(M[3])  # d
+'''
+num = int(input())  # 123
+summa = 0
+while num > 0:
+    summa += num % 10
+    print(num % 10)
+    num = num // 10
+print(summa)
+'''
+
+
+'''
+from random import randint, choice
+from time import sleep
+
+error_messages = [
+    "Пароль неверный, попробуйте снова: ",
+    "Не удалось подтвердить пароль, попробуйте еще раз: ",
+    "Пароль введен неверно, пожалуйста, попробуйте снова: ",
+    "Ошибка: неверный пароль. Пожалуйста, попробуйте еще раз: ",
+    "Пароль оказался неверным, повторите попытку: ",
+    "Введенный вами пароль не соответствует. Попробуйте снова: ",
+    "Ваш пароль неправильный. Пожалуйста, введите его еще раз: ",
+    "Пароль не подходит, попробуйте ввести его заново: ",
+    "Увы, пароль неверный. Пожалуйста, повторите ввод: ",
+    "Пароль неверен, попробуйте еще раз, пожалуйста: "
+]
+
+password = 'qwerty'
+pas = input('Введите пароль: ')
+count = 0
+while True:
+    if pas == password:
+        print('Пароль верный, добро пожаловать. ')
+        break
+    count += 1
+    if count == 3:
+        print('Проверка на робота.')
+        a = randint(0, 100)
+        b = randint(0, 100)
+        x = int(input(f'Решите пример: {a} + {b} = '))
+        if x == a + b:
+            print('Проверка пройдена успешно.')
+            count = 0
+        else:
+            print('Проверка не пройдена, попробуйте снова через 5 минут.')
+            sleep(5 * 60)
+            count = 0
+    pas = input(choice(error_messages))
+print('Вошли в учетную запись.')
+'''
+
+'''
+def divisors(num):
+    res = []
+    for j in range(1, int(num**0.5) + 1):
+        if num % j == 0:
+            res += [j, num // j]
+    return sorted(set(res))
+
+
+def divisors(num: int) -> list:
+    """
+    Возвращает список уникальных делителей заданного числа.
+
+    Аргументы:
+    num (int): Целое число, для которого необходимо найти делители.
+
+    Возвращаемое значение:
+    list: Отсортированный список уникальных делителей числа `num`.
+
+    Исключения:
+    ValueError: Если `num` не является положительным целым числом.
+
+    Примеры:
+    >>> divisors(10)
+    [1, 2, 5, 10]
+
+    >>> divisors(25)
+    [1, 5, 25]
+
+    >>> divisors(1)
+    [1]
+    """
+    if not isinstance(num, int) or num <= 0:
+        raise ValueError("Входное число должно быть положительным целым числом.")
+
+    res = []
+    for j in range(1, int(num ** 0.5) + 1):
+        if num % j == 0:
+            res += [j, num // j]
+
+    return sorted(set(res))
+'''
 
 # endregion Урок: ************************************************************
 # #
