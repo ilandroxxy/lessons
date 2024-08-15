99da13eca392595017b12e146bffcae36bd974ab
Ilya Andrianov <ilandroxxy@gmail.com>
2024-08-15 19:46:58 +0700

    ЗанятияЧетверг `15Август2024`:
    - #Дмитрий Плотно прошлись по теории циклов, написали классную программу по переводу в различные системы счисления.

--- DimaClosedez.py
+++ DimaClosedez.py
@@ -1,192 +1,260 @@
 # region Домашка: ******************************************************************
 
+'''
+x = -123
+
+# abs() - функция модуля числа (убирает знак минус)
+if abs(x) % 10 == 3:
+    print('Yes')
+else:
+    print('No')
+
+
+print(123 % 10)  # 3
+print(-123 % 10)  # 7
+'''
+
+
+'''
+a = int(input())
+L = [int(x) for x in str(a)]
+if L[0] * L[2] == L[1] + L[3] + L[4]:
+    print('Да')
+else:
+    print('Нет')
+'''
+
+# Вычисление суммы чисел по определенным условиям
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+summa = 0
+if (a % 7 == 0 and a % 49 != 0) or a % 40 == 0:
+    summa += a
+if (b % 7 == 0 and b % 49 != 0) or b % 40 == 0:
+    summa += b
+if (c % 7 == 0 and c % 49 != 0) or c % 40 == 0:
+    summa += c
+print(summa)
+'''
+
+
+# Разработка программы для определения вида треугольника
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    print("Равносторонний")
+elif a != b and a != c and b != c:
+    print("Разносторонний")
+else:
+    print("Равнобедренный")
+'''
 
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-# f - строки
+# Цикл for отвечает на запросы: "повтори N раз", "пробеги от A до B"
+
+
+# Работа цикла for с функцией range()
+# range(STOP), range(START, STOP), range(START, STOP, STEP)
 '''
-weather = 'облачно'
-temperature = 24
-# Сегодня облачно, а температура 24 градуса!
+for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
+    print(i, end=' ')   # 0 1 2 3 4 5 6 7 8 9
+print()
 
-print('Сегодня ', weather, ', а температура ', temperature, ' градуса!')
-print('Сегодня ' + weather + ', а температура ' + str(temperature) + ' градуса!')
-print('Сегодня {}, а температура {} градуса!'.format(weather, temperature))
-print(f'Сегодня {weather}, а температура {temperature} градуса!')
-'''
+for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
+    print(i, end=' ')   # 2 3 4 5 6 7 8 9
+print()
 
-# Переводы в различные системы счисления через f-строки
-'''
-n = 8
-print(bin(n)[2:])  # 1000
-print(oct(n)[2:])  # 10
-print(hex(n)[2:])  # 8
+for i in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
+    print(i, end=' ')   # 2 4 6 8
+print()
 
-n = 8
-print(f'{n:b}')  # 1000
-print(f'{n:o}')  # 10
-print(f'{n:x}')  # 8
-'''
+for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
+    print(i, end=' ')   # 2 4 6 8 10
+print()
+
+for i in range(10, 0):  # range(START=10, STOP=0-1, STEP=1)
+    print(i, end=' ')   # нет вывода
+print()
+
+for i in range(10, 0, -1):  # range(START=10, STOP=0-1, STEP=-1)
+    print(i, end=' ')   # 10 9 8 7 6 5 4 3 2 1
+print()
 
 
-# Использование библиотек в Python и для экзамена ЕГЭ
-'''
-print(16 ** 0.5)  # Взятие квадратного корня от числа 16
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
 
-import math
-print(math.sqrt(16))
+print(len(M))  # 5 Функция len() возвращает длину списка (кол-во элементов в нем)
 
-import math as m  # Переименовали math в переменную m
-print(m.sqrt(16))
+for i in range(len(M)):
+    # print(i, end=' ')  # 0 1 2 3 4
+    print(M[i], end=' ')  # a b c d e
+print()
 
-from math import sqrt, prod  # Импортировали конкретные функции
-print(sqrt(16))
-
-from math import *  # Импортировали сразу все 
-print(sqrt(16))
-'''
-
-# Пригождаются в 8 номере, а также в номерах: 1, 12, 9, 17, 24
-'''
-from itertools import product, permutations
-
-for p in product('12', repeat=5):
-    print(p)
-
-for p in permutations('12345', 5):
-    print(p)
+for i in range(len(M)):
+    M[i] = M[i] * i
+print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
 '''
 
-# Библиотечка для решения 13 номера
+# Цикл for может работать с последовательностями напрямую
 '''
-from ipaddress import *
-net = ip_network('115.192.0.0/255.192.0.0', 0)
-cnt = 0
-for ip in net:
-    s = f'{ip:b}'
-    if s.count('1') % 3 != 0:
-        cnt += 1
-print(cnt)
-'''
-# 2796202
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
 
+for x in M:
+    print(x, end=' ')  # a b c d e
+print()
 
-# Библиотека для решения 25 номера с масками
-'''
-from fnmatch import fnmatch
-for x in range(131, 10**8, 131):
-    if fnmatch(str(x), '1?0?6*39'):
-        print(x, x // 131)
+for x in M:
+    if x in 'ae':
+        print(x, end=' ')  # a e
+print()
 '''
 
-# Библиотека для решения 6 номера с черепашкой
+# Работа с строками и словарями через цикл for
 '''
-import turtle as t
+s = 'abcde'  # str
+for i in range(len(s)):
+    # print(i, end=' ')  # 0 1 2 3 4
+    print(s[i], end=' ')  # a b c d e
+print()
 
-import math
-print(math.prod([1, 2, 3]))
+my_dict = {'один': 'one', 'два': 'two', 'три': 'three'}
+print(my_dict.items())
 
-import string
-alphabet = string.digits + string.ascii_uppercase
-print(alphabet)  # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
-'''
+for elem in my_dict.items():
+    print(elem)
+    # ('один', 'one')
+    # ('два', 'two')
+    # ('три', 'three')
 
-# Условные операторы if, elif, else
-'''
-x, y = -4, 5
-if x > 0 and y > 0:
-    print('Первая четверть')
-elif x < 0 and y > 0:
-    print('Вторая четверть')
-elif x < 0 and y < 0:
-    print('Третья четверть')
-elif x > 0 and y < 0:
-    print('Четвертая четверть')
-else:
-    print('Лежит на осях')
-print('КОНЕЦ')
+for key, value in my_dict.items():
+    print(key, value)
+    # один one
+    # два two
+    # три three
 '''
 
+
+# Цикл while отвечает на запросы: "повторяй действия пока условие верно", "бесконечный цикл"
 '''
-x, y = -4, 5
-if x > 0:
-    if y > 0:  # x > 0, y > 0
-        print('Первая четверть')
-    else:  # x > 0, y <= 0
-        print('Четвертая четверть')
-else:
-    if y > 0:  # x <= 0, y > 0
-        print('Вторая четверть')
-    else:  # x <= 0, y <= 0
-        print('Третья четверть')
-'''
+for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
+    print(i, end=' ')   # 2 4 6 8 10
+print()
 
-# Логические связки and, or, not, ^
-'''
-a = -7
-b = 8
-c = 6
+i = 2
+while i <= 10:
+    print(i, end=' ')  # 2 4 6 8 10
+    i += 2
+print()
 
-if a > 0 and b > 0:  # and - гарантирует, что все условия выполняются
-    print('YES 1')
-if a > 0 or b > 0:  # or - говорит о том, что хотя бы одно условие должно быть верным
-    print('YES 2')
-if (a > 0) ^ (b > 0):  # гарантирует, что только одно условие выполняется
-    print('YES 3')
-if (a > 0) != (b > 0):  # гарантирует, что только одно условие выполняется
-    print('YES 3')
+n = 8  # number
+b = 2  # base system
+res = ''
+while n > 0:
+    res += str(n % b)
+    n //= b
+res = res[::-1]  # Срез в обратном порядке
+print(res)
 
-# Только одно число из трех должно быть положительно:
-if (a > 0) + (b > 0) + (c > 0) == 1:
-    print('yes 1')
+n = 8  # number
+b = 2  # base system
+res = ''
+while n > 0:
+    res = str(n % b) + res
+    n //= b
+print(res)
 
-# Хотя бы два элемента должно быть положительны:
-if (a > 0) + (b > 0) + (c > 0) >= 2:
-    print('yes 2')
+# Срез в обратном порядке
+s = 'abcde'
+print(s[::-1])  # edcba
 '''
 
-from random import randint, choice
+'''
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+def convert(n, b):
+    res = ''
+    while n > 0:
+        res = alphabet[n % b] + res
+        n //= b
+    return res
+
+
+n = int(input('n: '))
+b = int(input('b: '))
+# Пример вызова функции:
+print(convert(n, b))
+'''
+
+'''
+k = 0
+while True:
+    k += 1
+    print(k)
+'''
+
+from string import digits, ascii_uppercase
 from time import sleep
 
-text_error = [
-    "Неверный пароль, попробуйте ещё раз: ",
-    "Пароль неправильный, введите снова: ",
-    "Ошибка пароля, повторите попытку: ",
-    "Пароль введён некорректно, попробуйте ещё раз: ",
-    "Пароль не соответствует, введите ещё раз: ",
-    "Введён неверный пароль, повторите попытку: ",
-    "Пароль ошибочный, попробуйте снова: ",
-    "Неправильный пароль, введите заново: ",
-    "Пароль не подходит, попробуйте ещё раз: ",
-    "Неверный пароль, повторите ввод: "
-]
 
-password = 'qwerty'
-pas = input('Введите пароль от учетной записи: ')
-cnt = 0
-while True:
-    if pas == password:
-        print('Добро пожаловать!')
-        break
-    cnt += 1
-    if cnt == 3:
-        print('Пройдите проверку на робота: ')
-        a = randint(0, 100)
-        b = randint(0, 100)
-        x = int(input(f'Решите пример: {a} + {b} = '))
-        if x == a + b:
-            print('Проверка пройдена успешно.')
-            cnt = 0
+alphabet = digits + ascii_uppercase  # 0123456789 + ABCDEFGHIJKLMNOPQRSTUVWXYZ
+# alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+
+def convert(n: int, b: int) -> str:
+    """
+    Функция для перевода из 10-ой в любую другую систему счисления
+    :param n: Принимает число в 10-ой системе счисления
+    :param b: Принимает систему счисления для перевода
+    :return: Возвращает результат перевода в b-ую систему счисления
+    """
+    res = ''
+    while n > 0:
+        res = alphabet[n % b] + res
+        n //= b
+    return res
+
+
+if __name__ == '__main__':
+    while True:
+        case = input('\n'
+                     'case 1: Перевод из 10-й в N-ную систему счисления. \n'
+                     'case 2: Перевод из N-й в 10-ную систему счисления. \n'
+                     'case 3: Перевод из N-й в К-ную систему счисления. \n'
+                     'case 0: Выход из программы. \n'
+                     'case: ')
+
+        if case == '1':
+            n = int(input('Введите число в 10-ой системе счисления для перевода: '))
+            b = int(input('Введите систему в которую хотим делать перевод: '))
+            print(f'Результат перевода 10-го числа {n} в {b}-ую систему счисления: {convert(n, b)} \n')
+            sleep(3)
+
+        elif case == '2':
+            b = int(input('Введите систему из которой хотим делать перевод: '))
+            r = input(f'Введите число в {b}-ой системе счисления для перевода в 10-ую: ')
+            print(f'Результат перевода {b}-го числа {b} в 10-ую систему счисления: {int(r, b)} \n')
+            sleep(3)
+
+        elif case == '3':
+            sleep(3)
+            pass
+
+        elif case == '0':
+            print('Спасибо, что воспользовались нашим калькулятором!')
+            sleep(3)
+            break
+
         else:
-            print('Повторите попытку через 5 минут.')
-            sleep(5 * 60)
-
-    pas = input(choice(text_error))
-
-print('Мы в программе.')
+            print('Эта команда мне неизвестная, воспользуйтесь кнопками 1, 2, 3, 0')
 
 
 # endregion Урок: *************************************************************
