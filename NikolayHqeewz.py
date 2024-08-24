7ab0e4c9d6c9c0825b6bbc73871816801d189fdb
Ilya Andrianov <ilandroxxy@gmail.com>
2024-08-24 19:29:32 +0700

    Занятия Пятница `23Август2024`:
    - #Николай Разобрали все основные прототипы 14 номера.
    
    - #Alexey Быстро пробежались по 2 и 6 номерам.
    
    - #Тимур #пробник Провели стандартный пробный урок. Город Новосибирск, информатика вы школе есть на базовом уровне, подготовки в школе нет.

--- NikolayHqeewz.py
+++ NikolayHqeewz.py
@@ -1,135 +1,103 @@
 # region Домашка: ******************************************************************
 
+'''
+from itertools import *
+
+R = []
+num = 0
+for p in product(sorted("ФЕВРАЛЬ"), repeat=6):
+    slovo = "".join(p)
+    num += 1
+    if "А" not in slovo and "Е" not in slovo:
+        R.append(num)
+print(min(R))
+'''
 
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
+
+# Тип 14 №26959
+# Значение арифметического выражения 16**18 * 4**10 - 4**6 - 16 записали в системе счисления с основанием 4.
+# Сколько цифр 3 содержится в этой записи?
 '''
-from itertools import product, permutations
+n = 16**18 * 4**10 - 4**6 - 16
+b = 4
+M = []
+while n > 0:
+    M.append(n % b)
+    n //= b
+M = M[::-1]  # M.reverse()
+print(M.count(3))
 
-for p in permutations('123'):
-    print(p)
-    # ('1', '2', '3')
-    # ('1', '3', '2')
-    # ('2', '1', '3')
-    # ('2', '3', '1')
-    # ('3', '1', '2')
-    # ('3', '2', '1')
 
-for p in permutations('123', 2):
-    print(p)
-    # ('1', '2')
-    # ('1', '3')
-    # ('2', '1')
-    # ('2', '3')
-    # ('3', '1')
-    # ('3', '2')
-
-for p in product('123', repeat=100):
-    print(p)
-    # ('1', '1', '1')
-    # ('1', '1', '2')
-    # ('1', '1', '3')
-    # ('1', '2', '1')
-    # ('1', '2', '2')
-    # ('1', '2', '3')
-    # ('1', '3', '1')
-    # ('1', '3', '2')
-    # ('1', '3', '3')
-    # ('2', '1', '1')
-    # ('2', '1', '2')
-    # ('2', '1', '3')
-    # ('2', '2', '1')
-    # ('2', '2', '2')
-    # ('2', '2', '3')
-    # ('2', '3', '1')
-    # ('2', '3', '2')
-    # ('2', '3', '3')
-    # ('3', '1', '1')
-    # ('3', '1', '2')
-    # ('3', '1', '3')
-    # ('3', '2', '1')
-    # ('3', '2', '2')
-    # ('3', '2', '3')
-    # ('3', '3', '1')
-    # ('3', '3', '2')
-    # ('3', '3', '3')
+n = 16**18 * 4**10 - 4**6 - 16
+M = []
+while n > 0:
+    M = [n % 4] + M
+    n //= 4
+print(M.count(3))
 '''
 
-
-# Тип 8 №39237
+# № 16380 ЕГКР 27.04.24 (Уровень: Базовый)
 '''
-# Вариант 1
-s = sorted('АВТОР')
-num = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                slovo = a + b + c + d
-                num += 1
-                if slovo == 'ТАРА':
-                    print(num, slovo)
+n = 4*3125 ** 2019 + 3*625**2020 -2 * 125**2021  +25**2022 -4*5**2023 - 2024
+M = []
+while n > 0:
+    M.append(n % 25)
+    n //= 25
+M.reverse()
+print(len([x for x in M if x > 10]))
+'''
 
-# Вариант 2
-from itertools import *
-num = 0
-for p in product(sorted('АВТОР'), repeat=4):
-    slovo = ''.join(p)
-    num += 1
-    if slovo == 'ТАРА':
-        print(num, slovo)
+# № 17555 Основная волна 08.06.24 (Уровень: Базовый)
+'''
+for x in range(2030+1):
+    n = 7**91 + 7**160 - x
+    M = []
+    while n > 0:
+        M.append(n % 7)
+        n //= 7
+    M.reverse()
+    if M.count(0) == 70:
+        print(x)
+'''
+
+'''
+print(int('324', 37))
+# ValueError: int() base must be >= 2 and <= 36, or 0
 '''
 
 
-# Тип 8 №18586
+# Тип 14 №48385
 '''
-# Вариант 1
-s = 'СВЕТА'
-cnt = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    slovo = a + b + c + d + e
-                    if slovo.count('С') >= 1:
-                        cnt += 1
-print(cnt)
+from string import *
+alphbaet = digits + ascii_uppercase
+print(alphbaet)  # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
-# Вариант 2
-from itertools import *
-cnt = 0
-for p in product('СВЕТА', repeat=5):
-    slovo = ''.join(p)
-    if slovo.count('С') >= 1:
-        cnt += 1
-print(cnt)
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+for x in alphabet[:13]:
+    for y in alphabet[:13]:
+        A = int(f'8{x}78{y}', 13)
+        B = int(f'79{x}{y}7', 18)
+        if (A + B) % 9 == 0:
+            print((A + B) // 9)
+            exit()
 '''
 
+'''
+from string import *
+alphbaet = digits + ascii_uppercase
 
-# Тип 8 №59741
-# Сколько существует чисел, восьмеричная запись которых содержит 5 цифр,
-# причем в записи нет цифры 1.
-# Также все цифры записи различны и никакие две чётные и две нечётные цифры не стоят рядом.
-
-s = '01234567'
-cnt = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    num = a + b + c + d + e
-                    if a != '0' and '1' not in num:
-                        if len(num) == len(set(num)):
-                            num = num.replace('0', '2').replace('4', '2').replace('6', '2')
-                            num = num.replace('3', '1').replace('5', '1').replace('7', '1')
-                            if '11' not in num and '22' not in num:
-                                cnt += 1
-print(cnt)
+for p in range(7, 36+1):
+    for x in alphbaet[:p]:
+        for y in alphbaet[:p]:
+            for z in alphbaet[:p]:
+                if int(f'{y}4{y}', p) + int(f'{y}65', p) == int(f'{x}{z}33', p):
+                    print(int(x + y + z, p))
+'''
 
 
 # endregion Урок: *************************************************************
@@ -140,6 +108,6 @@ print(cnt)
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = [2, 6, 8, 12]
+# ФИПИ = [2, 6, 8, 12, 14]
 # КЕГЭ  = []
 # на следующем уроке:
