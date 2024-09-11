a9f250f11e09a9a7f232dcc68fb761dc597a5aa8
Ilya Andrianov <ilandroxxy@gmail.com>
2024-09-11 21:34:20 +0700

    Занятия Понедельник `09Сентябрь2024`:
    - #Кирилл Закрыли домашку по условным операторам и потратили урок на установку ПО и учебного репозитория.
    
    - #Кира Попробовали установить учебный репозиторий, но завис моноблок и мы перешли на теорию, успели обсудить только типы данных переменных и коллекций.
    
    - #Дмитрий Провели полноценный первый урок по типам данных и базовой арифметике.
    
    - #Артём Провели полноценный первый урок по типам данных и базовой арифметике.
    
    - #Alexey Плотно прошлись по 15 номерам, разобрали все вопросы и прототипы.

--- AlexeyLewkalepewka.py
+++ AlexeyLewkalepewka.py
@@ -6,186 +6,87 @@
 # #
 # region Урок: ********************************************************************
 
+# Тип 15 №33485
+# Обозначим через ДЕЛ(n, m) утверждение «натуральное число n делится без остатка на натуральное число m».
+# Для какого наибольшего натурального числа А формула
+# ДЕЛ(120, A) ∧ (¬ДЕЛ(x, А) → (ДЕЛ(x, 18) → ¬ДЕЛ(x, 24)))
+# тождественно истинна (то есть принимает значение 1 при любом натуральном значении переменной x)?
 '''
-def divisors(x):
-    div = []
-    for j in range(1, int(x**0.5)+1):
-        if x % j == 0:
-            div += [j, x//j]
-    return sorted(set(div))
+# Вариант 1
+def F(x, A):
+    return (120 % A == 0) and ((x % A != 0) <= ((x % 18 == 0) <= (x % 24 != 0)))
 
 
-def prime(x):
-    if x <= 1:
-        return False
-    for j in range(2, int(x**0.5)+1):
-        if x % j == 0:
-            return False
-    return True
+R = []
+for A in range(1, 10000):
+    flag = True
+    for x in range(1, 10000):
+        if not F(x, A):
+            flag = False
+            break
+    if flag:
+        R.append(A)
+
+print(max(R))
 
 
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-def convert(n, b):
-    r = ''
-    while n > 0:
-        r = alphabet[n % b] + r
-        n //= b
-    return r
-'''
+# Вариант 2
+def F(x, A):
+    return (120 % A == 0) and ((x % A != 0) <= ((x % 18 == 0) <= (x % 24 != 0)))
 
 
-# Тип 16 №6459
-'''
-def F(n):
-    if n <= 2:
-        return 2
-    if n > 2:
-        return 3 * F(n - 1) - F(n - 2)
+R = []
+for A in range(1, 10000):
+    k = 0
+    for x in range(1, 10000):
+        if F(x, A):
+            k += 1
+    if k == 9999:
+        R.append(A)
 
-print(F(6))
-'''
-
-'''
-def F(n):
-    if n <= 1:
-        return 0
-    if n > 1 and n % 2 != 0:
-        return F(n - 1) + 3*n**2
-    if n > 1 and n % 2 == 0:
-        return n / 2 + F(n - 1) + 2
-
-print(F(49))
+print(max(R))
 '''
 
-
-# Тип 16 №59721
+# Вариант 3
 '''
-import sys
-sys.setrecursionlimit(10000)
+def F(x, A):
+    return (120 % A == 0) and ((x % A != 0) <= ((x % 18 == 0) <= (x % 24 != 0)))
 
-def F(n):
-    if n == 1:
-        return n
-    if n > 1:
-        return n - 1 + F(n - 1)
+R = []
+for A in range(1, 10000):
+    if all(F(x, A) for x in range(1, 10000)):
+        R.append(A)
+print(max(R))
 
 
-print(F(2024) - F(2022))
+# Вариант 4
 
-# [Previous line repeated 996 more times]
-# RecursionError: maximum recursion depth exceeded
-
-# F(2024) = 2023 + F(2023)
-# F(2023) = 2022 + F(2022) - F(2022)
-print(2023 + 2022)
+print(max([A for A in range(1, 10000) if all(((120 % A == 0) and ((x % A != 0) <= ((x % 18 == 0) <= (x % 24 != 0)))) for x in range(1, 10000))]))
 '''
 
 
-# № 4739 (Уровень: Средний)
-'''
-from functools import *
-import sys
+# Тип 15 №14277
+# На числовой прямой даны два отрезка: P=[17, 40] и Q=[20, 57].
+# Отрезок A таков, что приведённая ниже формула истинна при любом значении переменной х:
+# ¬(x ∈ A) →(((x ∈ P) ⋀ (x ∈ Q)) → (x ∈ A))
+# Какова наименьшая возможная длина отрезка A?
 
-sys.setrecursionlimit(100000)
-@lru_cache(None)
-def F(n):
-    if n > 10000:
-        return n - 10000
-    if 1 <= n <= 10000:
-        return F(n + 1) + F(n + 2)
-
-print(F(12345) * (1) + F(10101))
-'''
-# F(10) = F(11) / F(11) = 1
+def F(a1, a2, x):
+    P = 17 <= x <= 40
+    Q = 20 <= x <= 57
+    A = a1 <= x <= a2
+    return (not A) <= ((P and Q) <= A)
 
 
-# № 17562 Основная волна 08.06.24 (Уровень: Базовый)
-# У исполнителя есть три команды, которым присвоены номера:
-# A. Прибавить 1
-# B. Прибавить 2
-# C. Прибавить 3
-# Сколько существует программ, которые преобразуют число 5 в число 11,
-# и при этом траектория вычислений содержит число 7?
-'''
-def F(a, b):  # a - начало + траектория, b - конец
-    if a > b:
-        return 0
-    elif a == b:
-        return 1
-    else:
-        return F(a+1, b) + F(a+2, b) + F(a+3, b)
+R = []
+M = [x / 4 for x in range(10 * 4, 60 * 4)]
+print(M)
+for a1 in M:
+    for a2 in M:
+        if all(F(a1, a2, x) for x in M):
+            R.append(a2 - a1)
+print(min(R))  # 20.0 -> 20
 
-
-print(F(5, 7) * F(7, 11))
-
-
-def F(a, b):  # a - начало + траектория, b - конец
-    if a >= b:
-        return a == b
-    return F(a+1, b) + F(a+2, b) + F(a+3, b)
-
-
-print(F(5, 7) * F(7, 11))
-'''
-
-
-# № 17877 Демоверсия 2025 (Уровень: Базовый)
-'''
-def F(a, b):
-    if a <= b:
-        return a == b
-    return F(a-2, b) + F(a//2, b)
-
-print(F(38, 16) * F(16, 2))
-'''
-
-'''
-from functools import *
-
-my_set = set()
-
-@lru_cache(None)
-def F(a, c):
-    if c == 68:
-        my_set.add(a)
-        return 0
-    else:
-        return F(a+3, c+1) + F(a-2, c+1)
-
-
-F(1, 0)
-print(len(my_set))
-'''
-
-
-# № 10027 (Уровень: Базовый)
-'''
-def F(a, b, c: str):
-    if a >= b:
-        return a == b and c[0] != 'C'
-    return F(a+2, b, c+'A') + F(a+3, b, c+'B') + F(a*2, b, c+'C')
-
-print(F(5, 30, ''))
-'''
-
-
-#  7623 Досрочная волна 2023 (Уровень: Базовый)
-# У исполнителя есть три команды, которым обозначены латинскими буквами:
-# A. Прибавить 1
-# B. Умножить на 2
-# C. Умножить на 3
-#
-# Сколько существует программ, для которых при исходном числе
-# 2 результатом является число 25, и при этом траектория вычислений содержит число 15, но не содержит число 11?
-'''
-def F(a, b):
-    if a >= b or a == 11:
-        return a == b
-    return F(a+1, b) + F(a*2, b) + F(a*3, b)
-
-
-print(F(2, 15) * F(15, 25))
-'''
 
 # endregion Урок: *************************************************************
 # #
@@ -196,7 +97,7 @@ print(F(2, 15) * F(15, 25))
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = [2, 5, 6, 8, 12, 13, 14, 25]
+# ФИПИ = [2, 5, 6, 8, 12, 13, 14, 15, 16, 23, 25]
 # КЕГЭ  = []
 # на следующем уроке:
 
