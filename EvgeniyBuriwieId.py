94f1288fdf42be5edf30d5157347783e7bf83f58
Ilya Andrianov <ilandroxxy@gmail.com>
2024-09-25 00:22:11 +0700

    Занятия Вторник `24Сентябрь2024`:
    - #Захар #Kirill Скачали все необходимые программы и установили учебный проект.
    
    - #Стас Разобрали 9 номера всех прототипов, порешали самостоятельно.
    
    - #Евгений Прорешивали 17 номера самостоятельно из домашки
    
    - #Матвей Провели урок по циклам. Написали программу проверки паролей.

--- EvgeniyBuriwieId.py
+++ EvgeniyBuriwieId.py
@@ -1,86 +1,56 @@
 # region Домашка: ************************************************************
 
 
+M = [1, 2, 3, 4, 5]
+'''
+# 1. Назовём парой два идущих подряд элемента последовательности.
+# 12 23 34 45
+for i in range(len(M)-1):
+    x, y = M[i], M[i+1]
+    print(f'{x}{y}', end=' ')
+print()
+
+# 2. Назовём тройкой три идущих подряд элемента последовательности.
+# 123 234 345
+for i in range(len(M)-2):
+    x, y, z = M[i], M[i+1], M[i+2]
+    print(f'{x}{y}{z}', end=' ')
+print()
+
+# 3. В данной задаче под парой подразумевается два различных элемента последовательности.
+# 12 13 14 15
+# 23 24 25
+# 34 35
+# 45
+for i in range(len(M)):
+    for j in range(i+1, len(M)):
+        x, y = M[i], M[j]
+        print(f'{x}{y}', end=' ')
+    print()
+'''
+
+# № 4415 (Уровень: Базовый)
+# В файле содержится последовательность из 10000 целых положительных чисел.
+# Каждое число не превышает  10000.  Определите и запишите в ответе сначала
+# количество пар элементов последовательности, у которых разность элементов кратна 60,
+# затем максимальную из разностей элементов таких пар.
+# В данной задаче под парой подразумевается два различных элемента последовательности.
+'''
+M = [int(x) for x in open("17.txt")]
+R = []
+for i in range(len(M)):
+    for j in range(i+1, len(M)):
+        x, y = M[i], M[j]
+        if abs(x - y) % 60 == 0:
+            R.append(x - y)
+print(len(R), max(R))
+'''
+
 # endregion Домашка: ************************************************************
 # #
 # #
 # region Урок: ************************************************************
 
-# Тип номер 16 из варианта: https://kompege.ru/variant?kim=25057665
-'''
-import sys
-sys.setrecursionlimit(10000)
-
-def F(n):
-    if n == 1:
-        return 1
-    if n > 1:
-        return (n - 1) * F(n - 1)
-
-
-print((F(2024) // 7 - F(2023)) / F(2022))
-'''
-
-
-# Тип номер 14 из варианта: https://kompege.ru/variant?kim=25057665
-'''
-for x in range(2030+1):
-    n = 6 ** 260 + 6 ** 160 + 6 ** 60 - x
-    M = []
-    while n > 0:
-        M.append(n % 6)
-        n //= 6
-    M.reverse()
-    if M.count(0) == 202:
-        print(x)
-'''
-'''
-for x in range(2030+1):
-    n = 7 ** 91 + 7 ** 160 - x
-    M = []
-    while n > 0:
-        M.append(n % 7)
-        n //= 7
-    M.reverse()
-    if M.count(0) == 70:
-        print(x)
-'''
-
-'''
-from ipaddress import *
-net = ip_network('115.192.0.0/255.192.0.0', 0)
-cnt = 0
-for ip in net:
-    s = f'{ip:b}'
-    if s.count('1') % 3 != 0:
-        cnt += 1
-print(cnt)
-'''
-
-
-'''
-s = sorted("ЛАЙМ")
-k = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    slovo = a + b + c + d + e
-                    k += 1
-                    if "М" not in slovo and "Л" not in slovo and "ЙЙ" not in slovo:
-                        print(k)
-'''
-
-
-M = [int(x) for x in open('17.txt')]
-D = [x for x in M if x > 0 and x % 41 == 0]
-R = []
-for i in range(len(M)-1):
-    x, y = M[i], M[i+1]
-    if x != y and abs(x - y) % min(D) == 0:
-        R.append(x + y)
-print(len(R), max(R))
 
 # endregion Урок: ************************************************************
 # #
