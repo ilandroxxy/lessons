56bc55dfa9f105510f42bee354bc2848bfd1d5a4
Ilya Andrianov <ilandroxxy@gmail.com>
2024-11-20 23:48:55 +0700

    Занятия Среда `20Ноябрь2024`:
    - #Egor Закрепили и самостоятельно решали 5 номер
    
    - #Ярослав #Женя Закрепили и самостоятельно решали 5 номер

--- YaroslavStockholm.py
+++ YaroslavStockholm.py
@@ -7,10 +7,17 @@
 # region Урок: ********************************************************************
 
 
-# Задание 5 https://education.yandex.ru/ege/task/0f0be116-5c24-4561-8675-493fe3c6ba53
+# Задание 5 https://education.yandex.ru/ege/task/fef8d85c-6b9d-43d3-954c-6acb16a1f5a9
+
 '''
-from string import *
-alphabet = digits + ascii_uppercase
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+print(alphabet[:2])  # ['0', '1']
+print(alphabet[:8])  # ['0', '1', '2', '3', '4', '5', '6', '7']
+print(alphabet[:16])  # ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+'''
+
+'''
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
 
 
 def convert(n, b):
@@ -21,70 +28,28 @@ def convert(n, b):
     return s[::-1]
 
 
-for n in range(1, 1000):
-    s = convert(n, 5)
-    if n % 25 == 0:
-        s = s[-3:] + s
+R = []
+for n in range(11, 1000):
+    # s = bin(n)[2:]
+    # s = f'{n:b}'
+    s = convert(n, 2)
+    if n % 5 == 0:
+        # s = s + s[-3] + s[-2] + s[-1]
+        s = s + s[-3:]
     else:
-        x = (n % 25)
-        s += convert(x, 5)
-    r = int(s, 5)
-
-    if r > 10000:
-        print(n)
-        break
-'''
-
-
-# Задание 5 https://education.yandex.ru/ege/task/4e651673-c848-47cd-8dab-813092d13201
-'''
-cnt = 0
-for n in range(100, 1000):
-    # if str(n).count('0') == 0:
-    if '0' not in str(n):
-        if all(n % int(x) == 0 for x in str(n)):
-            new_n = int(str(n)[::-1])
-            if all(new_n % int(x) == 0 for x in str(new_n)):
-                cnt += 1
-print(cnt)
-'''
-
-
-# Задание 5 https://education.yandex.ru/ege/task/96524f2d-3f1b-458d-9ee1-9b6d45c39389
-'''
-for n in range(1, 1000):
-    s = bin(n)[2:]
-    if len(s) % 2 == 0:
-        s = s[:len(s) // 2] + '000' + s[len(s) // 2:]
-    else:
-        s = '1' + s + '01'
+        x = (n % 5) * 5
+        s = convert(x, 2) + s
     r = int(s, 2)
+    if r > 512:
+        R.append(n)
 
-    if r > 100:
-        print(n)
-        break
+print(min(R))
 '''
 
 
-# Задание 5 https://education.yandex.ru/ege/task/6e679e3e-d6c0-4c26-9e5e-d6b89730c238
-'''
-M = []
-for n in range(1, 13):
-    s = bin(n)[2:]
-    if n % 2 == 0:
-        s = '10' + s
-    else:
-        s = '1' + s + '01'
-    r = int(s, 2)
-    if n <= 12:
-        M.append(r)
-print(max(M))
-'''
+# Задание 5 https://education.yandex.ru/ege/task/a579a1b6-fe4b-45e9-afc3-57649ff2fa75
 
-'''
-from string import *
-alphabet = digits + ascii_uppercase
-
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
 
 def convert(n, b):
     s = ''
@@ -94,16 +59,16 @@ def convert(n, b):
     return s[::-1]
 
 
-M = []
-for n in range(1, 1000):
-    s = convert(n, 3)
-    s = ''.join(sorted(s, reverse=True))
-    s = s + max(s)  # s[0]
-    r = int(s, 3)
-    if r < 1200:
-        M.append(r)
-print(max(M))
-'''
+for n in range(1, 10000):
+    s = convert(n, 7)
+    if s.count('2') % 2 == 0:
+        s = s + '555'
+    else:
+        s = '1' + s
+    r = int(s, 7)
+    if r < 3799:
+        print(n)
+
 
 # endregion Урок: *************************************************************
 # #
