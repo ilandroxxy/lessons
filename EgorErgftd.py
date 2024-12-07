9294461253d1b1750c26bcfe4cfde9af57848494
Ilya Andrianov <ilandroxxy@gmail.com>
2024-12-07 15:40:30 +0700

    Занятия Пятница `06Декабрь2024`:
    - #Николай Разбирали 1 номер кодом
    
    - #Ефим Разобрали ТИ на кучу и две кучи
    
    - #Ярослав #Женя Прорабатывали 12 номера, писали функцию проверки простого числа
    
    - #Егор #Михаил  Пробовали размотать 11 номер
    
    - #Dmitry Закрепляли 13 номера всех прототипов, самостоятельное решение

--- EgorErgftd.py
+++ EgorErgftd.py
@@ -1,138 +1,108 @@
 # region Домашка: ******************************************************************
 
 
+# № 17548 Основная волна 08.06.24 (Уровень: Базовый)
+'''
+# colors = 2 ** i
+# V = pixels * i
+
+# 2048 >= 2 ** 11
+i = 11
+v = 1024 * 960 * i  # Одна фотка
+m = 96_468_992 * 280   # Объем всего пакета фоток
+
+print(m / v)  # 2497.93
+'''
+
+# colors = 2 ** i
+# V = pixels * i
+
+
+# № 17626 Основная волна 19.06.24 (Уровень: Базовый)
+'''
+m = 1_966_080 * 280   # Объем всего пакета фоток
+V = m / 39
+pixels = 1280 * 1024
+i = V / pixels
+print(i)  # 10.7692307
+print(2**10)
+'''
+
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-# 1 бит - минимальная единица измерения информации
-# 1 байт - 8 бит - 2**3 бит
-# 1 Кбайт - 1024 байт - 2**10 - байт - 2**13 бит
-# 1 Мбайт - 1024 Кбайт - 2**20 байт - 2**23 бит
-# 1 Гбайт - 1024 Мбайт - 2**30 байт - 2**33 бит
 
-
-# Задание 7 https://education.yandex.ru/ege/task/37dd91ce-1a3d-4d33-9370-445d9c44bb85
-
-# pixels - кол-во пикселей на картинке (разрешение)
-# V - вес картинки в бит
-# i - сколько бит выделяется для 1 пикселя
-
-# V = pixels * i
-
-# colors - кол-во цветов передаваемое пикселем
-# colors = 2 ** i
+# Задание 11 https://education.yandex.ru/ege/task/8cdef5ec-7247-4ed7-aa24-502481f2a4c8
 '''
-i = 5  # бит
-pixels = 300 * 400
-V = (pixels * i) / (2**3) # бит
-print(V)
+# pin: symbols = 4, alphabet = 10 (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+symbols = 163
+alphabet = 10 + 1500
+# alphabet = 2 ** i , 2**i >= alphabet
+print(2**11)
+i = 11  # бит на один символ
+bit = symbols * i  # бит
+print(bit / 8)  # 224.125
+byte = 225
+print((byte * 65536) / 2**10)
 '''
 
 
-# https://education.yandex.ru/ege/task/c4adfb30-2b1d-4601-869e-d52ed7dab6cb
+# https://education.yandex.ru/ege/task/fe6ce526-abbb-40cd-8a91-ccfd1a766e1d
 '''
-pixels = 1024 * 768
+symbols = 64
+alphabet = 26 * 2
+print(2**6)  # 2**6 >= 52
+i = 6
+bit = symbols * i
+print(bit / 8)  # 48.0
 
-V = (1_638_400 * 220) / 27
-i = V / pixels
-print(i)  # 16.975
-print(2**16)
+byte = 48
+print(((byte + 64) * 768) / 2**10)
 '''
 
 
-# https://education.yandex.ru/ege/task/5511c181-ed14-4583-aadc-4a8a2d07ecd0
+# https://education.yandex.ru/ege/task/30431c9e-dfcf-41dc-8b5d-1c0fd87fcb2e
 '''
-pixels = 640 * 480
-V = 40*2**13
-i = V / pixels
+symbols = 31
+alphabet = 10 + 26
+i = 6
+bit = symbols * i
+print(bit / 8)  # 23.25
+byte = 24
+
+
+V = (12 * 2**10) / 128  # вес одного пользователя
+print(V - byte)
+'''
+
+
+#
+# todo разобрать № 17552 Основная волна 08.06.24 (Уровень: Сложный)
+'''
+symbols = 261
+
+byte = (31 * 2**20) / 252_500
+print(byte)  # 128.736
+bit = byte * 8
+i = bit / symbols
 print(i)
-print(2**1)
-'''
-
-
-# https://education.yandex.ru/ege/task/b211c7d5-1f29-4efd-bbdd-ee32b831869e
-'''
-pixels = 1280 * 1024
-i = 32
-V = pixels * i
-V = (V / 100) * 60
-print(V / (2**23))
-'''
-
-
-# https://education.yandex.ru/ege/task/c0059c7b-fccc-42d5-b1b0-dffa46d4fccc
-'''
-pixels = 480 * 768
-V = 405 * 2**13
-i = V / pixels
-print(i)  # 9
-print(2**6)
-'''
-
-
-# https://education.yandex.ru/ege/task/26133fed-3f76-4e26-9b4a-a96c4160e69c
-'''
-pixels = 1536*1024
-V = (9 * 2**23) / 6
-i = V / pixels
 print(2**i)
 '''
 
 
-# https://education.yandex.ru/ege/task/c750feb6-6609-485a-8bd0-c62b806727d3
 '''
-colors = 24
-i = 5
-pixels = 1024 * 768
-V = pixels * i
-print(V / 65536)
+byte = (11 * 2 ** 20) / 600_000
+print(byte)
+byte = 19
+bit = byte * 8
+symbols = 20
+i = bit / symbols
+print(i)
+print(2**7)
 '''
-
-# https://education.yandex.ru/ege/task/5b08e032-88b8-4cff-8b63-329f996e0472
-'''
-# V = a * b * c * t
-a = 2
-t = 90
-b = 48000
-
-V = 1600 * 86400
-c = V / (a * b * t)
-print(c)
-'''
-
-
-# https://education.yandex.ru/ege/task/32b13529-3681-4545-9ca9-fa9a88849498
-'''
-a = 4
-c = 8  # глубина кодирования 
-t = 8 * 60 + 32
-
-V = 62.5 * 2**23
-b = V / (a * c * t)
-print(b)
-'''
-
-'''
-a = 2
-b = 16000
-t = 4*60 + 16
-
-V = 16*2**23
-print(V / (a * b * t))
-'''
-# 16.384
-
-
-# https://education.yandex.ru/ege/task/eeee81f3-071e-46a6-967d-043f06c8af92
-
-t = 8
-a = 1
-b = 10000
-c = 16
-V = a * b * c * t
-print(V / 2**13)
 
 # endregion Урок: *************************************************************
 # #
@@ -143,6 +113,6 @@ print(V / 2**13)
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = [1, 2, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 23, 25]
+# ФИПИ = [1, 2, 4, 5, 6, 7, 8, 9, 11-, 12, 13, 14, 15, 16, 17, 23, 25]
 # КЕГЭ  = []
 # на следующем уроке:
--- YaroslavStockholm.py
+++ YaroslavStockholm.py
@@ -1,160 +1,117 @@
 # region Домашка: ******************************************************************
 
+'''
+from itertools import *
+
+for p in product('abc', repeat=5):
+    print(p)  # ('c', 'c', 'c', 'c', 'c')
+    print(''.join(p))  # 'ccccc'
+
+
+for p in permutations('abc', r=3):
+    print(p)  # ('c', 'b', 'a')
+    print(''.join(p))  # 'cba'
+'''
+
+
+# № 776 Джобс (Уровень: Сложный)
+'''
+from itertools import *
+R = []
+for p in permutations(['1' * 20, '3' * 40, '2' * 15], r=3):
+    s = '>' + ''.join(p) + '<'
+
+    while '><' not in s:
+        s = s.replace('>1', '3>', 1)
+        s = s.replace('>2', '2>', 1)
+        s = s.replace('>3', '1>', 1)
+        s = s.replace('3<', '<1', 1)
+        s = s.replace('2<', '<3', 1)
+        s = s.replace('1<', '<2', 1)
+    summa = sum([int(x) for x in s if x.isdigit()])
+    R.append(summa)
+print(max(R))
+'''
+
+'''
+def is_prime(x):  # x = 12 -> [1 - 12]
+    if x <= 1:
+        return False
+    for j in range(2, x):  # [2,  11]
+        if x % j == 0:
+            return False
+    return True
+
+
+print(is_prime(7))  # True
+print(is_prime(1))  # False
+print(is_prime(8))  # False
+print([x for x in range(100) if is_prime(x)])
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+'''
+
+
+'''
+from itertools import  *
+def is_primary(x):
+    if x <= 1:
+        return False
+    for z in range(2, x):
+        if x % z == 0:
+            return False
+    return True
+
+
+R = []
+
+for n in range(0, 10 ** 10):
+    for p in permutations(['2' * 39, '0' * 39, '1' * n ]):
+        s = '>' + ''.join(p)
+        while '>1' in s or '>2' in s or '>0' in s:
+            if '>1' in s:
+                s = s.replace('>1', '22>', 1)
+            if '>2' in s:
+                s = s.replace('>2', '2>', 1)
+            if '>0' in s:
+                s = s.replace('>0', '1>', 1)
+        summa = sum([int(x) for x in s if x.isdigit()])
+        if is_primary(summa) == True:
+            print(n)
+            exit()
+'''
+
+
+'''
+def is_prime(x):
+    if x <= 1:
+        return False
+    for j in range(2, x):
+        if x % j == 0:
+            return False
+    return True
+
+
+for n in range(1, 1000):
+    s = '>' + '0' * 39 + '1' * n + '2' * 39
+    while '>1' in s or '>2' in s or '>0' in s:
+        if '>1' in s:
+            s = s.replace('>1', '22>', 1)
+        if '>2' in s:
+            s = s.replace('>2', '2>', 1)
+        if '>0' in s:
+            s = s.replace('>0', '1>', 1)
+    summa = sum([int(x) for x in s if x.isdigit()])
+    if is_prime(summa) == True:
+        print(n)
+        break
+'''
+
 
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-# i  01234
-s = 'abcde'
-
-print(f'Первый элемент строки s: {s[0]}')
-print(f'Последний элемент строки s: {s[-1]}')
-
-# Срезы строк
-'''
-print(s[2:4])  # cd
-print(s[:4])  # abcd
-print(s[2:])  # cde
-print(s[0::2])  # ace
-print(s[1::2])  # bd
-print(s[::-1])  # edcba
-print(s[1:-1])  # bcd - все элементы кроме первого и последнего 
-'''
-
-
-# Функции строк:
-'''
-s = '122233345'
-print(len(s))   # 9 - Возвращает длину строки
-print(min(s), max(s))  # 1 5
-print(max('aAbB'))  # a
-print(sorted(s))  # ['1', '2', '3', '4', '5']
-print(sorted(s, reverse=True))  # ['1', '2', '3', '4', '5']
-print(set(s))  # {'3', '4', '2', '5', '1'}
-print(eval('((2+2) * 5) / 2'))  # 10.0 - решает арифметические примеры
-'''
-
-
-# Методы строк
-'''
-s = 'jiUHkjikIHiu'
-print(s.lower())  # jiuhkjikihiu
-print(s.upper())  # JIUHKJIKIHIU
-
-s = '1231253213213'
-s = s.replace('2', '*')  # Заменить все '2' на '*' разом
-print(s)  # 1*31*53*13*13
-
-s = s.replace('*', '+', 3)  # Заменить только первые три '*' на '+'
-print(s)  # 1+31+53+13*13
-'''
-
-'''
-id = '45.87.234.255'
-print(id.split('.'))  # ['45', '87', '234', '255'] - Разбить строку на список по символу '.'
-print(id.split('4'))  # ['', '5.87.23', '.255']
-
-s = '34 57  897  34    123'
-print(s.split(' '))  # ['34', '57', '', '897', '', '34', '', '', '', '123']
-print(s.split())  # ['34', '57', '897', '34', '123']
-
-M = ['34', '57', '897', '34', '123']
-new_s = ''.join(M)
-print(new_s)  # 345789734123
-
-new_s = ','.join(M)
-print(new_s)  # 34,57,897,34,123
-
-new_s = '***'.join(M)
-print(new_s)  # 34***57***897***34***123
-'''
-
-
-# Работа с циклами
-'''
-for i in range(len(s)):
-    # print(i, end=' ')  # 0 1 2 3 4
-    print(s[i], end=' ')  # a b c d e
-print()
-
-for x in s:
-    print(x, end=' ')  # a b c d e
-print()
-'''
-
-
-# Задание 12 https://education.yandex.ru/ege/task/0a80339b-a1c9-4b63-8e60-313bc5ef8710
-# ПОКА нашлось(0) ИЛИ нашлось(01)
-#    ЕСЛИ нашлось(01)
-#       ТО заменить(01, 10)
-#    ИНАЧЕ заменить(0, 111)
-'''
-s = '0' + '1' * 45
-while '0' in s or '01' in s:
-    if '01' in s:
-        s = s.replace('01', '10', 1)
-    else:
-        s = s.replace('0', '111', 1)
-print(s)  # 111111111111111111111111111111111111111111111111
-print(s.count('1'))  # 48
-'''
-
-
-'''
-for n in range(1, 1000):
-    s = '3' * 15 + '2' * 18 + '1' * n
-
-    while '31' in s or '33' in s or '21' in s:
-        if '31' in s:
-            s = s.replace('31', '123', 1)
-        if '33' in s:
-            s = s.replace('33', '211', 1)
-        if '21' in s:
-            s = s.replace('21', '1', 1)
-    summa = sum([int(x) for x in s])
-    if summa > 24:
-        print(n)
-        break
-'''
-
-# https://education.yandex.ru/ege/task/56888dcf-39cf-4282-8bf7-5fec144e945c
-'''
-maxi = 0
-for n in range(4, 10000):
-    s = '3' + '5' * n
-
-    while '333' in s or '555' in s:
-        if '555' in s:
-            s = s.replace('555', '3', 1)
-        else:
-            s = s.replace('333', '5', 1)
-
-    summa = sum([int(x) for x in s])
-    if maxi < summa:
-        maxi = summa
-        print(maxi)
-'''
-
-
-# https://education.yandex.ru/ege/task/b3c1add0-9671-42f4-b405-20b7927c3234
-'''
-for n in range(10, 1000):
-    s = '3' + '5' * n
-
-    while '25' in s or '355' in s or '555' in s:
-        if '25' in s:
-            s = s.replace('25', '32', 1)
-        if '355' in s:
-            s = s.replace('355', '25', 1)
-        if '555'in s:
-            s = s.replace('555', '3', 1)
-    summa = sum([int(x) for x in s])
-    if summa % 25 == 0:
-        print(n)
-        break
-'''
 
 # endregion Урок: *************************************************************
 # #
