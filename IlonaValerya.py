eb1d21346b6b01cab150e23ff51c8aef33972205
Ilya Andrianov <ilandroxxy@gmail.com>
2025-01-21 15:46:40 +0700

    Занятия Понедельник `20Января2025`:
    - #Кирилл  Разбирали 3 номера на ВПР
    
    - #Тимур Разбирали самостоятельно теорию игр 19-21 на две кучи
    
    - #Валерия Добили разбор пробника, номера: 12, 13, 14, 25 и повторяли 8 номера
    
    - #Лиза Пропуск урока без особой уважительной причины (без переноса).
    
    - #Дмитрий Самостоятельно решили 19-21 сложный прототип и разобрали простейший прототип 22 номера.
    
    - #Артём Разбирали 23 номера
    
    - #Alexey Закрыли вопросы по домашнему варианту (Яндекс номер 4), задачи: 12, 13, 19-21, 24, 25, теорию игр решали через Excel на убывание

--- IlonaValerya.py
+++ IlonaValerya.py
@@ -6,71 +6,153 @@
 # region Урок: ********************************************************************
 
 '''
+for n in range(4, 1000):
+    s = '1' + '8' * n
+    while '18' in s or '388' in s or '888' in s:
+        if '18' in s:
+            s = s.replace('18', '8', 1)
+        if '388' in s:
+            s = s.replace('388', '81', 1)
+        if '888' in s:
+            s = s.replace('888', '3', 1)
+    if s.count('1') == 3:
+        print(n)
+'''
+
+'''
+from ipaddress import*
+for mask in range(33):
+    net = ip_network(f'213.168.83.190/{mask}', 0)
+    if '213.168.64.0' in str(net):
+        print(net.netmask, mask, 32-mask)
+        # 255.255.192.0 18
+        # 255.255.224.0 32-19 =
+'''
+
+'''
 alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
 
+for x in alphabet[:23]:
+    A = int(f'7{x}38596',23)
+    B = int(f'14{x}36',23)
+    C = int(f'61{x}7',23)
+    if (A + B + C)% 22 == 0:
+        print((A+B+C)//22)
+'''
+
+
+# № 10707 (Уровень: Средний)
+'''
 def convert(n, b):
     s = ''
     while n > 0:
-        s = s + alphabet[n % b]
+        s = s + str(n % b)
         n = n // b
     return s[::-1]
 
 
-for n in range(1,1000):
-    r = convert(n, 3)
+R = []
+for n in range(6, 1000):
+    r = convert(n, 6)
     if n % 3 == 0:
-        r = '1' + r + '02'
+        r += r[0] + r[1]  # r[:2]
     else:
-        x = (n % 3) * 4
-        r = r + convert(x, 3)
-    res = int(r, 3)
-    if res < 199:
-        print(n)
+        x = (n % 3) * 10
+        r = r + convert(x, 6)
+    res = int(r, 6)
+    if res > 680:
+        R.append(res)
+print(min(R))
+'''
+
+# Прототип 25 номера с масками
+'''
+from fnmatch import *
+for x in range(2025, 10**8, 2025):
+    if fnmatch(str(x), '12*34?5'):
+        print(x, x//2025)
 '''
 
 
-
+# № 18133 (Уровень: Базовый)
 '''
-import turtle as t
-t.left(90)
-t.tracer(0)
-size = 40
-
-t.right(90)
-for i in range(3):
-    t.right(45)
-    t.forward(10 * size)
-    t.right(45)
-t.right(315)
-t.forward(10 * size)
-for i in range(2):
-    t.right(90)
-    t.forward(10 * size)
-
-t.up()
-for x in range(-50,50):
-   for y in range(-50,50):
-        t.goto(x * size, y * size)
-        t.dot(2, 'red')
-t.update()
-t.done()
-'''
-
-'''
-s = '0123456789ABCDEF'
-cnt = 0
-s1 = s[1::2]  # s1 = '13579BDF'
-s2 = s[0::2]
+s = sorted('КОДИМ')
+n = 0
 for a in s:
     for b in s:
         for c in s:
-            num = a + b + c
-            if a != '0':
-                if len(set(num)) == len(num):
-                    if a in s1 and b in s2 and c in s1:
-                        cnt += 1
-                    if a in s2 and b in s1 and c in s2:
-                        cnt += 1
+            for d in s:
+                for e in s:
+                    word = a + b + c + d + e
+                    n += 1
+                    if word.count('М') == 2:
+                        if 'ММ' not in word:
+                            print(n)
+'''
+
+
+#
+# № 18042 (Уровень: Базовый)
+'''
+s = 'ЛЮСТРА'
+cnt = 0
+for a in s:
+    for b in s:
+        for c in s:
+            for d in s:
+                for e in s:
+                    word = a + b + c + d + e
+                    if word.count('Ю') <= 2:
+                        if word[-1] not in 'ЛСТР':
+                            cnt += 1
+print(cnt)
+'''
+
+
+# № 16255 Джобс 03.05.24 (Уровень: Средний)
+'''
+s = '012345678'
+cnt = 0
+for a in '02468':  # не начинаются с нечетных цифр
+    for b in s:
+        for c in s:
+            for d in s:
+                for e in s:
+                    for f in s:
+                        for g in s:
+                            word = a + b + c + d + e + f + g
+                            if int(word[-1]) % 3 != 0:
+                                if word[0] != '0':
+                                    if '6' in word:
+                                        cnt += 1
+print(cnt)
+'''
+
+
+'''
+s = '0123456789ABCD'
+cnt = 0
+for a in s:
+    for b in s:
+        for c in s:
+            for d in s:
+                for e in s:
+                    word = a + b + c + d + e
+                    if word[0] != '0':
+                        if len(set(word)) == 2:
+                            if word[-1] in '03':
+                                cnt += 1
+print(cnt)
+
+
+from itertools import *
+cnt = 0
+for p in product('0123456789ABCD', repeat=5):
+    word = ''.join(p)
+    if word[0] != '0':
+        if len(set(word)) == 2:
+            if word[-1] in '03':
+                cnt += 1
 print(cnt)
 '''
 
--- KirillLokotov.py
+++ KirillLokotov.py
@@ -1,70 +1,10 @@
 # region Домашка: ************************************************************
 
-# № 12797 (Уровень: Средний) 🌶
-'''
-cnt = 0
-for s in open('files/9.csv'):
-    M = [int(x) for x in s.split(',')]
-    if len(set(M)) == 3:
-        copied = [x for x in M if M.count(x) == 2]
-        not_copied = [x for x in M if M.count(x) == 1]
-        if all(p % 2 != 0 for p in not_copied):
-            if all(p % 2 == 0 for p in copied):
-                cnt += 1
-print(cnt)
-'''
-
-
-# and - Оба условия выполняются
-# or - Хотя бы одно условие выполняется
-# () != () - Только одно условие выполняется
-'''
-cnt = 0
-for s in open('files/9.csv'):
-    M = [int(x) for x in s.split(',')]
-    flag = 0
-    if len(set(M)) != 6:
-        flag += 1
-    nechet = [x for x in M if x % 2 != 0]
-    if len(nechet) == 3:
-        flag += 1
-    if flag == 1:
-        cnt += 1
-print(cnt)
-'''
-
-
-# № 12795 (Уровень: Средний)
-'''
-cnt = 0
-for s in open('files/9.csv'):
-    M = [int(x) for x in s.split(',')]
-    avg = sum(M) // 7
-    if any(x == avg for x in M):
-        cnt += 1
-print(cnt)
-'''
-# Ответ: 35
-
 
 # endregion Домашка: ************************************************************
 # #
 # #
 # region Урок: ************************************************************
-
-
-# № 19233 ЕГКР 21.12.24 (Уровень: Базовый)
-from itertools import *
-print('1 2 3 4 5 6 7 8')
-graph = 'BD DB BE EB BC CB CH HC HE EH HF FH FA AF FG GF AG GA GD DG DE ED'
-table = '12 13 14 21 25 27 31 34 37 41 43 48 52 56 58 65 68 72 73 84 85 86'
-for p in permutations('BCEHFAGD'):
-    # ('D', 'G', 'A', 'F', 'H', 'B', 'C', 'E')
-    new_table = table
-    for i in range(1, 8+1):
-        new_table = new_table.replace(str(i), p[i-1])
-    if set(new_table.split()) == set(graph.split()):
-        print(*p)
 
 
 # endregion Урок: ************************************************************
@@ -76,6 +16,6 @@ for p in permutations('BCEHFAGD'):
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = [1, 2, 5, 6, 8, 9, 12, 13, 14, 15, 16, 17, 23, 25]
+# ФИПИ = [1, 2, 3, 5, 6, 8, 9, 12, 13, 14, 15, 16, 17, 23, 25]
 # КЕГЭ = []
 # на следующем уроке:
