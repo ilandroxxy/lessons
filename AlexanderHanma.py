33d2d4ad58e1c3d9470a8acd06dea8a991d71fd4
ilandroxxy <ilandroxxy@gmail.com>
2025-04-20 19:12:23 +0700

    Занятия Воскресенье `20Апрель2025`:
    - #Женя #Ярослав Разобрали 3 номер
    
    - #Dmitry Разобрали 22 номер
    
    - #Александр Повторили 24 и 9 номера

--- AlexanderHanma.py
+++ AlexanderHanma.py
@@ -6,171 +6,64 @@
 # #
 # region Урок: ********************************************************************
 
+# № 21412 Досрочная волна 2025 (Уровень: Базовый)
 '''
-from sys import *
-setrecursionlimit(10000)
-from functools import *
+from ipaddress import *
+net = ip_network('143.168.72.213/255.255.255.240', 0)
+for ip in net:
+    print(ip)
 
-@lru_cache(None)
-def F(n):
-    if n<6:
-        return n
-    if n>= 6:
-        return (3*n -2) * F(n-5)
-
-for i in range(1, 21000):
-    F(i)
-
-print((F(20568) - 51702 * F(20563))//F(20553))
+# 143.168.72.222
+# 14316872222
 '''
 
 
-# № 4740 (Уровень: Средний)
+# № 20909 Апробация 05.03.25 (Уровень: Средний)
 '''
-from math import factorial
-from sys import *
-from functools import *
-
-setrecursionlimit(1000000)
-
-
-@lru_cache(None)
-def F(n):
-    if n >= 5000:
-        return factorial(n)
-    if 1 <= n < 5000:
-        return 2 * F(n + 1) // (n + 1)
-
-
-for i in range(5500, 1, -1):
-    F(i)
-
-print(1000 * F(7) // F(4))
-'''
-
-# todo посмотреть задачу Александры № 18536 Сергей Горбачев
-'''
-from re import *
-s= open('0. files/24.txt').readline()
-s = s.replace('*', '-').replace('6', '7').replace('7', '9').replace('9', '8')
-print(s)
-num = r'[8][08]*|[0]'
-reg = rf'{num}([-]{num})*'
-
-M = [x.group() for x in finditer(reg, s)]
+s = open('0. files/24.txt').readline()
+s = s.split('AB')
 maxi = 0
-for x in M:
-    if len(x) == 108:
-        print(x)
-    maxi = max(maxi, len(x))
+for i in range(len(s)-100):
+    r = 'B' + 'AB'.join(s[i:i+101]) + 'A'
+    maxi = max(maxi, len(r))
 print(maxi)
 '''
 
+
+
 '''
-def f(n, b):
-    res = ''
-    alp = sorted('012345678')
-    while n > 0:
-        res += alp[n % b]
-        n //= b
-    return res[::-1]  # тут return съехал
-
-
-R = []
-for n in range(0, 1000):
-    s = f(n, 9)
-    b = sum(map(int, s))  # тут сумма цифр
-    b = sum(int(x) for x in s)
-    if b % 2 == 0:
-        s = s + '52'
-    else:
-        s = '73' + s[2:] + '44'  # тут кривой срез
-    r = int(s, 9)  # тут 9
-    if r > 13950:
-        R.append(n)
-print(min(R))
+s=open('0. files/24.txt').readline()
+s=s.split('WWF')
+maxi=0
+for i in range(len(s)-120):
+    e='WF'+'WWF'.join(s[i:i+121])+'FW'
+    e = e.replace('WSFWW', 'WSFW SFWW').split()
+    for x in e:
+        maxi=max(maxi,len(x))
+print(maxi)
 '''
 
-
-# a = 5, b = 155
-
-# a = 155
-# a = 160
-# a = 200  a = 40
+# https://education.yandex.ru/ege/task/0f1bb8de-fe34-40c1-a82e-8d012e8519dc
 '''
-def f(a, b, c):
-    if a > b or a == 40:
-        return 0
-    elif a == b:
-        return 1 and 'BB' not in c
-    else:
-        return f(a + 1, b, c+'A') + f(a * 2, b, c+'B') + f(a ** 2, b, c+'C')
-
-
-print(f(5, 80, '') * f(80, 155, ''))
-
-
-def f(a, b, c):
-    if a >= b or a == 40:
-        return a == b and 'BB' not in c
-    else:
-        return f(a + 1, b, c+'A') + f(a * 2, b, c+'B') + f(a ** 2, b, c+'C')
-
-
-print(f(5, 80, '') * f(80, 155, ''))
+cnt=0
+for s in open('0. files/9.csv'):
+    M=sorted([int(x)for x in s.split(';')])
+    if (M[-1]+M[0])**2>(M[1]**2+M[2]**2+M[3]**2):
+        cnt+=1
+print(cnt)
 '''
 
 
-# № 17939 Сергей Горбачев
-'''
-M = [int(x) for x in open('0. files/17.txt')]
-D = [x for x in M if len(str(abs(x))) == 4]
-T = [x for x in M if x < 0 and abs(x) % 5 == 0]
+cnt=0
+for s in open('0. files/9.csv'):
+    M=sorted([int(x)for x in s.split(';')])
+    povt=[x for x in M if M.count(x)==3]
+    nepov=[x for x in M if M.count(x)==1]
+    if len(povt) == 3 and len(nepov) == 3:
+        if ((povt[0]**2)*3 > nepov[0]**2 + nepov[1]**2 + nepov[2]**2):
+            cnt+=1
+print(cnt)
 
-R = []
-for i in range(len(M) - 2):
-    x, z, c = M[i:i + 3]
-    if (x in D) + (z in D) + (c in D) >= 2:
-        if (x + z + c) >= len(T):
-            R.append(x + z + c)
-print(len(R), min(R))
-'''
-
-
-# № 18322 Сергей Горбачев
-'''
-def F(x, a1, a2):
-    P = 32 <= x <= 55
-    Q = 40 <= x <= 79
-    A = a1 <= x <= a2
-    return ((Q) <= (not A)) <= ((not A) == (not P))
-
-
-R = []
-M = [x / 4 for x in range(20 * 4, 90 * 4)]
-for a1 in M:
-    for a2 in M:
-        if all(F(x, a1, a2) for x in M):
-            R.append(a2 - a1)
-print(min(R))
-'''
-
-'''
-def f(x, a1, a2):
-    D = 7 <= x <= 68
-    C = 29 <= x <= 100
-    A = a1 <= x <= a2
-    return (D) <= (((not C) and (not A)) <= (not D))
-
-
-R = []
-E = [x / 4 for x in range(2 * 4, 110 * 4)]
-for a1 in E:
-    for a2 in E:
-        if all(f(x, a1, a2) for x in E):
-            R.append(a2 - a1)
-print(min(R))
-'''
 
 # endregion Урок: ********************************************************************
 # #
--- DmitryChixxxol.py
+++ DmitryChixxxol.py
@@ -6,104 +6,22 @@
 # #
 # region Урок: ********************************************************************
 
-# № 20144
+# № 20962 (Уровень: Базовый)
 '''
-def divisors(x):
-    div = []
-    for j in range(2, int(x ** 0.5) + 1):  # не считая самого числа
-        if x % j == 0:
-            div += [j, x // j]
-    return sorted(set(div))
+from functools import *
 
-# print([x for x in range(2, 100) if len(divisors(x)) == 0])
+@lru_cache(None)
+def F(n):
+    if n < 6:
+        return n
+    if n >= 6:
+        return (3*n - 2) * F(n - 5)
 
-cnt = 0
-for x in range(3_333_337+1, 10**10):
-    d = [j for j in divisors(x) if len(divisors(j)) == 0]
-    if len(d) > 0:
-        R = max(d) - min(d)
-        if R > 1000 and R % 3 == 0:
-            print(x, R)
-            cnt += 1
-            if cnt == 5:
-                break
+for n in range(1, 21000):
+    F(n)
+
+print((F(20568) - 51702 * F(20563)) // F(20553))
 '''
-
-
-# 20068
-'''
-print(25 * 34 + 25 * 17 - 13 * 17)
-
-import turtle as t
-t.screensize(5000, 5000)
-t.tracer(0)
-l = 30
-
-for x in range(777):
-    t.forward(25 * l)
-    t.left(90)
-    t.forward(34 * l)
-    t.left(90)
-t.up()
-t.forward(12 * l)
-t.left(90)
-t.forward(17 * l)
-t.right(90)
-t.down()
-for x in range(1996):
-    t.forward(25 * l)
-    t.left(90)
-    t.forward(17 * l)
-    t.left(90)
-
-t.up()
-for x in range(-50, 50):
-    for y in range(-50, 50):
-        t.goto(x * l, y * l)
-        t.dot(3, 'red')
-t.update()
-t.done()
-
-'''
-
-
-# 19895
-'''
-# sym - ?
-alp = 10 + 2040
-print(2**12)
-i = 12
-
-byte = 369 * 2**10 / 718
-print(byte)  # 526.261
-byte = 526
-
-bit = byte * 8
-
-sym = bit / i
-print(sym)  # 350.66
-'''
-
-
-# 19892
-'''
-pixels = 1920 * 1080
-i = 16  # 2 ** 16 >= colors
-bit = pixels * i  # бит на одну фотографию
-print(512 - 52)  # 460
-
-for i in range(1, 15):
-    if 460 % i == 0:
-        print(i, 460 // i)
-        # 5 92
-
-
-print((92 * bit) / 2**23)  # 363.86
-'''
-# Ответ: 364
-
-
-
 # endregion Урок: *************************************************************
 # #
 # #
@@ -113,7 +31,7 @@ print((92 * bit) / 2**23)  # 363.86
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19-21, 23, 25]
+# ФИПИ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19-21, 23, 25]
 # КЕГЭ  = [23]
 # на следующем уроке:
 
