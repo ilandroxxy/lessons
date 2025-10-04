33cd77d4dd6a94c2ad8166f61e1840b99ae6d095
ilandroxxy <ilandroxxy@gmail.com>
2025-10-04 08:59:50 +0700

    Занятия Четверг `02Октября2025`:
    - #Дамир Провели урок по типа данных и базовой арифметике
    
    - #Дмитрий #Федор Разбирали 5 номера
    
    - #Иван #Egor Разбирали 13 номера
    
    - #РоманСергеевич #РоманВладимирович Разбирали 8 номера все прототипы
    
    - #Ярик Проговорили теорию циклов
    
    - #Артемий Проговорили теорию циклов

--- ArtemyGleb.py
+++ ArtemyGleb.py
@@ -1,44 +1,138 @@
 # region Домашка: ******************************************************************
 
-# Сочетание клавиш ctrl + alt + L - приводит код в соответствие PEP8
-'''
-a = int(input())
-b = int(input())
-print(a // b)
-print(a % b)
-'''
-from runpy import run_path
 
-# https://stepik.org/lesson/1309430/step/8?unit=1324546
-'''
-print(123 // 10)  # 12
-print(123 % 10)  # 3
-
-n = int(input())  # 48395
-a = n // 10000  # 4
-b = (n // 1000) % 10
-c = (n % 1000) // 100
-d = (n % 100) // 10
-e = n % 10  # 5
-print(a * b * c * d * e)
-print(a + b + c + d + e)
-'''
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-a = int(input())
-b = int(input())
-c = int(input())
-if a == b == c:
-    print("Равносторонний")
-elif (a == b) or (a == c) or (c == b):
-    print("Равнобедренный")
-else:
-    print("Разносторонний")
+
+# Цикл for отвечает на запросы: "повтори N раз", "пробеги от А до В"
+
+# Работы через функцию range()
+'''
+# range(0, STOP-1, 1)    range(10)
+# range(START, STOP-1, 1)    range(2, 10)
+# range(START, STOP-1, STEP)    range(2, 10, 2)
+
+for i in range(10):
+    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
+print()
+
+for i in range(2, 10):
+    print(i, end=' ')  # 2 3 4 5 6 7 8 9
+print()
+
+for i in range(2, 10, 2):
+    print(i, end=' ')  # 2 4 6 8
+print()
+
+for i in range(2, 10+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
+print()
+
+for i in range(10, 1, -1):
+    print(i, end=' ')
+print()
+'''
 
 
+# Работы напрямую через последовательность
+'''
+# i   0    1    2    3    4
+L = ['a', 'b', 'c', 'd', 'e']
+
+for x in L:
+    print(x, end=' ')  # a b c d e
+print()
+
+for x in L:
+    if x in 'ae':
+        print(x, end=' ')  # a e
+print()
+
+
+print(len(L))  # 5 - Кол-во элементов в списке L
+
+for i in range(len(L)):
+    # print(i, end=' ')  # 0 1 2 3 4
+    print(L[i], end=' ')  # a b c d e
+print()
+
+for i in range(len(L)):
+    L[i] = L[i] * i
+print(L)  # ['', 'b', 'cc', 'ddd', 'eeee']
+'''
+
+
+# Цикл while отвечает на запросы: "пока условие верное - делаем действие", "бесконечный цикл"
+'''
+for i in range(2, 10+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
+print()
+
+
+i = 2
+while i <= 10:
+    print(i, end=' ')  # 2 4 6 8 10
+    i += 2
+print()
+'''
+
+
+# Перевод из 10-й в b-ю систему
+'''
+n = 8
+b = 2
+r = ''
+while n > 0:
+    r = str(n % b) + r
+    n //= b
+print(r)
+'''
+
+'''
+from string import *
+alp = digits + ascii_uppercase
+print(alp) # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(alp[:2]) # 01
+print(alp[:8]) # 01234567
+print(alp[:16]) # 0123456789ABCDEF
+
+# alp = '0123456789ABCDE....'
+
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
+
+print(convert(10000, 2))  # 10011100010000
+print(convert(10000, 8))  # 23420
+print(convert(10000, 16))  # 2710
+print(convert(10000, 24))  # H8G
+'''
+
+
+# Бесконечные циклы и операторы: break, continue, exit()
+'''
+k = 0
+while True:
+    k += 1
+    if k % 2 != 0:
+        continue  # Прерывает итерацию (шаг) цикла
+    if k == 50_000:
+        break  # Прерывает цикл в котором мы лежим
+    if k == 100_000:
+        exit()  # Прерывает выполнение сразу всей программы
+    print(k)
+
+
+print('Продолжение программы')
+'''
 
 # endregion Урок: *************************************************************
 # #
--- FedorDmitry.py
+++ FedorDmitry.py
@@ -7,54 +7,207 @@
 # region Урок: ********************************************************************
 
 
-# № 23739 Демоверсия 2026 (Уровень: Базовый)
+
+
+# Пример работы f-строки
 '''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = (x or y) and (not(y == z)) and (not w)
-                if F == 1:
-                    print(x, y, z, w)
+name = input('Введите свое имя: ')
+print('Имя пользователя:', name)
+print(f'Имя пользователя: {name}')
+'''
+from pydoc import apropos
+
+"""
+''' Привет, Илья! Сегодня облачно, а температура 24 градуса! '''
+
+from random import choice
+name = input('Введите свое имя: ')
+weather = choice(['облачно', 'мрачно', 'солнечно'])
+print('Сегодня погода:', weather)
+temperature = int(input('Введите температуру: '))
+
+print('Привет, ', name, '! Сегодня ', weather, ', а температура ', temperature, ' градуса!')
+print('Привет, ' + name + '! Сегодня ' + weather + ', а температура ' + str(temperature) + ' градуса!')
+print('Привет, {}! Сегодня {}, а температура {} градуса!'.format(name, weather, temperature))
+print(f'Привет, {name}! Сегодня {weather}, а температура {temperature} градуса!')
+"""
+
+
+# Функции перевода в различные системы счисления
+'''
+# Перевод в двоичную запись
+n = 8
+print(bin(n)[2:])  # 1000
+print(f'{n:b}')  # 1000
+print(int('1000', 2))  # 8 - Перевод из 2-й в 10-ю
+
+# Перевод в восьмеричную запись
+n = 8
+print(oct(n)[2:])  # 10
+print(f'{n:o}')  # 10
+print(int('10', 8))  # 8 - Перевод из 8-й в 10-ю
+
+# Перевод в шестнадцатеричная запись
+n = 10**8
+print(hex(n)[2:])  # 5f5e100
+print(f'{n:x}')  # 5f5e100
+print(f'{n:X}')  # 5F5E100
+print(int('5F5E100', 16))  # 10**8 - Перевод из 16-й в 10-ю
+# ValueError: int() base must be >= 2 and <= 36, or 0
+'''
+
+# Своя функция перевода из 10-й в b-ю систему
+'''
+from string import digits, ascii_uppercase
+alp = digits + ascii_uppercase
+
+# alp = '0123456789ABCDE...'
+
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+print(alp[:2])  # ['0', '1']
+print(alp[:8])  # ['0', '1', '2', '3', '4', '5', '6', '7']
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b  # n = n // b
+    return r
+
+print(convert(8, 2))  # 1000
+print(convert(8, 3))  # 22
+print(convert(10**8, 16))  # 5F5E100
 '''
 
 
-# № 13797 (Уровень: Базовый)
+# № 23364 Резервный день 19.06.25 (Уровень: Базовый)
 '''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = ((not x) and (z <= y) and (not w)) or ((z == w) and (x or y <= w))
-                if F == 1:
-                    print(x, y, z, w)
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
+
+RES = []
+for n in range(1, 10000):
+    s = convert(n, 3)
+    if n % 3 == 0:
+        s = '1' + s + '02'
+    else:
+        x = (n % 3) * 4
+        s = s + convert(x, 3)
+    r = int(s, 3)
+    if r < 100:
+        RES.append(n)
+print(max(RES))
 '''
 
-# № 13798 (Уровень: Базовый)
+
+# № 23742 Демоверсия 2026 (Уровень: Базовый)
 '''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = (x or ((not z) and w) or w) == (y and (not x) and w)
-                if F == 1:
-                    print(x, y, z, w)
+RES = []
+for n in range(1, 10000):
+    # s = convert(n, 2)
+    # s = bin(n)[2:]
+    s = f'{n:b}'
+    if n % 3 == 0:
+        s = s + s[-3:]
+    else:
+        x = (n % 3) * 3
+        s = s + f'{x:b}'
+    r = int(s, 2)
+    if r >= 200:
+        RES.append(n)
+print(min(RES))
+'''
+
+'''
+a, b = 7, 2
+a, b = b, a
+print(a, b)  # 2 7
+
+# Замена через третью переменную
+# 2 -> 3, 3 -> 2
+s = '1231232313212323'  
+s = s.replace('2', '*')
+print(s)  # 1*31*3*313*1*3*3
+s = s.replace('3', '2')
+print(s)  # 1*21*2*212*1*2*2
+s = s.replace('*', '3')
+print(s)  # 1321323212313232
 '''
 
 
-# № 12234 ЕГКР 16.12.23 (Уровень: Базовый)
+# № 22272 (Уровень: Средний)
 '''
-print('x y z w')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = y and (x or z) or (not(y or z)) or w
-                if F == 0:
-                    print(x, y, z, w)
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
+
+RES = []
+for n in range(1, 10000):
+    s = convert(n, 9)
+    if s[0] == '7':
+        s = s.replace('6', '*')
+        s = s.replace('3', '6')
+        s = s.replace('*', '3')
+        s = s + '34'
+    else:
+        # s = '3' + s + '45'  # дописать
+        s = '3' + s[1:] + '45'  # заменить
+    r = int(s, 9)
+    if r < 2876:  # 2795
+        RES.append([r, n])
+print(max(RES))  # [2795, 79]
+'''
+
+
+# № 19237 ЕГКР 21.12.24 (Уровень: Базовый)
+'''
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
+
+RES = []
+for n in range(1, 10000):
+    s = convert(n, 3)
+    if n % 3 == 0:
+        s = s + s[-2:]
+    else:
+        summa = s.count('1') + s.count('2') * 2
+        s = s + convert(summa, 3)
+    r = int(s, 3)
+    if r > 220 and r % 2 == 0:
+        RES.append(r)
+print(min(RES))
+'''
+
+
+# Вычисляется сумма цифр полученной троичной записи
+'''
+s = '1298712349847'
+
+summa = 0
+for x in s:
+    summa += int(x)
+open(summa)
+
+summa = sum([int(x) for x in s])
+
+summa = sum(map(int, s))
 '''
 
 
@@ -67,6 +220,6 @@ for x in 0, 1:
 # endregion Разобрать: *************************************************************
 # #
 # #
-# ФИПИ = [2]
+# ФИПИ = [2, 5]
 # КЕГЭ = []
 # на следующем уроке:
