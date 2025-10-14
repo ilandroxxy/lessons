dec850ed2ed039b25a39df4a8cb939e46996f4c7
ilandroxxy <ilandroxxy@gmail.com>
2025-10-15 01:45:22 +0700

    Занятия Вторник `14Октября2025`:
    - #Вадим #Арсений Разбирали 14 номера
    
    - #Женя #Кирилл #Дима Разбирали 15 номера
    
    - #Дамир #Никита Разбирали теорию циклов for и while
    
    - #Нестор #Вероника Разбирали 15 номера
    
    - #РоманСергеевич #РоманВладимирович Разбирали 15 номера
    
    - #Ярик #Савелий Разбирали 14 номера

--- DamirNikita.py
+++ DamirNikita.py
@@ -6,272 +6,217 @@
 # #
 # region Урок: ********************************************************************
 
-# Поговорить про библиотеки Python
+
+# Цикл for отвечает на запросы: "повтори N раз", "пробежать от А до В"
+
+# Цикл for с функцией range()
 '''
-# ctrl + B - открывает документацию к фунциям
+# range(0, STOP-1, 1)
+# range(START, STOP-1, 1)
+# range(START, STOP-1, STEP)
 
-from math import sqrt
-print(f'Взять квадратный корень от числа 16: {sqrt(16)}')
+for i in range(10):
+    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
+print()
 
-print(f'Взять квадратный корень от числа 16: {16 ** (1/2)}')
+for i in range(2, 10):
+    print(i, end=' ')  # 2 3 4 5 6 7 8 9
+print()
 
-from Useful import divisors
-'''
+for i in range(2, 10, 2):
+    print(i, end=' ')  # 2 4 6 8
+print()
 
+for i in range(3, 10, 3):
+    print(i, end=' ')  # 3 6 9
+print()
 
-# Способы подключения библиотек
-'''
-import math  # Это самый простой способ подключения 
-print(math.sqrt(16))
+n = 10
+for i in range(2, n+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
+print()
 
-import math as m  # Переименовываем библиотеку через любое короткое имя 
-print(m.sqrt(16))
-
-from math import sqrt, factorial  # Подключаем конкретные функции 
-print(sqrt(16))
-
-from math import *  # Подключение сразу всего содержимого 
-print(sqrt(16))
-print(factorial(5))
-'''
-
-# Пример конфликта имен:
-'''
-count = 0
-from itertools import permutations
-for p in permutations('abc'):
-    count += 1
-    print(count, p)
-    # 1 ('a', 'b', 'c')
-    # 2 ('a', 'c', 'b')
-    # 3 ('b', 'a', 'c')
-    # 4 ('b', 'c', 'a')
-    # 5 ('c', 'a', 'b')
-    # 6 ('c', 'b', 'a')
-
-count = 0
-from itertools import *
-for p in permutations('abc'):
-    count += 1
-    print(count, p)
-# TypeError: unsupported operand type(s) for +=: 'type' and 'int'
+for i in range(10, 0, -1):
+    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1
+print()
 '''
 
-
-# Список полезных библиотек для успешной сдачи ЕГЭ по информатике!
-
-# 1⃣ Библиотека черепашки для решения 6 номера кодом:
+# Цикл for с последовательностями
 '''
-import turtle as t
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
+print(len(M))  # 5
 
-t.tracer(0)
+for x in M:
+    print(x, end=' ')  # a b c d e
+print()
 
-t.fd(10)  # t.bk(10)
-t.rt(90)  # t.lt(90)
+for x in M:
+    if x in 'ae':
+        print(x, end=' ')  # a e
+print()
 
-t.up()
-t.down()
+for i in range(len(M)):
+    # print(i, end=' ')  # 0 1 2 3 4
+    print(M[i], end=' ')  # a b c d e
+print()
 
-x, y = 0, 0
-t.goto(x, y)
-t.dot(2, 'red')
-
-t.done()
-'''
-
-# 2⃣ Библиотека itertools для решения 1, 8, 9, 17, 24 номеров кодом:
-'''
-from itertools import product, permutations
-
-perms = list(product('abc', repeat=2))
-for perm in perms:
-    print(perm)
-    # ('a', 'a')
-    # ('a', 'b')
-    # ('a', 'c')
-    # ('b', 'a')
-    # ('b', 'b')
-    # ('b', 'c')
-    # ('c', 'a')
-    # ('c', 'b')
-    # ('c', 'c')
-
-perms = list(permutations("abc", r=3))
-for perm in perms:
-    print(perm)
-    # ('a', 'b', 'c')
-    # ('a', 'c', 'b')
-    # ('b', 'a', 'c')
-    # ('b', 'c', 'a')
-    # ('c', 'a', 'b')
-    # ('c', 'b', 'a')
-'''
-
-# 3⃣ Библиотека ipaddress для решения нового 13 номера:
-'''
-from ipaddress import *
-net = ip_network('узла/маска', 0)
-print(net, net.netmask, net.num_addresses)
-'''
-
-# 4⃣ Две библиотеки для решения 16 номера:
-# Одна увеличивает глубину рекурсии:
-'''
-import sys
-sys.setrecursionlimit(10000)
-'''
-# Вторая ускоряет вычисления через использование кэширования:
-'''
-from functools import *
-@lru_cache(None)
-def F(n):
-'''
-
-# 5⃣ Библиотеки fnmatch и re для решения 25 номера с масками и 24 номера с регулярными выражениями:
-
-# № 23764 Демоверсия 2026 (Уровень: Базовый)
-# Назовём маской числа последовательность цифр, в которой также могут встречаться следующие символы:
-# – символ «?» означает ровно одну произвольную цифру;
-# – символ «*» означает любую последовательность цифр произвольной длины;
-# в том числе «*» может задавать и пустую последовательность.
-
-# Среди натуральных чисел, не превышающих 10**10, найдите все числа,
-# соответствующие маске 3?12?14*5,
-# делящиеся на 1917 без остатка.
-'''
-# Вариант 1
-from fnmatch import *
-for x in range(1917, 10**10, 1917):
-    if fnmatch(str(x), '3?12?14*5'):
-        print(x, x // 1917)
-
-# Вариант 2
-from re import *
-for x in range(1917, 10**10, 1917):
-    if fullmatch('3[0-9]12[0-9]14[0-9]*5', str(x)):
-        print(x, x // 1917)
-'''
-
-# 6⃣ Библиотека string хранит в себе много полезных элементов:
-'''
-import string
-alphabet = string.ascii_uppercase
-print(alphabet)  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-print(string.punctuation)
-# !"#$%&'()*+,-./:;<=>?@]^_`{|}~
-
-alp36 = string.digits + string.ascii_uppercase
-# 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-print(alp36[:2])  # 01
-print(alp36[:8])  # 01234567
-print(alp36[:16])  # 0123456789ABCDEF
-'''
-
-# 7⃣ Библиотека math хранит в себе много полезных математических функций:
-'''
-from math import floor, ceil, dist
-
-print(4 / 3)  # 1.333(3)
-print(round(4 / 3))  # 1 - округление по математике
-print(ceil(4 / 3))  # 2 - округление строго вверх
-print(floor(4 / 3))  # 1 - округление строго вниз
-
-def d(A, B):
-    x1, y1 = A
-    x2, y2 = B
-    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-
-print(d([4, 5], [6, 7]))  # 2.82842
-print(dist([4, 5], [6, 7]))  # 2.82842
+for i in range(len(M)):
+    M[i] = M[i] * i
+print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
 '''
 
 
-# Условные операторы (ветвление): if, elif, else
-'''
-n = int(input('n: '))
-if n > 0:  # if - если 
-    print('Положительное')
-elif n < 0:  # elif - иначе если 
-    print('Отрицательное')
-else:  # else - иначе 
-    print('Равно нулю')
-'''
+# Цикл while отвечает на запросы: "пока условие верное - делаем действие", "бесконечный цикл"
 
-# x = int(input('x: '))
-# y = int(input('y: '))
 '''
-x, y = 5, 6
-if x > 0 and y > 0:
-    print('Первая четверть')
-elif x < 0 and y > 0:
-    print('Вторая четверть')
-elif x < 0 and y < 0:
-    print('Третья четверть')
-elif x > 0 and y < 0:
-    print('Четвертая четверть')
-else:
-    print('Лежит на осях')
+n = 10
+for i in range(2, n+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
+print()
 
-print('Продолжение программы ')
+
+i = 2
+while i <= 10:
+    print(i, end=' ')  # 2 4 6 8 10
+    i += 2  # i = i + 2
+print()
 '''
 
-
-# Открытие и считывание файла для 17 номера
 '''
-clustersA = [[], []]
-# clustersB = [[], [], []]
+n = 8
+b = 2
+r = ''
+while n > 0:
+    r = str(n % b) + r
+    n //= b  # n = n // b
+print(r)  # 1000
+
+b = 2
+print(int('1000', b))  # 8
+'''
+
+# Универсальная функция перевода в различные системы счисления
+'''
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
 
 
-for s in open('0. files/27_A.txt'):
-    s = s.replace(',', '.')
-    x, y = [float(i) for i in s.split()]
+print(convert(8, 2))  # 1000
+print(convert(8, 3))  # 22
+print(convert(10**8, 16))  # 5F5E100
 
-    if y > 10:
-        clustersA[0].append([x, y])
+
+print(int('1000', 2))  # 8
+# ValueError: int() base must be >= 2 and <= 36, or 0
+'''
+
+
+# Бесконечный цикл и операторы: break, continue, exit()
+'''
+k = 0
+while True:
+     k += 1
+     if k % 2 != 0:
+         continue  # Прерывает шаг итерации цикла 
+     if k == 50_000:
+         break  # Прерывает выполнение цикла в котором лежит
+     if k == 25_000:
+         exit()  # Прерывает полностью выполнение программы
+     print(k)
+
+print('Продолжение программы')
+'''
+
+
+# Простая консольная программа по переводу в различные системы счисления
+'''
+from string import digits, ascii_uppercase
+alp = digits + ascii_uppercase
+# '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
+
+
+while True:
+    case = input('\n'
+                 'case 1: Перевод из 10-й в b-ю систему \n'
+                 'case 2: Перевод из b-й в 10-ю систему \n'
+                 'case 3: Перевод из b-й в k-ю систему \n'
+                 'case 0: Выход из программы. \n'
+                 'case: ')
+
+    if case == '1':
+        n = int(input('Введите 10-е число для перевода: '))
+        b = int(input('Введите b-ю систему счисления: '))
+        r = convert(n, b)
+        print(f'Результат перевода числа {n} в {b}-ю систему: {r}')
+
+    elif case == '2':
+        b = int(input('Введите b-ю систему счисления: '))
+        r = input(f'Введите число в {b}-й системе: ')
+        n = int(r, b)
+        print(f'Результат перевода числа {r} из {b}-й системы: {n}')
+
+
+    elif case == '3':
+        b = int(input('Введите b-ю систему счисления: '))
+        r = input(f'Введите число в {b}-й системе: ')
+        k = int(input('Введите k-ю систему счисления: '))
+        n = int(r, b)
+        new_r = convert(n, k)
+        print(f'Результат перевода числа {r} из {b}-й системы в {k}-ю: {new_r}')
+
+
+    elif case == '0':
+        print('Спасибо, что пользовались нашим калькулятором!')
+        break
+
     else:
-        clustersA[1].append([x, y])
-
-for s in clustersA[1]:
-    print(s)
+        print('Я понимаю только команды 1, 2, 3, 0.')
 '''
 
 
-# Логические связки: and, or, not, in
-'''
-flag = True
-print(not flag)  # False
-print(not(not flag))  # True
+# Проверка паролей
 
+from random import randint
+import time
 
-s = '1234234123213487436578345'
+password = 'qwerty'
+pas = input('Введите пароль: ')
+count = 0
+while True:
+    count += 1
+    if pas == password:
+        print('Пароль верный.')
+        break
+    else:
+        if count == 3:
+            print('Проверка на робота!')
+            a = randint(0, 100)
+            b = randint(0, 100)
+            x = int(input(f'Решите пример: {a} + {b} = '))
+            if x == a + b:
+                print('Проверка пройдена успешно.')
+                count = 0
+            else:
+                print('Повторите попытку через 5 минут.')
+                time.sleep(5 * 60)
 
-for x in s:
-    if x in '02468':
-        print(x, end=' ')  # 2 4 2 4 2 2 4 8 4 6 8 4
-print()
+        pas = input('Пароль неверный, повторите попытку: ')
 
-for x in s:
-    if x not in '02468':
-        print(x, end=' ')  # 1 3 3 1 3 1 3 7 3 5 7 3 5 
-print()
-
-
-a, b, c = 4, 5, 6
-
-if a > 0 and b > 0 and c > 0:
-    print('and - все условия верные')
-if a > 0 or b > 0 or c > 0:
-    print('or - Хотя бы одно условие верное ')
-
-if (a > 0) + (b > 0) + (c > 0) == 1:
-    print('Только одно условие верное')
-if (a > 0) + (b > 0) + (c > 0) == 2:
-    print('Только два условия верные')
-if (a > 0) + (b > 0) + (c > 0) >= 1:
-    print('Хотя бы одно условие верное ')
-'''
+print('Добро пожаловать в программу!')
 
 # endregion Урок: *************************************************************
 # #
