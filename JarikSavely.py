dc86a042b286708d640734a8520e86fb45f41674
ilandroxxy <ilandroxxy@gmail.com>
2025-11-11 19:40:38 +0700

    Занятия Четверг `06Ноября2025`:
    - #Дмитрий #Федор Разбирали 15 номера
    
    - #Савелий #Ярик  Разбирали ТИ кодом
    
    - #РоманСергеевич #РоманВладимирович Разбирали 18 номера
    
    - #Артемий #Глеб  Разбирали 5 номера

--- JarikSavely.py
+++ JarikSavely.py
@@ -1,136 +1,58 @@
 # region Домашка: ******************************************************************
 
 
+# № 8594 (Уровень: Базовый)
+'''
+def F(a,s, n):
+    if a*s >= 455:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(a+1, s, n-1), F(a,s+1, n-1), F(a*2,s, n-1), F(a, s*2, n-1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
+
+print([s for s in range(1, 91) if  F(5,s, n=2)])
+print([s for s in range(1, 91) if  F(5,s, n=3) and not F(5,s, n=1) ])
+print([s for s in range(1, 91) if F(5,s, n=4) and not F(5,s, n=2)] )
+'''
+
+
+# 1 куча: +1, +2, *2 | s >= 61 | 1 <= s <= 56
+'''
+def F(s, n, c):
+    if s >= 61:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    if c == 'A':
+        h = [F(s + 2, n - 1, 'B'), F(s * 2, n - 1, 'C')]
+    elif c == 'B':
+        h = [F(s + 1, n - 1, 'A'), F(s * 2, n - 1, 'C')]
+    elif c == 'C':
+        h = [F(s + 1, n - 1, 'A'), F(s + 2, n - 1, 'B')]
+    else:
+        h = [F(s + 1, n - 1, 'A'), F(s + 2, n - 1, 'B'), F(s * 2, n - 1, 'C')]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+
+
+print([s for s in range(1, 57) if F(s, 2, '')])
+print([s for s in range(1, 57) if F(s, 3, '') and not F(s, 1, '')])
+print([s for s in range(1, 57) if F(s, 4, '') and not F(s, 2, '')])
+'''
+
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-
-# Пример 23 номера
-'''
-def F(a, b):
-    if a > b or a == 21:
-        return 0
-    elif a == b:
-        return 1
-    else:
-        h = [F(a + 2, b), F(a + 3, b), F(a * 2, b)]
-        return sum(h)
-
-print(F(7, 14) * F(14, 32))
-'''
-
-
-# № 22437 (Уровень: Базовый)
-# 1 куча: +4, +7, *4 | s >= 471 | 1 ≤ s ≤ 470
-
-# s - это кол-во камней в куче
-# n - это шаг игры:
-# n = 1: Петя 1
-# n = 2: Ваня 1
-# n = 3: Петя 2
-# n = 4: Ваня 2
-'''
-def F(s, n):
-    if s >= 471:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(s+4, n-1), F(s+7, n-1), F(s*4, n-1)]
-    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
-
-print([s for s in range(1, 471) if F(s, n=2)])
-print([s for s in range(1, 471) if F(s, n=3) and not F(s, n=1)])
-print([s for s in range(1, 471) if F(s, n=4) and not F(s, n=2)])
-'''
-
-
-# № 21905 Открытый вариант 2025 (Уровень: Базовый)
-# 1 куча: +1, +4, *3 | s >= 67 | 1 ≤ s ≤ 66
-'''
-def F(s, n):
-    if s >= 67:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(s+1, n-1), F(s+4, n-1), F(s*3, n-1)]
-    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
-
-print([s for s in range(1, 67) if F(s, n=2)])
-print([s for s in range(1, 67) if F(s, n=3) and not F(s, n=1)])
-print([s for s in range(1, 67) if F(s, n=4) and not F(s, n=2)])
-'''
-
-
-# № 23759 Демоверсия 2026 (Уровень: Базовый)
-# 1 куча: -3, -5, /4 (вниз) | s <= 30 | s ≥ 31
-'''
-from math import floor, ceil
-def F(s, n):
-    if s <= 30:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(s-3, n-1), F(s-5, n-1), F(floor(s/4), n-1)]
-    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
-
-print([s for s in range(31, 1000) if F(s, n=2)])
-print([s for s in range(31, 1000) if F(s, n=3) and not F(s, n=1)])
-print([s for s in range(31, 1000) if F(s, n=4) and not F(s, n=2)])
-'''
-
-# № 22066 (Уровень: Базовый)
-# 2 кучи: a+3, s+3, a*2, s*2 | a + s >= 100 | a = 17 | 1 ≤ s ≤ 82
-'''
-def F(a, s, n):
-    if a+s >= 100:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(a+3, s, n-1), F(a, s+3, n-1), F(a*2, s, n-1), F(a, s*2, n-1)]
-    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
-
-print([s for s in range(1, 83) if F(17, s, n=2)])
-print([s for s in range(1, 83) if F(17, s, n=3) and not F(17, s, n=1)])
-print([s for s in range(1, 83) if F(17, s, n=4) and not F(17, s, n=2)])
-'''
-
-
-# № 20907 Апробация 05.03.25 (Уровень: Базовый)
-'''
-def F(a, s, n):
-    if a+s >= 81:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(a+1, s, n-1), F(a, s+1, n-1), F(a*2, s, n-1), F(a, s*2, n-1)]
-    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
-
-print([s for s in range(1, 74) if F(7, s, n=2)])
-print([s for s in range(1, 74) if F(7, s, n=3) and not F(7, s, n=1)])
-print([s for s in range(1, 74) if F(7, s, n=4) and not F(7, s, n=2)])
-'''
-
-
-# № 19635 (Уровень: Базовый)
-# 2 кучи: a-3 и s-3, a/2, s/2 (вниз) | a + s <= 100 | a = 48 | s > 52
-'''
-from math import floor, ceil
-def F(a, s, n):
-    if a+s <= 100:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(a-3, s-3, n-1), F(floor(a/2), s, n-1), F(a, floor(s/2), n-1)]
-    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
-
-print([s for s in range(53, 1000) if F(48, s, n=2)])
-print([s for s in range(53, 1000) if F(48, s, n=3) and not F(48, s, n=1)])
-print([s for s in range(53, 1000) if F(48, s, n=4) and not F(48, s, n=2)])
-'''
-
-
+print('x y z w')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = (x or y) and (not(y == z)) and (not w)
+                if F == 1:
+                    print(x, y, z, w)
 
 # endregion Урок: *************************************************************
 # #
