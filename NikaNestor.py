cdb8f34de41888f01bf49092de2de81fd067cfc7
ilandroxxy macbook <ilandroxxy@example.com>
2025-12-24 00:55:53 +0700

    Занятия Вторник `23Декабря2025`:
    - #Женя #Дима #Кирилл Разбирали вопросы ДЗ по Теории игр 19-21 и разобрали 3 номер через ВПР
    
    - #Арсений #Вадим Разбирали 17 номер
    
    - #Данил Разбирали 27 номер
    
    - #Вероника #Нестор Разбриали 22 номер движение процессов и быстро 1 номер кодом
    
    - #РоманВладимирович #РоманСергеевич пропуск урока
    
    - #Савелий #Ярик Разбирали 3 номера

--- NikaNestor.py
+++ NikaNestor.py
@@ -6,58 +6,28 @@
 # #
 # region Урок: ********************************************************************
 
-'''
-from functools import *
+print('1 2 3 4 5 6 7 8')
+from itertools import permutations
+table = '12 13 14 21 25 27 31 34 37 41 43 48 52 56 58 65 68 72 73 84 85 86'
+graph = 'BD DB BE EB BC CB CH HC EH HE ED DE DG GD GA AG GF FG HF FH AF FA'
+for p in permutations('ABCDEFGH'):
+    nt = table
+    for i in range(1, 8+1):
+        nt = nt.replace(str(i), p[i-1])
+    if set(nt.split()) == set(graph.split()):
+        print(*p)
+        # 1 2 3 4 5 6 7 8
+        # E H B D F A C G
 
-@lru_cache(None)
-def F(n):
-    if n >= 21:
-        return F(n - 5) + 3480
-    if n < 21:
-        return 10 * (G(n - 9) - 30)
 
-def G(n):
-    if n >= 264685:
-        return n / 20 + 33
-    if n < 264685:
-        return G(n + 9) - 2
-
-for i in range(700):
-    F(i)
-    G(i)
-
-print(F(675))
-'''
-
-'''
-res = []
-def f(x, y, A):
-    return (x * y < A) or (x < 7 * y) or (343 < x)
-
-for A in range(0, 10000):
-    if all(f(x, y, A) for x in range(0, 300) for y in range(0, 300)):
-        print(A)
-        break
-'''
-# todo 15, 16 ЕГКР пересмотреть
-
-'''
-def is_prime(x):
-    if x <= 1:
-        return False
-    for j in range(2, int(x**0.5)+1):
-        if x % j == 0:
-            return False
-    return True
-
-print([x for x in range(1, 100) if is_prime(x)])
-print([x for x in range(1, 100) if not is_prime(x)])
-'''
+    # i 0    1    2    3    4    5    6    7
+    # ('H', 'G', 'F', 'E', 'D', 'B', 'A', 'C')
+    #   1    2    3    4    5    6    7    8
 
 
 # endregion Урок: *************************************************************
 # #
 # #
-# ФИПИ = [5, 8, 9, 7, 13, 14, 15, 16, 17, 18, 19-21, 22, 23, 25, 27]
+# ФИПИ = [1, 5, 8, 9, 7, 13, 14, 15, 16, 17, 18, 19-21, 22, 23, 25, 27]
 # КЕГЭ = []
 # на следующем уроке:
