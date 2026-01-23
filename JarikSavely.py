55970a33ec02d15f4a41f66a13a5873c507572b6
ilandorxxy <ilandroxxy@gmail.com>
2026-01-23 15:57:39 +0700

    Занятия Четверг `22Января2026`:
    - #Дмитрий #Федор Разбирали 7 номера
    
    - #Ярик #Савелий Начали разбирать 24 номер часть 1
    
    - #Влад #РоманВладимирович #РоманСергеевич Повторяли ТИ 19-21 номера

--- JarikSavely.py
+++ JarikSavely.py
@@ -1,68 +1,174 @@
 # region Домашка: ******************************************************************
 
 
+# № 23764 Демоверсия 2026 (Уровень: Базовый)
+# Назовём маской числа последовательность цифр, в которой также могут встречаться следующие символы:
+# – символ «?» означает ровно одну произвольную цифру;
+# – символ «*» означает любую последовательность цифр произвольной длины; в том числе «*» может задавать и пустую последовательность.
+# Например, маске 123*4?5 соответствуют числа 123405 и 12300405.
+# Среди натуральных чисел, не превышающих 1010, найдите все числа, соответствующие маске 3?12?14*5, делящиеся на 1917 без остатка.
+# В ответе запишите в первом столбце таблицы все найденные числа в порядке возрастания, а во втором столбце – соответствующие им
+# результаты деления этих чисел на 1917.
+'''
+from fnmatch import *
+for x in range(1917, 10**10, 1917):
+    if fnmatch(str(x), '3?12?14*5'):
+        print(x)
+
+from re import *
+for x in range(1917, 10**10, 1917):
+    if fullmatch('3[0-9]12[0-9]14[0-9]+5', str(x)):
+        print(x)
+'''
 
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-# № 25345 (Уровень: Базовый)
-# В начальный момент Черепаха находится в начале координат,
-# её голова направлена вдоль положительного направления оси ординат, хвост опущен.
+# https://education.yandex.ru/ege/inf/task/8337d1c9-9cbc-4ff8-ac40-ce04ba5dd256
+# Текстовый файл состоит не более чем из 10**6 символов X, Y и Z.
+# Определите максимальное количество идущих подряд символов Y.
 
-# Черепаха выполнила следующую программу:
-# Повтори 6 [Вперёд 33 Направо 90 Вперёд 20 Направо 90]
-# Поднять хвост
-# Вперёд 3 Направо 90 Вперёд 9 Налево 90
-# Опустить хвост
-# Повтори 6 [Вперёд 24 Направо 90 Вперёд 25 Направо 90]
+# Вариант 1: ctrl + F
+'''
+s = open('files/24.txt').readline()
+print(s)
+print(len('YYYYYYYYYYY'))
+'''
 
-# Определите, сколько точек с целочисленными координатами находится внутри пересечения фигур,
-# ограниченных заданными алгоритмом линиями.
-# Точки на границах этого пересечения учитывать не следует.
+# Вариант 2: Как 17 номер
+'''
+s = open('files/24.txt').readline()
+# s = 'xxxxxYYYYYYxxxYYxxxxYYYxxxYx'
+cnt = 1
+maxi = 0
+for i in range(0, len(s)-1, 1):
+    # if s[i] == 'Y' and s[i+1] == 'Y':
+    if s[i:i+2] == 'YY':
+        cnt += 1
+        maxi = max(maxi, cnt)
+    else:
+        cnt = 1
+print(maxi)
+'''
 
-import turtle as t
-t.screensize(5000, 5000)
-t.tracer(0)  # Отключает анимацию отрисовки
-t.left(90)
-t.down()
-size = 20
+# Вариант 3: Через замену replace()
+'''
+s = open('files/24.txt').readline()
+# s = 'ZXZXZXYYYYYYXZXZXZYYZXXZXZYYYZXZYX'
+s = s.replace('X', ' ').replace('Z', ' ')
+print(max([len(x) for x in s.split()]))
+'''
 
-for i in range(6):
-    t.forward(33 * size)
-    t.right(90)
-    t.fd(20 * size)
-    t.rt(90)
-t.up()  # Поднять хвост
-t.forward(3 * size)
-t.right(90)
-t.forward(9 * size)
-t.left(90)
-t.down()  # Опустить хвост
-for i in range(6):
-    t.forward(24 * size)
-    t.right(90)
-    t.fd(25 * size)
-    t.rt(90)
+# Вариант 4: Через import re
+'''
+from re import *
+s = open('files/24.txt').readline()
+# s = 'ZXZXZXYYYYYYXZXZXZYYZXXZXZYYYZXZYX'
+# print([x.group(0) for x in finditer('[Y]+', s)])  # ['YYYYYY', 'YY', 'YYY', 'Y']
+print(max([len(x.group(0)) for x in finditer('[Y]+', s)]))
+'''
 
 
-# Тут рисуем точки:
-t.up()
-for x in range(-50, 50):
-    for y in range(-50, 50):
-        t.goto(x * size, y * size)
-        t.dot(2, 'red')
+# https://education.yandex.ru/ege/inf/task/4a723df3-ea35-4f64-bba3-afda2c8fb879
+'''
+s = open('files/24.txt').readline()
+s = s.replace('X', ' ').replace('Z', ' ')
+print(max([len(x) for x in s.split()]))
 
-t.update()  # Для корректной работы t.tracer(0)
-t.done()
+s = open('files/24.txt').readline()
+s = s.replace('Y', ' ').replace('Z', ' ')
+print(max([len(x) for x in s.split()]))
 
+s = open('files/24.txt').readline()
+s = s.replace('Y', ' ').replace('X', ' ')
+print(max([len(x) for x in s.split()]))
+'''
+
+# https://education.yandex.ru/ege/inf/task/cd3c4d7c-cce1-4818-a4fc-b0f79a867e13
+'''
+s = open('files/24.txt').readline()
+cnt = 1
+maxi = 0
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        cnt += 1
+        maxi = max(maxi, cnt)
+    else:
+        cnt = 1
+print(maxi)
+
+s = open('files/24.txt').readline()
+while 'XX' in s or 'YY' in s or 'ZZ' in s:
+    s = s.replace('XX', 'X X').replace('YY', 'Y Y').replace('ZZ', 'Z Z')
+print(max([len(x) for x in s.split()]))
+'''
+
+# https://education.yandex.ru/ege/inf/task/28e37b75-0bb5-41f6-a308-b8d391400eb2
+'''
+s = open('files/24.txt').readline()
+s = s.replace('E', ' ').replace('F', ' ')
+print(max([len(x) for x in s.split()]))
+'''
+
+
+# https://education.yandex.ru/ege/inf/task/dd3c7b28-6972-4dde-8d11-ef34db484b6d
+'''
+s = open('files/24.txt').readline()
+s = s.replace('AB', 'A B')
+print(max([len(x) for x in s.split()]))
+'''
+
+# https://education.yandex.ru/ege/inf/task/915c9534-1982-40ce-9f6b-f5c8cec6bdef
+'''
+s = open('files/24.txt').readline()
+for x in 'LISENOK':
+    s = s.replace(x, ' ')
+print(max([len(x) for x in s.split()]))
+'''
+
+
+# https://education.yandex.ru/ege/inf/task/8fccd20b-438a-48a7-91f8-49e34350e04c
+'''
+s = open('files/24.txt').readline()
+s = s.replace('A', 'B').replace('B', 'C')
+while 'CC' in s:
+    s = s.replace('CC', 'C C')
+print(max([len(x) for x in s.split()]))
+'''
+
+
+# https://education.yandex.ru/ege/inf/task/98ae80c8-1e05-4cad-b4cc-4adc1edcc487
+
+s = open('files/24.txt').readline()
+cnt = 3
+maxi = 0
+for i in range(len(s)-3):
+    if s[i:i+4] in ('SQRP', 'QRPS', 'RPSQ', 'PSQR'):
+        cnt += 1
+        maxi = max(maxi, cnt)
+    else:
+        cnt = 3
+print(maxi)
+
+# QRP RP P SQRPSQRPSQRPSQRP S SQ SQR
+
+
+from re import *
+s = open('files/24.txt').readline()
+print([x.group(0) for x in finditer('(QRP|RP|P)(SQRP)+(S|SQ|SQR)', s)])
+print(max([len(x.group(0)) for x in finditer('(QRP|RP|P)(SQRP)+(S|SQ|SQR)', s)]))
+
+
+
+# https://education.yandex.ru/ege/inf/task/417b95a7-a644-43d9-b3f1-d1a1f753789a
 
 
 
 # endregion Урок: *************************************************************
 # #
 # #
-# ФИПИ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 18, 19-21, 22, 23, 25, 27]
+# ФИПИ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 18, 19-21, 22, 23, 24.1, 25, 27]
 # КЕГЭ = []
 # на следующем уроке: 10, 12, 24, 26
--- RomanPandorks.py
+++ RomanPandorks.py
@@ -7,125 +7,146 @@
 # region Урок: ********************************************************************
 
 
-# № 19882 (Уровень: Базовый)
 '''
-import sys
-sys.setrecursionlimit(2025)
-
-def F(n):
-    if n < 4:
-        return 1
-    if n >= 4:
-        return F(n - 1) + n * 2
-
-print(F(2025))
-'''
+n = 234
+s = str(n)
+summa = 0
+for x in s:
+    summa += int(x)
+print(summa)
 
 
-# № 20906 Апробация 05.03.25 (Уровень: Базовый)
-'''
-import sys
-sys.setrecursionlimit(10**8)
+print(sum([int(x) for x in str(n)]))
 
-def F(n):
-    if n == 1:
-        return 1
-    if n > 1:
-        return n * F(n - 1)
-
-print((F(2024) // 4 + F(2023)) // F(2022))
-'''
-# print((F(2024) / 4 + F(2023)) / F(2022))
-# OverflowError: integer division result too large for a float
-
-
-# № 23756 Демоверсия 2026 (Уровень: Базовый)
-'''
-import sys
-sys.setrecursionlimit(10**8)
-
-def F(n):
-    return 2 * (G(n - 3) + 8)
-
-def G(n):
-    if n < 10:
-        return 2 * n
-    if n >= 10:
-        return G(n - 2) + 1
-
-print(F(15548))
+print(sum(map(int, str(n))))
 '''
 
 
-# № 12470 PRO100 ЕГЭ 29.12.23 (Уровень: Базовый)
+# № 8585 (Уровень: Базовый)
 '''
-from functools import *
-
-@lru_cache(None)
-def F(n):
-    if n < 3:
-        return n
-    if n > 2 and n % 2 != 0:
-        return F(n - 1) + F(n - 2) + 1
-    if n > 2 and n % 2 == 0:
-        return sum([F(i) for i in range(1, n)])
-
-for i in range(50):
-    F(i)
-
-print(F(38))
-'''
-
-
-# № 21716 ЕГКР 19.04.25 (Уровень: Базовый)
-# A. Прибавь 3
-# B. Прибавь 7
-# C. Умножь на 3
-# Сколько существует таких программ, которые исходное число 12 преобразуют
-# в 89, и при этом траектория вычислений программы содержит
-# числа 40 и 72 и не содержит 56?
-'''
-def F(a, b):
+def F(a,b):
     if a == b:
         return 1
-    elif a > b or a == 56:
+    elif a < b:
         return 0
     else:
-        return F(a + 3, b) + F(a + 7, b) + F(a * 3, b)
+        return F(a - sum(map(int, str(a))), b) + F(a // 2, b) + F(a-1, b)
 
-print(F(12, 40) * F(40, 72) * F(72, 89))
-
-
-def F(a, b):
-    if a >= b or a == 56:
-        return a == b
-    return F(a + 3, b) + F(a + 7, b) + F(a * 3, b)
-
-print(F(12, 40) * F(40, 72) * F(72, 89))
+print(F(40, 25) * F(25, 10))
 '''
 
 
-# № 19253 ЕГКР 21.12.14 (Уровень: Базовый)
-# A. вычти 1
-# B. вычти 6
-# C. найди целую часть от деления на 2
-# Сколько существует таких программ, которые исходное число 34 преобразуют
-# в число 6, и при этом траектория вычислений содержит числа 19 и 29 и не содержит числа 24?
+# n = 1: Петя первый ход
+# n = 2: Ваня первый ход
+# n = 3: Петя второй ход
+# n = 4: Ваня второй ход
 
-def F(a, b):
-    if a == b:
-        return 1
-    elif a < b or a == 24:
+# № 25358 (Уровень: Базовый)
+# 1 куча: +2, +4, *2 | s >= 125 | 1 ≤ s ≤ 124
+'''
+def F(s, n):
+    if s >= 125:
+        return n % 2 == 0
+    if n == 0:
         return 0
-    else:
-        return F(a - 1, b) + F(a - 6, b) + F(a // 2, b)
-print(F(34, 29) * F(29, 19) * F(19, 6))
+    h = [F(s + 2, n - 1), F(s + 4, n - 1), F(s * 2, n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
 
+print(19, [s for s in range(1, 124+1) if F(s, n=2)])
+print(20, [s for s in range(1, 124+1) if F(s, n=3) and not F(s, n=1)])
+print(21, [s for s in range(1, 124+1) if F(s, n=4) and not F(s, n=2)])
+'''
+
+
+# № 23565 Пересдача 03.07.25(Уровень: Базовый)
+'''
+from math import ceil, floor
+def F(s, n):
+    if s <= 15:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s - 3, n - 1), F(s - 8, n - 1), F(floor(s / 3), n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+
+
+print(19, [s for s in range(16, 1000) if F(s, n=2)])
+print(20, [s for s in range(16, 1000) if F(s, n=3) and not F(s, n=1)])
+print(21, [s for s in range(16, 1000) if F(s, n=4) and not F(s, n=2)])
+'''
+
+
+# № 23759 Демоверсия 2026(Уровень: Базовый)
+'''
+from math import ceil, floor
+def F(s, n):
+    if s <= 30:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s - 3, n - 1), F(s - 5, n - 1), F(floor(s / 4), n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+
+
+print(19, [s for s in range(31, 1000) if F(s, n=2)])
+print(20, [s for s in range(31, 1000) if F(s, n=3) and not F(s, n=1)])
+print(21, [s for s in range(31, 1000) if F(s, n=4) and not F(s, n=2)])
+'''
+
+
+# № 20907 Апробация 05.03.25 (Уровень: Базовый)
+# 2 кучи: s1+1, s2+1, s1*2, s2*2 | s1 + s2 >= 81 | s1 = 7 | 1 <= s <= 73
+'''
+def F(s1, s2, n):
+    if s1 + s2 >= 81:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s1+1, s2, n - 1), F(s1, s2+1, n - 1), F(s1*2, s2, n - 1), F(s1, s2*2, n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
+
+
+print(19, [s for s in range(1, 73+1) if F(7, s, n=2)])
+print(20, [s for s in range(1, 73+1) if F(7, s, n=3) and not F(7, s, n=1)])
+print(21, [s for s in range(1, 73+1) if F(7, s, n=4) and not F(7, s, n=2)])
+'''
+
+
+# № 17532 Основная волна 07.06.24 (Уровень: Базовый)
+'''
+def F(s1, s2, n):
+    if s1 + s2 >= 65:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s1 + 1 , s2, n - 1), F(s1, s2 + 1, n - 1), F(s1 * 3 , s2, n - 1), F(s1, s2 * 3, n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
+
+print(19, [s for s in range(1, 58+1) if F(6, s, n=2)])
+print(20, [s for s in range(1, 58+1) if F(6, s, n=3) and not F(6, s, n=1)])
+print(21, [s for s in range(1, 58+1) if F(6, s, n=4) and not F(6, s, n=2)])
+'''
+
+# № 18268 (Уровень: Базовый)
+
+from math import ceil, floor
+def F(s1, s2, n):
+    if s1 + s2 <= 72:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s1 - 3, s2, n - 1), F(s1, s2 - 3, n - 1), F(ceil(s1 / 2), s2, n - 1), F(s1, ceil(s2 / 2), n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)  # else any(h)
+
+
+print(19, [s for s in range(23, 1000) if F(50, s, n=2)])
+print(20, [s for s in range(23, 1000) if F(50, s, n=3) and not F(50, s, n=1)])
+print(21, [s for s in range(23, 1000) if F(50, s, n=4) and not F(50, s, n=2)])
 
 
 # endregion Урок: *************************************************************
 # #
 # #
 # ФИПИ = [1, 2, 3, 4, 5, 6, 8, 13, 14, 15, 16, 18, 19-21, 23, 25, 27.1]
-# КЕГЭ = [5, 8, 14, 15, 16, 23]
+# КЕГЭ = [5, 8, 14, 15, 16, 23, 19-21]
 # на следующем уроке:
