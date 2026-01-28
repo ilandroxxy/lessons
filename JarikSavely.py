df4f3851a20399ba80bfbeeabfba3d3226e1ab22
ilandorxxy <ilandroxxy@gmail.com>
2026-01-28 12:56:58 +0700

    Занятия Вторник #27Января2026
    - #Дмитрий #Федор Разбирали 11 номер
    
    - #Арсений #Вадим Разобрали 22 номер построение таблиц (часть 1)
    
    - #Вероника #Нестор Разбирали 16 номер через списки и 24 номер сложные задачи прототип с T 100 раз
    
    - #Влад #РоманВладимирович #РоманСергеевич Разбирали контрольную с задачами сложнее ЕГЭ
    
    - #Савелий #Ярик Практиковали простейшие 24 номер

--- JarikSavely.py
+++ JarikSavely.py
@@ -1,168 +1,98 @@
 # region Домашка: ******************************************************************
 
 
-# № 23764 Демоверсия 2026 (Уровень: Базовый)
-# Назовём маской числа последовательность цифр, в которой также могут встречаться следующие символы:
-# – символ «?» означает ровно одну произвольную цифру;
-# – символ «*» означает любую последовательность цифр произвольной длины; в том числе «*» может задавать и пустую последовательность.
-# Например, маске 123*4?5 соответствуют числа 123405 и 12300405.
-# Среди натуральных чисел, не превышающих 1010, найдите все числа, соответствующие маске 3?12?14*5, делящиеся на 1917 без остатка.
-# В ответе запишите в первом столбце таблицы все найденные числа в порядке возрастания, а во втором столбце – соответствующие им
-# результаты деления этих чисел на 1917.
-'''
-from fnmatch import *
-for x in range(1917, 10**10, 1917):
-    if fnmatch(str(x), '3?12?14*5'):
-        print(x)
-
-from re import *
-for x in range(1917, 10**10, 1917):
-    if fullmatch('3[0-9]12[0-9]14[0-9]+5', str(x)):
-        print(x)
-'''
 
 # endregion Домашка: ******************************************************************
 # #
 # #
 # region Урок: ********************************************************************
 
-# https://education.yandex.ru/ege/inf/task/8337d1c9-9cbc-4ff8-ac40-ce04ba5dd256
-# Текстовый файл состоит не более чем из 10**6 символов X, Y и Z.
-# Определите максимальное количество идущих подряд символов Y.
+# № 858 (Уровень: Базовый)
+'''
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+s = open('files/24.txt').readlines()
+cnt = 0
+for x in s:
+    for a in alp:
+        if f'F{a}O' in x:
+            cnt += 1
+            break
+print(cnt)
+'''
 
-# Вариант 1: ctrl + F
+
+# № 223 Джобс 14.09.2020 (Уровень: Базовый)
+# Текстовый файл состоит не более чем из 106 символов I, K, O, T. Сколько раз встречаются комбинации «TIK» или «TOK».
 '''
 s = open('files/24.txt').readline()
-print(s)
-print(len('YYYYYYYYYYY'))
-'''
-
-# Вариант 2: Как 17 номер
-'''
-s = open('files/24.txt').readline()
-# s = 'xxxxxYYYYYYxxxYYxxxxYYYxxxYx'
-cnt = 1
-maxi = 0
-for i in range(0, len(s)-1, 1):
-    # if s[i] == 'Y' and s[i+1] == 'Y':
-    if s[i:i+2] == 'YY':
-        cnt += 1
-        maxi = max(maxi, cnt)
-    else:
-        cnt = 1
-print(maxi)
-'''
-
-# Вариант 3: Через замену replace()
-'''
-s = open('files/24.txt').readline()
-# s = 'ZXZXZXYYYYYYXZXZXZYYZXXZXZYYYZXZYX'
-s = s.replace('X', ' ').replace('Z', ' ')
-print(max([len(x) for x in s.split()]))
-'''
-
-# Вариант 4: Через import re
-'''
-from re import *
-s = open('files/24.txt').readline()
-# s = 'ZXZXZXYYYYYYXZXZXZYYZXXZXZYYYZXZYX'
-# print([x.group(0) for x in finditer('[Y]+', s)])  # ['YYYYYY', 'YY', 'YYY', 'Y']
-print(max([len(x.group(0)) for x in finditer('[Y]+', s)]))
+s = s.replace('TIK', '*').replace('TOK', '+')
+for x in 'IKOT':
+    s = s.replace(x, '')
+print(len(s))
 '''
 
 
-# https://education.yandex.ru/ege/inf/task/4a723df3-ea35-4f64-bba3-afda2c8fb879
-'''
-s = open('files/24.txt').readline()
-s = s.replace('X', ' ').replace('Z', ' ')
-print(max([len(x) for x in s.split()]))
 
-s = open('files/24.txt').readline()
-s = s.replace('Y', ' ').replace('Z', ' ')
-print(max([len(x) for x in s.split()]))
-
-s = open('files/24.txt').readline()
-s = s.replace('Y', ' ').replace('X', ' ')
-print(max([len(x) for x in s.split()]))
-'''
-
-# https://education.yandex.ru/ege/inf/task/cd3c4d7c-cce1-4818-a4fc-b0f79a867e13
+# № 105 Джобс 07.09.2020 (Уровень: Базовый)
+# Текстовый файл состоит не более чем из 106 символов F, A, I, L.
+# Определите максимальное количество подряд идущих одинаковых букв.
 '''
 s = open('files/24.txt').readline()
 cnt = 1
 maxi = 0
 for i in range(len(s)-1):
-    if s[i] != s[i+1]:
+    if s[i] == s[i+1]:
         cnt += 1
         maxi = max(maxi, cnt)
     else:
         cnt = 1
 print(maxi)
-
-s = open('files/24.txt').readline()
-while 'XX' in s or 'YY' in s or 'ZZ' in s:
-    s = s.replace('XX', 'X X').replace('YY', 'Y Y').replace('ZZ', 'Z Z')
-print(max([len(x) for x in s.split()]))
 '''
 
-# https://education.yandex.ru/ege/inf/task/28e37b75-0bb5-41f6-a308-b8d391400eb2
+
+# № 318 (Уровень: Средний)
+# Текстовый файл состоит не более чем из 106 символов и содержит только заглавные латинские буквы и десятичные цифры.
+# Определите максимальное нечётное число, записанное в этом файле.
 '''
 s = open('files/24.txt').readline()
-s = s.replace('E', ' ').replace('F', ' ')
-print(max([len(x) for x in s.split()]))
-'''
 
-
-# https://education.yandex.ru/ege/inf/task/dd3c7b28-6972-4dde-8d11-ef34db484b6d
-'''
-s = open('files/24.txt').readline()
-s = s.replace('AB', 'A B')
-print(max([len(x) for x in s.split()]))
-'''
-
-# https://education.yandex.ru/ege/inf/task/915c9534-1982-40ce-9f6b-f5c8cec6bdef
-'''
-s = open('files/24.txt').readline()
-for x in 'LISENOK':
+alp = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+for x in alp:
     s = s.replace(x, ' ')
-print(max([len(x) for x in s.split()]))
-'''
 
-
-# https://education.yandex.ru/ege/inf/task/8fccd20b-438a-48a7-91f8-49e34350e04c
-'''
-s = open('files/24.txt').readline()
-s = s.replace('A', 'B').replace('B', 'C')
-while 'CC' in s:
-    s = s.replace('CC', 'C C')
-print(max([len(x) for x in s.split()]))
-'''
-
-
-# https://education.yandex.ru/ege/inf/task/98ae80c8-1e05-4cad-b4cc-4adc1edcc487
-
-s = open('files/24.txt').readline()
-cnt = 3
 maxi = 0
-for i in range(len(s)-3):
-    if s[i:i+4] in ('SQRP', 'QRPS', 'RPSQ', 'PSQR'):
-        cnt += 1
-        maxi = max(maxi, cnt)
-    else:
-        cnt = 3
+for x in s.split():
+    while x[-1] in '02468':
+        x = x[:-1]
+        if len(x) == 0:
+            break
+    if len(x) > 0:
+        maxi = max(maxi, int(x))
 print(maxi)
+'''
 
-# QRP RP P SQRPSQRPSQRPSQRP S SQ SQR
-
-
-from re import *
+# № 864 (Уровень: Базовый)
+# В текстовом файле находится цепочка из символов латинского алфавита A, B, C, D, E, F.
+# Найдите длину самой длинной подцепочки, не содержащей гласных букв.
+'''
 s = open('files/24.txt').readline()
-print([x.group(0) for x in finditer('(QRP|RP|P)(SQRP)+(S|SQ|SQR)', s)])
-print(max([len(x.group(0)) for x in finditer('(QRP|RP|P)(SQRP)+(S|SQ|SQR)', s)]))
+alp = 'ABCDEF'
+for x in alp:
+    s = s.replace('A', ' ').replace('E', ' ')
+print(max([len(x) for x in s.split()]))
+'''
 
 
+# № 1077 (Уровень: Средний)
+# Текстовый файл состоит не более чем из 106 символов 1, 2, 3, A, B, C.
+# Определите максимальное количество идущих подряд в неубывающем порядке цифр (цифры могут повторяться).
 
-# https://education.yandex.ru/ege/inf/task/417b95a7-a644-43d9-b3f1-d1a1f753789a
+s = open('files/24.txt').readline()
+alp = 'ABC'
+for x in alp:
+    s = s.replace(x, ' ')
+print(max([len(x) for x in s.split() if sorted(x) == list(x)]))
+
 
 
 
