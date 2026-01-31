fc6524854f3cf5e23e68bfbee40b288a3976ea10
ilandorxxy <ilandroxxy@gmail.com>
2026-01-31 22:12:52 +0700

    Занятия Суббота `31Января2026`:
    - #Анна #Татьяна Разбирали вариант ЕГКР номер 1
    
    - #Артемий #Глеб Разбирали 17 номера

--- TatianaAnna.py
+++ TatianaAnna.py
@@ -8,199 +8,216 @@
 
 
 
-# Номер 5 Статград вариант 2
+# Номер 5 (ЕГКР 13.12.25 Вариант 1)
+'''
+def G(n, b):
+    r = ''
+    while n > 0:
+        r = str(n % b) + r
+        n //= b
+    return r
 
 L = []
 for n in range(1, 10000):
-    s = bin(n)[2:]
-    if n % 5 == 0:
-        s = s + '11'
+    s = G(n, 3)
+    if n % 3 == 0:
+        s = s + s[-2:]
     else:
-        s = s + bin(n // 5)[2:]
+        x = sum([int(i) for i in s]) * 3
+        s = s + G(x, 3)
 
-    r = int(s, 2)
-    if r > 896 and n % 2 == 0:
-        L.append(n)
-print(min(L))
-
-
-# Номер 7 Статград вариант 2
-'''
-a = 2
-b = 56000
-c = 15
-t = 1647
-# print(27 * 60 + 27)
-I_music = a * b * c * t  # Только вес МУЗЫКИ
-
-
-U = 367_217_732
-T = 332
-I_all = U * T  # Вес МУЗЫКИ и ЗАГОЛОВКОВ
-
-print(I_all - I_music)  # Вес ЗАГОЛОВКОВ
-print((I_all - I_music) / 28)  # бит на один ЗАГОЛОВОК
-print(((I_all - I_music) / 28) / 2**13)  # бит на один ЗАГОЛОВОК
-'''
-# 519449.842285 -> 519449
-
-
-
-# Номер 8 Статград вариант 2
-'''
-cnt = 0
-s = sorted('0123456789ABCDEF')
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                num = a + b + c + d
-                if a != '0':
-                    if num.count('3') == 1:
-                        if num[0] != num[1] and num[1] != num[2] and num[2] != num[3]:
-                            cnt += 1
-print(cnt) # 13500
+    r = int(s, 3)
+    if r > 208 and r % 2 != 0:
+        L.append(r)
+print(min(L))  # 243
 '''
 
-# Номер 17 Статград вариант 2
+
+# Номер 14 (ЕГКР 13.12.25 Вариант 1)
 '''
-M = [int(x) for x in open('files/17.txt')]
-A = [x for x in M if x < 0 and len(str(abs(x))) == 4 and abs(x) % 9 == 0]
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r = alp[n % b] + r
+        n //= b
+    return r
+
 R = []
-for i in range(len(M) - 1):
-    x, y = M[i], M[i+1]
-    if (x < 0) + (y < 0) == 1:
-        if (x + y) > max(A):
-            R.append(x**2 + y**2)
-print(len(R), min(R))
-'''
-
-# Номера 19-21 Статград вариант 2
-'''
-from math import ceil, floor
-def F(s, n):
-    if s <= 537:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(s-4, n-1), F(floor(s/5), n-1)]
-    return any(h) if (n-1) % 2 == 0 else all(h)  # else any(h)
-
-print(max([s for s in range(538, 20000) if F(s, n=2)])) # 13449
-print([s for s in range(538, 20000) if F(s, n=3) and not F(s, n=1)]) # 542 543
-print([s for s in range(538, 20000) if F(s, n=4) and not F(s, n=2)]) # 546
+for x in range(1, 27000):
+    n = 3 * 27**9 + 2 * 27**6 + 27**3 - x
+    s = convert(n, 27)
+    if s.count('0') == 6:
+        R.append(x)
+print(min(R))  # 27
 '''
 
 
-# Номер 9 Статград вариант 2
+# Номер 16 (ЕГКР 13.12.25 Вариант 1)
+'''
+from functools import *
+
+@lru_cache(None)
+def F(n):
+    if n >= 19:
+        return F(n-4) + 3580
+    if n < 19:
+        return 6 *(G(n-7) - 36)
+
+@lru_cache(None)
+def G(n):
+    if n >= 248045:
+        return n / 20 + 28
+    if n < 248045:
+        return G(n+9) - 4
+
+for i in range(250000, 0, -1):
+    G(i)
+
+for i in range(1, 1000):
+    F(i)
+
+print(F(673))
+'''
+
+# Вариант 2
+'''
+G = [0] * 250000
+F = [0] * 1000
+
+for n in range(250000-1, 0, -1):
+    if n >= 248045:
+        G[n] = n / 20 + 28
+    if n < 248045:
+        G[n] = G[n + 9] - 4
+
+for n in range(1, 1000):
+    if n >= 19:
+        F[n] = F[n - 4] + 3580
+    if n < 19:
+        F[n] = 6 * (G[n - 7] - 36)
+
+print(F[673])
+'''
+
+
+# todo глянуть: Номер 15 (ЕГКР 13.12.25 Вариант 1)
+'''
+def F(x, y, A):
+    return (78125 != y + 4*x) or (A > x) and (A > y)
+
+RES = []
+for A in range(1, 5000):
+    if all(F(x, 78_125 - 4*x, A) for x in range(1, 19531)):
+        RES.append(A)
+print(min(RES))
+
+# 78_125 != y + 4*x
+# y != 78_125 - 4*x
+
+print(78_125 / 4)  # 19531.25
+'''
+
+# Номер 9 (ЕГКР 13.12.25 Вариант 1)
 '''
 cnt = 0
 for s in open('files/9.csv'):
     M = [int(x) for x in s.split(';')]
     copied1 = [x for x in M if M.count(x) == 1]
-    if 3 <= M.count(max(M)) <= 4 and 4 <= len(copied1) <= 5:
-        if min(copied1) + max(copied1) <= sum(copied1) - max(copied1) - min(copied1):
+    copied3 = [x for x in M if M.count(x) == 3]
+    if len(copied3) == 3 and len(copied1) == 4:
+        # if max(M) in copied1:
+        # if max(M) not in copied3:
+        if M.count(max(M)) == 1:
             cnt += 1
-print(cnt)
+print(cnt)  # 1595
 '''
 
 
-# Номер 24 Статград вариант 2
+# Номер 17 (ЕГКР 13.12.25 Вариант 1)
 '''
-from re import *
+M = [int(x) for x in open('files/17.txt')]
+A = [x for x in M if len(str(abs(x))) == 4]
+B = [x for x in M if abs(x) % 100 == 30]
+R = []
+for i in range(len(M) - 2):
+    x, y, z = M[i], M[i+1], M[i+2]
+    if (x in A) + (y in A) + (z in A) == 0:
+        if (x + y + z) > max(B):
+            R.append(x + y + z)
+print(len(R), max(R))  # 1032 285423
+'''
+
+
+# Номер 25 (ЕГКР 13.12.25 Вариант 1)
+'''
+def divisors(x):  # 24
+    d = []
+    for j in range(1, int(x**0.5)+1):
+        if x % j == 0:
+            d.append(j)  # 4
+            d.append(x // j)  # 24 // 4 == 6
+    return sorted(set(d))
+
+cnt = 0
+for x in range(1_350_050+1, 10**10):
+    d = [j for j in divisors(x) if j != x and j != 11 and j % 100 == 11]
+    if len(d) > 0:
+        print(x, min(d))
+        cnt += 1
+        if cnt == 5:
+            break
+'''
+
+
+# Номер 11 (ЕГКР 13.12.25 Вариант 1)
+'''
+sym = 105
+# alp - ?
+# i - ?
+# bit - ?
+
+byte = 7 * 2**20 / 65_536
+print(byte)  # 112.0  - 112  (потребовалось не менее 7 Мбайт)
+bit = 112 * 8  # бит на один пароль
+
+# bit = sym * i
+# i = bit / sym
+i = bit / sym
+print(i)  # 8.533 -> 9 (потребовалось не менее 7 Мбайт)
+
+# При i = 9
+print(f'Минимальная возможная мощность алфавита: {2**8 + 1}')
+print(f'Максимальная возможная мощность алфавита: {2**9}')
+
+# Ответ: 257
+
+
+alp = 64  # i = 6
+
+alp = 50  # i = 6
+
+alp = 33 # i = 6  (2**5 + 1) 
+
+alp = 32  # i = 5
+'''
+
+
+# Номер 24 (ЕГКР 13.12.25 Вариант 1)
+'''
 s = open('files/24.txt').readline()
-pat = '[1-9]+([+*][1-9]+)*'
-M = [x.group(0) for x in finditer(pat, s)]
-
+for x in '02468':
+    s = s.replace(x, '*')
+s = s.split('F')
 maxi = 0
-for x in M:
-    s = s.replace('+', '*')
-    test = s.split('*')
-    if max([len(i) for i in test]) > 40:
-        continue
-    maxi = max(maxi, len(x))
+for i in range(len(s)-76):
+    r = 'F'.join(s[i:i+77])
+    if r.count('*') == 1 and r[0] == '*':
+        maxi = max(maxi, len(r))
 print(maxi)
 '''
 
-# Номер 6 Статград вариант 2
-'''
-import turtle as t
-
-t.tracer(0)
-t.left(90)
-t.screensize(5000, 5000)
-size = 10
-
-for i in range(7):
-    t.forward(17 * size)
-    t.right(90)
-    t.forward(26 * size)
-    t.right(90)
-
-t.up()
-
-t.forward(4 * size)
-t.right(90)
-t.forward(6 * size)
-t.left(90)
-
-t.down()
-
-for i in range(7):
-    t.forward(278 * size)
-    t.right(90)
-    t.forward(345 * size)
-    t.right(90)
-
-t.up()
-
-for x in range(-25, 25):
-    for y in range(-25, 25):
-        t.goto(size * x, size * y)
-        t.dot(3, 'red')
-
-t.update()
-t.done()
-'''
-
-
-
-from math import dist
-clustersA = [[], []]
-clustersB = [[], [], []]
-
-for s in open('files/27B.txt'):
-    s = s.replace(',', '.')
-    x, y = [float(i) for i in s.split()]
-    if 25 < y < 31 and 22 < x < 29:
-        clustersB[0].append([x, y])
-    if 34 < y < 41 and 20 < x < 27:
-        clustersB[1].append([x, y])
-    if 43 < y < 50 and 19 < x < 26:
-        clustersB[2].append([x, y])
-
-
-def center(cl):
-    R = []
-    for p in cl:
-        summa = 0
-        for g in cl:
-            summa += dist(p, g)
-            R.append([summa, p])
-    return min(R)[1]
-
-
-print(center(clustersB[0])) # [27.076197499885254, 26.453819388436255]
-print(center(clustersB[1])) # [21.71746862058927, 37.42511852914926]
-print(center(clustersB[2])) # [20.46221682747454, 44.66104487492894]
-
-print(dist([0.0, 0.0], [27.076197499885254, 26.453819388436255])) # 37.853996239350984
-print(dist([0.0, 0.0], [21.71746862058927, 37.42511852914926])) # 43.26994268781911
-print(dist([0.0, 0.0], [20.46221682747454, 44.66104487492894])) # 49.12546434197848
-
-QxB = 20.46221682747454 * 10000
-QyB = 26.453819388436255 * 10000
-print(int(QxB), int(QyB))
+# 3, 27
 
 # endregion Урок: *************************************************************
 # #
