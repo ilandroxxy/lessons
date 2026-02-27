b4ea06d11069a86a1e08280f0faae68cdc6a4cb1
ilandorxxy <ilandroxxy@gmail.com>
2026-02-27 15:11:36 +0700

    Занятия Вторник `24Февраля2026`:
    - #Арсений #Вадим Разобрали сложные 15 и 16 номера из варианта ЕГКР также разобрали 12 номер
    
    - #Дмитрий #Федор Повторяли 11 номера
    
    - #Иван #Egor Разбирали вопросы по домашнему варианта номера 25 17 12
    
    - #Влад #РоманВладимирович #РоманСергеевич Повторяли и практиковали 17 номер
    
    - #Савелий #Ярик Плотно повторяли 23 номер

--- EgorIvan.py
+++ EgorIvan.py
@@ -6,134 +6,109 @@
 # #
 # region Урок: ********************************************************************
 
-# 19-21
+
+# Статград 23.10.25 вариант 2 (https://disk.yandex.ru/i/6Z6cTKKeb8lQNQ) Номер 7
 '''
-from math import ceil, floor
-def F(s, n):
-    if s <= 505:
-        return n % 2 == 0
-    if n == 0:
-        return 0
-    h = [F(s - 3, n - 1), F(floor(s / 5), n - 1)]
-    return any(h) if (n-1) % 2 == 0 else all(h)
+# V_music = a * b * c * t
+a = 2
+b = 56000
+c = 15
+t = 27 * 60 + 27
+V_music = a * b * c * t
 
-print(max([s for s in range(505, 20000) if F(s, n = 2)]))
-print([s for s in range(505, 10000) if not F(s, n=1) and F(s, n=3)])
-print([s for s in range(505, 10000) if F(s, n=4) and not F(s, n = 2)])
+U = 367_217_732  # бит/с
+T = 332  # с
+V_all = U * T
+
+print(V_all - V_music)   # 28 заголовков
+print((V_all - V_music) / 28)  # 1 заголовок в бит
+print(((V_all - V_music) / 28) / 2**13)  # 1 заголовок в бит
 '''
 
 
-# 9
+# Статград 23.10.25 вариант 2 (https://disk.yandex.ru/i/6Z6cTKKeb8lQNQ) Номер 11
+'''
+sym = 128
+byte = 29 * 2**20 / 335_793
+print(byte)  # 90.557 -> 90 (отведено не более 29 Мбайт)
+bit = 90 * 8
+i = bit / sym
+print(i)  # 5.625 -> 5 (отведено не более 29 Мбайт)
+print(f'Максимально возможную мощность алфавита: {2 ** 5}')
+print(f'Минимальная возможную мощность алфавита: {2 ** (5 - 1) + 1}')
+'''
+
+
+# Статград 23.10.25 вариант 2 (https://disk.yandex.ru/i/6Z6cTKKeb8lQNQ) Номер 14
+'''
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r += alp[n % b]
+        n //= b
+    return r[::-1]
+
+L = []
+for x in range(1, 7290+1):
+    n = 27 ** 298 + 27 ** 269 - x
+    s = convert(n, 27)
+    L.append(s.count('0'))
+print(max(L))
+'''
+
+# натуральное число range(1, ...)
+# целое неотрицательное число range(0, ...)
+# целое положительное число range(1, ...)
+
+
+# Статград 23.10.25 вариант 2 (https://disk.yandex.ru/i/6Z6cTKKeb8lQNQ) Номер 9
 '''
 cnt = 0
 for s in open('files/9.csv'):
     M = [int(x) for x in s.split(';')]
     copied1 = [x for x in M if M.count(x) == 1]
-    # if (M.count(min(M)) == 2 or M.count(min(M)) == 3) and (len(copied1) == 6 or len(copied1) == 5):
-    if 2 <= M.count(min(M)) <= 3 and 5 <= len(copied1) <= 6:
-        if min(copied1) ** 2 + max(copied1) ** 2 <= (sum(copied1) - min(copied1) - max(copied1)) ** 2:
+    if 3 <= M.count(max(M)) <= 4 and 4 <= len(copied1) <= 5:
+        if max(copied1) + min(copied1) <= sum(copied1) - max(copied1) - min(copied1):
             cnt += 1
 print(cnt)
 '''
 
-
-# 17
-'''
-M = [int(x) for x in open('files/17.txt')]
-A = [x for x in M if x < 0 and abs(x) % 6 == 0 and len(str(abs(x))) == 3]
-R = []
-for i in range(len(M)-1):
-    x, y = M[i], M[i+1]
-    if (x < 0) + (y < 0) == 1:
-        if (x + y) > max(A):
-            R.append(x ** 2 + y ** 2)
-print(len(R), max(R))
-'''
-
-
-#
+# Статград 23.10.25 вариант 2 (https://disk.yandex.ru/i/6Z6cTKKeb8lQNQ) Номер 25
 '''
 def divisors(x):
     d = []
-    for j in range(1, int(x**0.5)+1):
-        if x % j == 0:
-            d.append(j)
-            d.append(x // j)
-    return sorted(set(d))
-
-
-print([x for x in range(2, 100) if len(divisors(x)) == 2])
-'''
-
-# 25
-'''
-def divisors(x):
-    d = []
-    for j in range(2, int(x**0.5)+1):  # не считая самого числа.
+    for j in range(2, int(x ** 0.5)+1):  # не считая самого числа.
         if x % j == 0:
             d.append(j)
             d.append(x // j)
     return sorted(set(d))
 
 cnt = 0
-for x in range(1_325_000-1, -1, -1):
+for x in range(1_475_000-1, -1, -1):
     d = [j for j in divisors(x) if len(divisors(j)) == 0]
     if len(d) > 0:
         S = sum(d)
-        if S != 0 and S <= 30000 and S % 5 == 0:
+        if S != 0 and S <= 42000 and S % 6 == 0:
             print(x)
             cnt += 1
             if cnt == 5:
                 break
 '''
 
-# 11
+
+# Статград 23.10.25 вариант 2 (https://disk.yandex.ru/i/6Z6cTKKeb8lQNQ) Номер 17
 '''
-sym = 102
-
-byte = 53 * 2**20 / 282_952
-print(byte)  # 196.409 -> 196 (отведено не более 53 Мбайт)
-bit = 196 * 8
-
-i = bit / sym
-print(i)  # 15.372 -> 15 (отведено не более 53 Мбайт)
-
-print(2 ** 15)
-print(2 ** 14 + 1)
-'''
-
-
-# 7
-'''
-# V = a * b * c * t
-
-a = 4  # формате квадро
-b = 33000  # с частотой дискретизации 33 000 Гц
-c = 37  # и разрешением 37 бит
-t = 41 * 60 + 33
-
-V = a * b * c * t
-
-U = 363_956_352  # бит/с
-T = 307
-V_all = U * T
-
-print((V_all - V) / 30)
-print(((V_all - V) / 30) / 2**13)
-'''
-
-# 23
-'''
-def F(a, b):
-    if a >= b:
-        return a == b
-    return F(a + 1, b) + F(a * 2, b) + F(a * 3, b)
-
-print(F(6, 14) * F(14, 18) * F(18, 48))  # и через 14 и через 18 (пересечение)
-print(F(6, 14) * F(14, 48))  # разность - только через 14
-print(F(6, 18) * F(18, 48))  # разность - только через 18
-
-print(45 + 48 - 24)  # 69
+M = [int(x) for x in open('files/17.txt')]
+A = [x for x in M if x < 0 and abs(x) % 9 == 0 and len(str(abs(x))) == 4]
+R = []
+for i in range(len(M)-1):
+    x, y = M[i], M[i + 1]
+    # в которых есть только одно отрицательное число
+    if (x < 0) + (y < 0) == 1:
+        if (x + y) > max(A):
+            R.append(x ** 2 + y ** 2)
+print(len(R), min(R))
 '''
 
 
@@ -142,4 +117,4 @@ print(45 + 48 - 24)  # 69
 # #
 # ФИПИ = [1, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19-21, 22, 23, 25, 27]
 # КЕГЭ = []
-# на следующем уроке: 12, 9, 25, 22, 27
+# на следующем уроке: 9, 17, 12, 22, 27
