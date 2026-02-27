802671e3f7b4a0cfed28e9140b4de17441d3c7d8
ilandorxxy <ilandroxxy@gmail.com>
2026-02-27 15:00:23 +0700

    Занятия Четверг `18Февраля2026`:
    - #НикитаВолков #ЕгорВолков Продолжили решать 8 номера с цифрами
    
    - #Софья #Лиза Разобрали вопросы по по домашнему варианту и добили 7 номер на звуки
    
    - #Маша #Дарья #Вика Два урока разбирали вопросы по домашнему варианту статград вариант 1 16 декабаря
    
    - #Екатерина Разбирали вопросы по домашнему  варианту и повторяли 11 номер.mp4

--- NikitaEgor.py
+++ NikitaEgor.py
@@ -1,5 +1,154 @@
 # region Домашка: ******************************************************************
 
+# № 1933 (Уровень: Базовый)
+'''
+R = []
+from itertools import permutations
+for p in permutations('КЛАБХАУС'):
+    word = ''.join(p)
+    # if 'АА' not in word:
+    # if all(x not in word for x in 'АА ЛЛ КК ББ ХХ СС УУ'.split()):
+    if all(word[i] != word[i+1] for i in range(len(word)-1)):
+        R.append(word)
+print(len(set(R)))
+'''
+
+'''
+# i  0  1  2  3  4
+M = [1, 2, 3, 4, 5]
+
+# x[i], x[i+1]
+# 1 2
+# 2 3
+# 3 4
+# 4 5
+'''
+
+
+# № 23746 Демоверсия 2026 (Уровень: Базовый)
+'''
+n = 0
+L = []
+s = sorted('СТРОКА')
+for a in s:
+    for b in s:
+        for c in s:
+            for d in s:
+                for e in s:
+                    n += 1
+                    word = a + b + c + d + e
+                    if n % 2 == 0:
+                        if word[0] not in 'АСТ':
+                            if word.count('О') == 2:
+                                L.append(n)
+print(max(L))
+'''
+
+# № 17521 Основная волна 07.06.24 (Уровень: Базовый)
+# https://stepik.org/lesson/1038667/step/14?unit=1062772
+'''
+cnt = 0
+from itertools import product
+for p in product('01234567', repeat=5):
+    num = ''.join(p)
+    if num[0] != '0':
+        if num[0] not in '1357':
+            if num[-1] not in '26':
+                if num.count('7') <= 2:
+                    cnt += 1
+print(cnt)
+'''
+
+
+# № 4613 Основная волна 2022 (Уровень: Базовый)
+# Определите количество пятизначных чисел, записанных в девятеричной системе счисления,
+# которые не начинаются с нечетных цифр, не оканчиваются цифрами 1 или 8,
+# а также содержат в своей записи не более одной цифры 3.
+'''
+cnt = 0
+from itertools import product
+for p in product('012345678', repeat = 5):
+    num = ''.join(p)
+    if num[0] != '0':
+        if num[0] not in '1357':
+            if num[-1] not in '18':
+                if num.count('3') <= 1:
+                    cnt += 1
+print(cnt)
+'''
+
+
+# № 4668 Резервный день 2022 (Уровень: Базовый)
+# Определите количество пятизначных чисел, записанных в семеричной системе счисления,
+# которые начинаются с четных цифр, не оканчиваются на цифры, меньшие 3, а также
+# содержат в своей записи не более одной цифры 4.
+'''
+cnt = 0
+from itertools import product
+for p in product('0123456', repeat = 5):
+    num = ''. join(p)
+    if num[0] != '0':
+        if num[0] in '246':
+            if num[-1] not in '012':
+                if num.count('4') <= 1:
+                    cnt += 1
+print(cnt)
+'''
+
+# № 4696 Демоверсия 2023 (Уровень: Базовый)
+# Определите количество пятизначных чисел, записанных в восьмеричной системе счисления,
+# в записи которых только одна цифра 6, при этом никакая нечётная цифра не стоит рядом с цифрой 6.
+'''
+cnt = 0
+from itertools import product
+
+for p in product('01234567', repeat=5):
+    num = ''.join(p)
+    if num[0] != '0':
+        if num.count('6') == 1:
+            for x in '1357':
+                num = num.replace(x, '*')
+            if '*6' not in num and '6*' not in num:
+                cnt += 1
+print(cnt)
+'''
+
+
+# № 5114 /dev/inf 11.22 (Уровень: Базовый)
+# (А. Рогов) Определите количество пятизначных чисел, записанных в семеричной системе счисления,
+# в записи которых ровно одна цифра 5, при этом никакая четная цифра не стоит рядом с цифрой 5.
+'''
+cnt = 0
+from itertools import product
+for p in product('0123456', repeat = 5):
+    num = ''. join(p)
+    if num[0] != '0':
+        if num.count('5') == 1:
+            for x in '0246':
+                num = num.replace(x, '*')
+            if '*5' not in num and '5*' not in num:
+                cnt += 1
+print(cnt)
+'''
+
+
+# № 6591 Пробник ИМЦ СПб (Уровень: Средний)
+# Определите количество пятизначных чисел, записанных в семеричной системе счисления, в записи которых:
+# 1. только одна цифра 6;
+# 2. сумма четных цифр числа меньше суммы нечетных цифр числа.
+
+cnt = 0
+from itertools import product
+for p in product('0123456', repeat = 5):
+    num = ''.join(p)
+    if num[0] != '0':
+        if num.count('6') == 1:
+            chet = [int(x) for x in num if x in '0246']
+            nechet = [int(x) for x in num if x in '135']
+            if sum(chet) < sum(nechet):
+                cnt += 1
+print(cnt)
+
 
 # endregion Домашка: ******************************************************************
 # #
@@ -7,200 +156,7 @@
 # region Урок: *************************************************************
 
 
-#  № 6901 (Уровень: Средний)
-'''
-L = []
-s = sorted('БАРШ')
-n = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    word = a + b + c + d + e
-                    n += 1
-                    sogl = [x for x in word if x in 'БРШ']
-                    copied1 = [x for x in word if word.count(x) == 1]
-                    copied2 = [x for x in word if word.count(x) == 2]
-                    if len(sogl) <= 3:
-                        if len(copied2) == 2 and len(copied1) == 3:
-                            L.append(n)
-print(max(L))
-'''
 
-
-# № 5553 (Уровень: Средний)
-'''
-R = []
-from itertools import permutations
-for p in permutations('СОТОЧКА'):
-    word = ''.join(p)
-    if 'ОА' in word or 'АО' in word or 'ОО' in word:
-        R.append(word)
-print(len(set(R)))
-'''
-
-
-# № 13094 (Уровень: Средний)
-# Сколько существует 9-значных девятеричных чисел, в записи
-# которых не встречается цифра 0, любые две соседние цифры имеют
-# разную чётность, и никакая цифра не повторяется больше 3 раз?
-'''
-cnt = 0
-s = '12345678'
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    for f in s:
-                        for g in s:
-                            for h in s:
-                                for p in s:
-                                    num = a + b + c + d + e + f + g + h + p
-                                    copied = [x for x in num if num.count(x) <= 3]
-                                    if len(copied) == 9:
-                                        for x in '02468':
-                                            num = num.replace(x, '2')
-                                        for x in '1357':
-                                            num = num.replace(x, '1')
-                                        if '11' not in num and '22' not in num:
-                                            cnt += 1
-print(cnt)
-'''
-
-# № 13094 (Уровень: Средний)
-'''
-cnt = 0
-from itertools import product
-for p in product('12345678', repeat=9):
-    num = ''.join(p)
-    copied = [x for x in num if num.count(x) <= 3]
-    if len(copied) == 9:
-        for x in '02468':
-            num = num.replace(x, '2')
-        for x in '1357':
-            num = num.replace(x, '1')
-        if '11' not in num and '22' not in num:
-            cnt += 1
-print(cnt)
-'''
-
-
-
-# № 17627 Основная волна 19.06.24 (Уровень: Базовый)
-# Определите количество 15-ричных пятизначных чисел, в записи которых ровно
-# одна цифра 8 и не менее двух цифр с числовым значением, превышающим 9.
-'''
-cnt = 0
-s = '0123456789ABCDE'
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    num = a + b + c + d + e
-                    if num[0] != '0':
-                        if num.count('8') == 1:
-                            B = [x for x in num if x > '9']
-                            if len(B) >= 2:
-                                cnt += 1
-print(cnt)
-
-cnt = 0
-from itertools import product
-for p in product('0123456789ABCDE', repeat=5):
-    num = ''.join(p)
-    if num[0] != '0':
-        if num.count('8') == 1:
-            B = [x for x in num if x > '9']
-            if len(B) >= 2:
-                cnt += 1
-print(cnt)
-'''
-
-
-
-# № 17521 Основная волна 07.06.24 (Уровень: Базовый)
-# Определите количество восьмеричных пятизначных чисел, которые
-# не начинаются с нечётных цифр, не оканчиваются цифрами 2 или 6,
-# а также содержат не более двух цифр 7.
-'''
-cnt = 0
-s = '01234567'
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    num = a + b + c + d + e
-                    if num[0] != '0':
-                        if num[0] not in '1357':
-                            if num.count('7') <= 2:
-                                if num[-1] not in '26':
-                                    cnt += 1
-print(cnt)
-'''
-
-
-# № 4613 Основная волна 2022 (Уровень: Базовый)
-# Определите количество пятизначных чисел, записанных в девятеричной
-# системе счисления, которые не начинаются с нечетных цифр,
-# не оканчиваются цифрами 1 или 8, а также содержат в своей записи
-# не более одной цифры 3.
-'''
-cnt = 0
-from itertools import product
-for p in product('012345678', repeat=5):
-    num = ''.join(p)
-    if num[0] != '0':
-        if num[0] not in '1357':
-            if num.count('3') <= 1:
-                if num[-1] not in '18':
-                    cnt += 1
-print(cnt)
-'''
-
-
-# № 16374 ЕГКР 27.04.24 (Уровень: Базовый)
-# Сколько существует семизначных семеричных чисел, которые содержат
-# в своей записи ровно две чётные цифры?
-'''
-cnt = 0
-s = '0123456'
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    for f in s:
-                        for g in s:
-                            num = a + b + c + d + e + f + g
-                            if num[0] != '0':
-                                chet = [x for x in num if x in '0246']
-                                if len(chet) == 2:
-                                # if num.count('0') + num.count('2') + num.count('4') + num.count('6') == 2:
-                                    cnt += 1
-print(cnt)
-'''
-
-
-# № 4588 Основная волна 2022 (Уровень: Базовый)
-# Определите количество пятизначных чисел, записанных в восьмеричной
-# системе счисления, в записи которых ровно одна цифра 6, при этом
-# никакая нечётная цифра не стоит рядом с цифрой 6.
-
-cnt = 0
-from itertools import product
-for p in product('01234567', repeat=5):
-    num = ''.join(p)
-    if num[0] != '0':
-        if num.count('6') == 1:
-            for x in '1357':
-                num = num.replace(x, '1')
-            if '16' not in num and '61' not in num:
-                cnt += 1
-print(cnt)
 
 
 # endregion Урок: *************************************************************
--- VikaDaria.py
+++ VikaDaria.py
@@ -7,251 +7,254 @@
 # region Урок: ********************************************************************
 
 
-# todo глянуть новый 15 номер статград январь
-'''
-def F(x, y, A):
-    return (y < A) and (x < A) or (89_241 - 5*y)
 
-for A in range(0, 10000):
-    if all(F(89_241 - 5*y, y, A) for y in range(100000)):
-        print(A)
-        break
-'''
-
-
-
-# Статград вариант 1 (27.01.26) номер 16
-'''
-F = [0] * 1000
-G = [0] * 400_000
-
-for n in range(400_000-1, -1, -1):
-    if n >= 384_242:
-        G[n] = n / 4 + 18
-    if n < 384_242:
-        G[n] = 12 + G[n + 41]
-
-for n in range(0, 1000):  # 0, 999
-    if n >= 20:
-        F[n] = F[n - 4] + 4620
-    if n < 20:
-        F[n] = 8 * (G[n - 12] - 21)
-
-print(F[913])
-'''
-
-'''
-from functools import *
-
-@lru_cache(None)
-def F(n):
-    if n >= 20:
-        return F(n - 4) + 4620
-    if n < 20:
-        return 8 * (G(n - 12) - 21)
-
-@lru_cache(None)
-def G(n):
-    if n >= 384_242:
-        return n / 4 + 18
-    if n < 384_242:
-        return 12 + G(n + 41)
-
-for n in range(400_000-1, -1, -1):
-    G(n)
-
-for n in range(1, 1000):
-    F(n)
-
-print(F(913))
-'''
-
-# G(n + 41)
-# G(100) -> G(141) -> G(182)
-# for n in range(400_000-1, -1, -1):
-
-# G(n - 40)
-# G(100) -> G(60) -> G(20)
-# for n in range(1, 400_000):
-
-
-
-# № 9846 Основная волна 27.06.23 (Уровень: Базовый)
-# Назовём маской числа последовательность цифр, в которой также могут встречаться следующие символы:
+# (PRO100 ЕГЭ) Назовём маской числа последовательность цифр, в которой
+# также могут встречаться следующие символы:
+#
 # — символ «?» означает ровно одну произвольную цифру;
 # — символ «*» означает любую последовательность цифр произвольной длины;
 # в том числе «*» может задавать и пустую последовательность.
-
-# Среди натуральных чисел, не превышающих 10**8, найдите все числа, соответствующие маске 12*34?5,
-# делящиеся на 2025 без остатка.
-# В ответе запишите в первом столбце таблицы все найденные числа в порядке возрастания,
-# а во втором столбце — соответствующие им результаты деления этих чисел на 2025.
-# Количество строк в таблице для ответа избыточно.
+#
+# Среди натуральных чисел, не превышающих 107, найдите все простые числа,
+# соответствующие маске 3?1111*
 '''
 from fnmatch import *
-for x in range(2025, 10**8, 2025):
-    if fnmatch(str(x), '12*34?5'):
-        print(x, x // 2025)
 
-from re import *
-for x in range(2025, 10**8, 2025):
-    if fullmatch('12[0-9]*34[0-9]5', str(x)):
-        print(x, x // 2025)
-'''
-
-
-# № 12477 PRO100 ЕГЭ 29.12.23 (Уровень: Средний)
-'''
-def is_prime(x):
+def prost(x):
     if x <= 1:
         return False
-    for j in range(2, int(x**0.5)+1):
-        if x % j == 0:
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
             return False
     return True
 
-# print([x for x in range(100) if is_prime(x)])
-# # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-
-from fnmatch import *
 for x in range(1, 10**7):
     if fnmatch(str(x), '3?1111*'):
-        if is_prime(x):
+        if prost(x):
             print(x)
 '''
+from typing import is_protocol
 
+# https://stepik.org/lesson/1228669/step/4?unit=1242202
+# Найдите все натуральные числа N, принадлежащие отрезку
+# [400_000_000; 600_000_000], которые можно представить в виде,
+# N = 2 ** m * 3 ** n где m — чётное число, n — нечётное число.
+#  В ответе запишите все найденные числа в порядке возрастания.
+'''
+R = []
+for m in range(0, 30+1, 2):
+    for n in range(1, 19+1, 2):
+        N = 2 ** m * 3 ** n
+        if 400_000_000 <= N <= 600_000_000:
+            R.append(N)
 
-# № 11814 (Уровень: Средний)
-'''
-from re import *
-for x in range(1777, 10**10, 1777):
-    if fullmatch('21[0-9][0-9][0-9]68[0-9]79', str(x)):
-        print(x, x // 1777)
-'''
-
-# № 11955 (Уровень: Средний)
-'''
-from re import *
-for x in range(133, 10**10, 133):
-    if fullmatch('1[02468]2157[13579]*4', str(x)):
-        print(x, x // 133)
-'''
-
-# № 13832 (Уровень: Средний)
-'''
-from re import *
-for x in range(7777, 10**9, 7777):
-    if fullmatch('[02468][13579][02468][02468][13579][02468][13579]77', str(x)):
-        print(x, x // 7777)
+for x in sorted(R):
+    print(x)
 '''
 
 
-# № 14437 (Уровень: Средний)
+# Статград 16.12.25 Вариант 2 Номер 11
 '''
-def divisors(x):
-    d = []
-    for j in range(2, int(x**0.5)+1):  # не считая единицы и самого числа
-        if x % j == 0:
-            d += [j, x // j]
-    return sorted(set(d))
+sym = 128
 
+byte = 29 * 2**20 / 335_793
+print(byte) # 90.557 -> 90  (отведено не более 29 Мбайт)
+bit = 90 * 8
+
+i = bit / sym
+print(i)  # 5.625 -> 5  (отведено не более 29 Мбайт)
+
+print(2 ** 5)  # 32
+print(2 ** 4 + 1)
+'''
+
+
+# Статград 16.12.25 Вариант 1 Номер 11
+'''
+sym = 30
+alp = 26 * 2 + 10 + 15  # 25 + 52
+i = 7
+
+bit = sym * i
+print(bit / 8)  # 26.25 -> 27
+byte = 27
+
+dop = 28
+
+user = byte + dop
+
+print(2 * 2 ** 20 / user)  # 38130.036
+'''
+
+
+# Статград 16.12.25 Вариант 1 Номер 6
+'''
+import turtle as t
+
+t.tracer(0)
+t.screensize(5000, 5000)
+t.left(90)
+size = 15
+
+t.right(180)
+for i in range(9):
+    t.forward(59 * size)
+    t.left(90)
+    t.forward(84 * size)
+    t.left(90)
+t.up()
+t.forward(18 * size)
+t.left(90)
+t.forward(38 * size)
+t.right(90)
+t.down()
+for i in range(9):
+    t.forward(120 * size)
+    t.right(90)
+    t.forward(99 * size)
+    t.right(90)
+
+t.up()
+for x in range(-50, 150):
+    for y in range(-50, 50):
+        t.goto(x * size, y * size)
+        t.dot(2, 'red')
+
+t.update()
+t.done()
+'''
+
+# Статград 16.12.25 Вариант 1 Номер 8
+# Определите количество 16-ричных шестизначных чисел, в записи которых
+# содержится не менее одной цифры 5 и ровно две цифры с числовым
+# значением, превышающим 12, причём стоящие рядом.
+'''
 cnt = 0
-for x in range(700_000-1, -1, -1):
-    d = divisors(x)
-    if len(d) > 0:
-        M = int(sum(d) / len(d))
-        if M % 1000 == 313:
-            print(x, M)
-            cnt += 1
-            if cnt == 7:
-                break
-'''
-
-
-# № 21980 (Уровень: Средний)
-'''
-def is_prime(x):
-    if x <= 1:
-        return False
-    for j in range(2, int(x**0.5)+1):
-        if x % j == 0:
-            return False
-    return True
-
-def divisors(x):
-    d = []
-    for j in range(2, int(x**0.5)+1):  # не считая самого числа
-        if x % j == 0:
-            d += [j, x // j]
-    return sorted(set(d))
-
-cnt = 0
-for x in range(750_000-1, -1, -1):
-    d = [j for j in divisors(x) if is_prime(j) and j % 10 == 7]
-    if len(d) > 0:
-        F = int(sum(d) / len(d))
-        if F != 0 and F % 111 == 0:
-            print(x, F)
-            cnt += 1
-            if cnt == 5:
-                break
+from itertools import *
+for i in product('0123456789ABCDEF', repeat = 6):
+    word = ''.join(i)
+    if word[0] != '0':
+        if word.count('5') >= 1:
+            for x in 'DEF':
+                word = word.replace(x, 'D')
+            if word.count('DD') == 1 and word.count('D') == 2:
+                cnt += 1
+print(cnt)
 '''
 
 # Вариант 2
 '''
-def divisors(x):
-    d = []
-    for j in range(2, int(x**0.5)+1):  # не считая самого числа
-        if x % j == 0:
-            d += [j, x // j]
-    return sorted(set(d))
+K = []
+for x in 'DEF':
+    for y in 'DEF':
+        K.append(x + y)
 
+from itertools import *
 cnt = 0
-for x in range(750_000-1, -1, -1):
-    d = [j for j in divisors(x) if len(divisors(j)) == 0 and j % 10 == 7]
-    if len(d) > 0:
-        F = int(sum(d) / len(d))
-        if F != 0 and F % 111 == 0:
-            print(x, F)
-            cnt += 1
-            if cnt == 5:
-                break
+for i in product('0123456789ABCDEF', repeat = 6):
+    word = ''.join(i)
+    if word[0] != '0':
+        if word.count('5') >= 1:
+            k = 0
+            for x in K:
+                if x in word:
+                    k += 1
+            if k == 1:
+                M = [p for p in word if p in 'DEF']
+                if len(M) == 2:
+                    cnt += 1
+print(cnt)
 '''
 
 
-# № 23569 Пересдача 03.07.25 (Уровень: Средний)
+# Статград 16.12.25 Вариант 1 Номер 9
+'''
+n = 0
+for s in open('files/9.csv'):
+    M = [int(x) for x in s.split(';')]
+    chet = [x for x in M if x % 2 == 0]
+    nechet = [x for x in M if x % 2 != 0]
+    n += 1
+    if M == sorted(set(M)):
+        if len(chet) == len(nechet):
+            print(n, sum(M))
+'''
 
-def is_prime(x):
-    if x <= 1:
-        return False
-    for j in range(2, int(x**0.5)+1):
-        if x % j == 0:
-            return False
-    return True
 
-def divisors(x):
-    d = []
-    for j in range(1, int(x**0.5)+1):
-        if x % j == 0:
-            if is_prime(j) and is_prime(x // j):
-                if str(j).count('6') == 1 and str(x//j).count('6') == 1:
-                    d += [j, x // j]
-    return sorted(set(d))
+'''
+from re import *
+for x in range(2026, 10 ** 10, 2026):
+    if fullmatch('7[02468]23[02468]64[0-9]*8', str(x)):
+        print(x)
+'''
 
-cnt = 0
-for x in range(6_086_055+1, 10**10):
-    d = divisors(x)
-    if len(d) > 0:
-        print(x, max(d))
-        cnt += 1
-        if cnt == 5:
-            break
+
+# todo глянуть интересный 24 номер статград
+'''
+from re import *
+s = open('files/24.txt').readline()
+# s = 'AC12BDE3F2ED6'
+# pat = '[0-9]([A-Z]+[0-9])+'
+pat = '[0-9][A-Z]+[0-9]+'
+M = [x.group(0) for x in finditer(pat, s)]
+print(M)
+# print(min([len(x) for x in M if len(x) > 10000]))
+
+#    2BDE3F2       2 BDE3 F2
+# AC12BDE3F2ED6
+#        3F2ED6    3 F2 ED6
+'''
+
+'''
+s = open('files/24.txt').readline()
+for i in '0123456789':
+    s = s.replace(i, '*')
+c = 0
+k = []
+for i in s:
+    if i == '*':
+        c += 1
+    else:
+        if c != 0:
+            k.append(c)
+        c = 0
+print(k)
+
+s = s.split('*')
+mini = 10 ** 10
+m = [len(i) for i in s if len(i) != 0]
+print(m)
+for i in range(len(m) - 9999):
+    summ = sum(m[i:i + 10000])
+    if summ < mini:
+        mini = summ
+        l = sum(k[i:i + 10000])
+print(mini + l)
+'''
+
+
+
+f = open('files/27B_new.txt', mode='w')
+
+for s in open('files/27B.txt'):
+    s = s.replace('.', ',')
+    f.write(s)
+
+
+
+
+
+
+
+# 17 25 27,
+# ?: 24, 27 (не получилось построить диаграмму)
+
 
 
 # endregion Урок: *************************************************************
 # #
 # #
 # ФИПИ = [1, 2, 3, 5, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 18, 19-21, 22, 23, 24.1, 25, 27]
-# КЕГЭ = [7, 11, 25]
+# КЕГЭ = [6, 7, 9, 11, 25]
 # на следующем уроке:
 # Разбирать задачи 24 номера по типу арифметика и символ X 80 раз (как 51993 решу егэ)
