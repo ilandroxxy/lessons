1b177bc5232179288df3324a6d486b8db1019efd
ilandorxxy <ilandroxxy@gmail.com>
2026-09-10 01:00:59 +0700

    Занятия Четверг `03Сентября2026`:
    - #Вика Устанавливали программы и настраивали рабочий репозиторий
    
    - #Полина Разбирали теорию циклов while и for
    
    - #ДимаДюбченко Повторяли 14 номера, решали самостоятельно
    
    - #Максим Повторили 17 и 3 номера

--- polinkkqz.py
+++ polinkkqz.py
@@ -1,92 +1,150 @@
 
-# Условные операторы или ветвление: if, elif, else
-
-'''
-n = int(input('n: '))
-if n > 0:  # если
-    print('Число положительное')
-elif n < 0: # иначе если
-    print('Число отрицательное')
-else:  # иначе
-    print('Число равно нулю')
-'''
-
-'''
-x, y = 5, -4
-
-if x > 0 and y > 0:
-    print('Первая четверть')
-elif x > 0 and y < 0:
-    print('Четвертая четверть')
-elif x < 0 and y > 0:
-    print('Вторая четверть')
-elif x < 0 and y < 0:
-    print('Третья четверть')
-else:
-    print('Лежит на осях')
-print('Продолжение программы')
-'''
-
-# https://stepik.org/lesson/1309432/step/5?unit=1324548
-'''
-n = int(input())
-a = n // 10000
-b = (n // 1000) % 10
-c = (n % 1000) // 100
-d = (n % 100) // 10
-e = n % 10
-if a * c == b + d + e:
-    print('Да')
-# if a * c != b + d + e:
-#     print('Нет')
-else:
-    print('Нет')
-'''
 
 
-# Логические связки: and, or, not, not in, in, ==, != и тд
 
-# a = 5 - присваивание значения переменной
-# a == 5 - сравнение значений переменных (равны ли они)
-# a != 5 - сравнение значений переменных (не равны ли они)
-# a > 5 - проверяем, что а больше 5
-# a >= 5 - проверяем, что а больше или равно 5
-'''
-flag = True
-print(not flag)  # False
-print(not not flag)  # True
-'''
+
+
+# Циклы while и for в python
+
+# Цикл for отвечает на запросы: "поватори n раз", "пробеги от числа А до числа Б"
+
+# range(0, STOP-1, 1)
+# range(START, STOP-1, 1)
+# range(START, STOP-1, STEP)
 
 '''
-s = '329487234'
-
-for x in s:
-    if x in '02468':
-        print(x, end=' ')  # 2 4 8 2 4     
+for i in range(10):  # повтори 10 раз
+    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
 print()
 
+for i in range(8):
+    print(i, end=' ')  # 0 1 2 3 4 5 6 7
+print()
 
-for x in s:
-    if x not in '02468':
-        print(x, end=' ')  # 3 9 7 3 
+for i in range(2, 10):  # пробеги от числа А до числа Б
+    print(i, end=' ')  # 2 3 4 5 6 7 8 9
+print()
+
+for i in range(2, 10, 2):
+    print(i, end=' ')  # 2 4 6 8
+print()
+
+for i in range(2, 10+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
+print()
+
+for i in range(10, 0, -1):
+    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1
+print()
+
+# С циклами for можно работать и через последовательность
+
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
+
+print(len(M))  # 5 - возвращает длину последовательности M
+
+for i in range(len(M)):
+    # print(i, end=' ')  # 0 1 2 3 4
+    print(M[i], end=' ')  # a b c d e
+print()
+
+for i in range(len(M)):
+    M[i] = M[i] * i
+print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
+
+
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
+
+for x in M:
+    print(x, end=' ') # a b c d e
+print()
+
+for x in M:
+    if x in 'ae':
+        print(x, end=' ')  # a e
 print()
 '''
 
-'''
-s = '329487234'
 
-for x in s:
-    if x in '02468':
-        print(f'{x} - число четное')
+# Цикл while отвечает на запросы: "делать действие, пока условие верное", "бесконечные циклы"
+
+'''
+for i in range(2, 10+1, 2):
+    print(i, end=' ')  # 2 4 6 8 10
+print()
+
+i = 2
+while i <= 10:  # пока
+    print(i, end=' ')  # 2 4 6 8 10 
+    i += 2
+print()
+'''
+
+
+# Пример использования цикла while при проверке паролей
+'''
+password = 'qwerty'
+pas = input('Введите пароль: ')
+while True:
+    if pas == password:
+        print('Пароль верный!')
+        break  
     else:
-        print(f'{x} - число нечетное')
-print()
+        pas = input('Повторите попытку: ')
+
+print('Добро пожаловать!')
 '''
 
+
+# Бесконечные циклы и операторы: break, continue и exit()
 '''
-a, b, c = -4, -5, -6
-if a > 0 and b > 0 and c > 0:
-    print('AND')  # - это связка, которая проверяет, что ВСЕ условия выполняются
-if a > 0 or b > 0 or c > 0:
-    print('OR')  # - хотя бы одно условие доложно быть верное (выполняться)
+k = 0
+while True:
+    k += 1  # k = k + 1
+    if k % 2 == 1:
+        continue  # - прерывание итеррации цикла (шага цикла)
+    if k == 50_000:
+        exit()  # - прерывает выполнение всей программы
+    if k == 100_000:
+        break  # - прерывает выполнение цикла
+    print(k)
+print('Вышли из цикла')
+
+
+
+# ПОКА УСЛОВИЕ ВЕРНОЕ
+
+k = 0
+while k < 10:
+    print(k)
+'''
+
+
+# n = 1234
+# print(n % 10)  # 4
+# n //= 10  # n = 123
+# print(n % 10)  # 3
+# n //= 10  # n = 12
+# print(n % 10)  # 2
+# n //= 10  # n = 1
+
+'''
+n = int(input())  # 1234
+summa = 0
+count = 0
+total = 1
+while n > 0:
+    ostat = n % 10  # 4 3 2 1
+
+    summa += ostat  # 0 + 4 + 3 + 2 + 1 = 10
+    count += 1  # 1 2 3 4
+    total *= ostat  # 1 * 4 * 3 * 2 * 1 =
+
+    n //= 10
+
+print(summa)
+print(count)
+print(total)
 '''
